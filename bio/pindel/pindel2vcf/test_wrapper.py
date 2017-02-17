0ea111b893862ad1d2aa81359138a5a53dbee542
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-02-17 16:30:33 +0100

    Fixes.

--- bio/pindel/pindel2vcf/test_wrapper.py
+++ bio/pindel/pindel2vcf/test_wrapper.py
@@ -5,4 +5,4 @@ def setup_module():
     os.chdir(os.path.join(os.path.dirname(__file__), "test"))
 
 def test():
-    subprocess.check_call(["snakemake", "calls/deletions.vcf", "--use-conda", "-F"])
+    subprocess.check_call(["snakemake", "pindel/all_D.vcf", "--use-conda", "-F"])
