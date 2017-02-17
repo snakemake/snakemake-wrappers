1be35d6f4be4fa59eba8281bb26a71ea8254d17c
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-02-17 16:08:53 +0100

    Better test data, fixed output files.

--- bio/pindel/test_wrapper.py
+++ bio/pindel/test_wrapper.py
@@ -5,4 +5,4 @@ def setup_module():
     os.chdir(os.path.join(os.path.dirname(__file__), "test"))
 
 def test():
-    subprocess.check_call(["snakemake", "calls/pindel.vcf", "--use-conda", "-F"])
+    subprocess.check_call(["snakemake", "pindel/all_D", "--use-conda", "-F"])
