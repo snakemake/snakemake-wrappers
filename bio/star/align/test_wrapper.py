ecd262036ce4f3bb5700793bdc0de53c312bfec9
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-02-19 16:57:45 +0100

    Add comment.

--- bio/star/align/test_wrapper.py
+++ bio/star/align/test_wrapper.py
@@ -3,6 +3,7 @@ import os
 
 def setup_module():
     os.chdir(os.path.join(os.path.dirname(__file__), "test"))
+    # generate index on the fly, because it is huge regardless of genome size
     os.makedirs("index", exist_ok=True)
     subprocess.check_call(["STAR", "--genomeDir", "index", "--genomeFastaFiles", "genome.fasta", "--runMode", "genomeGenerate"])
 
