189788abc06f2b977f12f23c39958bc4b4d374f3
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-02-19 16:56:56 +0100

    Fix tests.

--- bio/star/align/test_wrapper.py
+++ bio/star/align/test_wrapper.py
@@ -3,6 +3,8 @@ import os
 
 def setup_module():
     os.chdir(os.path.join(os.path.dirname(__file__), "test"))
+    os.makedirs("index", exist_ok=True)
+    subprocess.check_call(["STAR", "--genomeDir", "index", "--genomeFastaFiles", "genome.fasta", "--runMode", "genomeGenerate"])
 
 def test():
     subprocess.check_call(["snakemake", "star/a/Aligned.out.bam", "--use-conda", "-F"])
