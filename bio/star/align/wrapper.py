73f6404fc83472b047b3a92a86aa1a9d2b4f8611
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-02-19 15:57:24 +0100

    Add STAR wrapper.

---
+++ bio/star/align/wrapper.py
@@ -0,0 +1,34 @@
+__author__ = "Johannes Köster"
+__copyright__ = "Copyright 2016, Johannes Köster"
+__email__ = "koester@jimmy.harvard.edu"
+__license__ = "MIT"
+
+
+import os
+from snakemake.shell import shell
+
+extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+n = len(snakemake.input.sample)
+assert n == 1 or n == 2, "input->sample must have 1 (single-end) or 2 (paired-end) elements."
+
+if snakemake.input.sample[0].endswith(".gz"):
+    readcmd = "--readFilesCommand zcat"
+else:
+    readcmd = ""
+
+
+outprefix = os.path.dirname(snakemake.output[0])
+
+
+shell(
+    "(STAR "
+    "{snakemake.params.extra} "
+    "--runThreadN {snakemake.threads} "
+    "--genomeDir {snakemake.params.index} "
+    "--readFilesIn {snakemake.input.sample} "
+    "{readcmd} "
+    "--outSAMtype BAM "
+    "--outFileNamePrefix {outprefix} "
+    "--outStd Log")
