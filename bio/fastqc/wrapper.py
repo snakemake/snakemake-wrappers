925c0c5d032412e9f3ea1d1edf4ffab8e08b58cd
Julian de Ruiter <julianderuiter@gmail.com>
2017-06-14 15:52:33 +0200

    Add fastqc wrapper.

---
+++ bio/fastqc/wrapper.py
@@ -0,0 +1,14 @@
+"""Snakemake wrapper for fastqc."""
+
+__author__ = "Julian de Ruiter"
+__copyright__ = "Copyright 2017, Julian de Ruiter"
+__email__ = "julianderuiter@gmail.com"
+__license__ = "MIT"
+
+from os import path
+from snakemake.shell import shell
+
+output_dir = path.dirname(snakemake.output.html)
+
+shell('fastqc {snakemake.params} --quiet '
+      '--outdir {output_dir} {snakemake.input[0]}')
