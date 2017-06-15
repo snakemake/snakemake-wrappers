05d151f95d1acc5a489bd84a99a7ecaea8fd7f17
Julian de Ruiter <julianderuiter@gmail.com>
2017-06-15 17:22:23 +0200

    Simplify bwa-mem wrapper.

--- bio/bwa/mem/wrapper.py
+++ bio/bwa/mem/wrapper.py
@@ -3,83 +3,52 @@ __copyright__ = "Copyright 2016, Johannes Köster and Julian de Ruiter"
 __email__ = "koester@jimmy.harvard.edu, julianderuiter@gmail.com"
 __license__ = "MIT"
 
-import errno
-import os
 
 from snakemake.shell import shell
 
 
-n = len(snakemake.input)
-assert n == 1 or n == 2, "Input must have 1 (single-end) or 2 (paired-end) elements."
+# Check inputs.
+if len(snakemake.input) not in {1, 2}:
+    raise ValueError("input must have 1 (single-end) or "
+                     "2 (paired-end) elements")
 
-extra = snakemake.params.get('extra', '')
+# Extract arguments.
+extra = snakemake.params.get("extra", "")
 
-sort = snakemake.params.get('sort', 'none')
-sort_order = snakemake.params.get('sort_order', 'coordinate')
-sort_extra = snakemake.params.get('sort_extra', '')
+sort = snakemake.params.get("sort", "none")
+sort_order = snakemake.params.get("sort_order", "coordinate")
+sort_extra = snakemake.params.get("sort_extra", "")
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
-if sort == 'none':
+# Determine which pipe command to use for converting to bam or sorting.
+if sort == "none":
 
-    # Run bwa mem, piped into samtools view.
-    shell(
-        "(bwa mem"
-        " -t {snakemake.threads}"
-        " {extra}"
-        " {snakemake.params.index}"
-        " {snakemake.input} |"
-        " samtools view -Sbh -o {snakemake.output[0]} -"
-        ") {log}")
+    # Simply convert to bam using samtools view.
+    pipe_cmd = "samtools view -Sbh -o {snakemake.output[0]} -"
 
-elif sort == 'samtools':
+elif sort == "samtools":
 
-    # Create output directory if it doesn't exist.
-    output_dir = os.path.dirname(snakemake.output[0])
-
-    try:
-        os.makedirs(output_dir)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+    # Sort alignments using samtools sort.
+    pipe_cmd = "samtools sort {sort_extra} -o {snakemake.output[0]} -"
 
     # Add name flag if needed.
-    if sort_order == 'queryname':
-        sort_extra += ' -n'
+    if sort_order == "queryname":
+        sort_extra += " -n"
 
-    # Run bwa mem, piped into samtools sort. Temp prefix
-    # is needed to avoid issue with samtools not being
-    # able to open the output files.
-    temp_prefix = os.path.splitext(snakemake.output[0])[0]
+elif sort == "picard":
 
-    shell(
-        "(bwa mem"
-        " -t {snakemake.threads}"
-        " {extra}"
-        " {snakemake.params.index}"
-        " {snakemake.input} |"
-        " samtools sort"
-        " {sort_extra}"
-        " -o {snakemake.output[0]}"
-        " -T {temp_prefix}"
-        " -"
-        ") {log}")
-
-elif sort == 'picard':
-
-    # Run bwa mem, piped into picard SortSam.
-    shell(
-        "(bwa mem"
-        " -t {snakemake.threads}"
-        " {extra}"
-        " {snakemake.params.index}"
-        " {snakemake.input} |"
-        " picard SortSam"
-        " {sort_extra}"
-        " INPUT=/dev/stdin"
-        " OUTPUT={snakemake.output[0]}"
-        " SORT_ORDER={sort_order}"
-        ") {log}")
+    # Sort alignments using picard SortSam.
+    pipe_cmd = ("picard SortSam {sort_extra} INPUT=/dev/stdin"
+                " OUTPUT={snakemake.output[0]} SORT_ORDER={sort_order}")
 
 else:
-    raise ValueError('Unknown value for params.sort ({})'.format(sort))
+    raise ValueError("Unexpected value for params.sort ({})".format(sort))
+
+shell(
+    "(bwa mem"
+    " -t {snakemake.threads}"
+    " {extra}"
+    " {snakemake.params.index}"
+    " {snakemake.input}"
+    " | " + pipe_cmd + ") {log}")
