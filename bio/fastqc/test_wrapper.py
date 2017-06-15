ad0ef66bce6d24de31ebc7f30b147b3c88098fa1
Julian de Ruiter <julianderuiter@gmail.com>
2017-06-15 09:12:59 +0200

    Allow arbitrary outputs for fastqc.

--- bio/fastqc/test_wrapper.py
+++ bio/fastqc/test_wrapper.py
@@ -5,4 +5,4 @@ def setup_module():
     os.chdir(os.path.join(os.path.dirname(__file__), "test"))
 
 def test():
-    subprocess.check_call(["snakemake", "qc/a_fastqc.html", "--use-conda", "-F"])
+    subprocess.check_call(["snakemake", "qc/a.html", "--use-conda", "-F"])
