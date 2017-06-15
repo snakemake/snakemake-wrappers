53f15b973a8b6ee75135a650a87e0ef2f3e8a71e
Julian de Ruiter <julianderuiter@gmail.com>
2017-06-15 16:38:00 +0200

    Move bwa with sorting into a single wrapper.

--- bio/bwa/mem/test_wrapper.py
+++ bio/bwa/mem/test_wrapper.py
@@ -4,5 +4,14 @@ import os
 def setup_module():
     os.chdir(os.path.join(os.path.dirname(__file__), "test"))
 
-def test():
-    subprocess.check_call(["snakemake", "mapped/a.bam", "--use-conda", "-F"])
+def test_sort_none():
+    subprocess.check_call(["snakemake", "mapped/a.bam", "--config",
+                           "sort=none", "--use-conda", "-F"])
+
+def test_sort_samtools():
+    subprocess.check_call(["snakemake", "mapped/a.bam", "--config",
+                           "sort=samtools", "--use-conda", "-F"])
+
+def test_sort_picard():
+    subprocess.check_call(["snakemake", "mapped/a.bam", "--config",
+                           "sort=picard", "--use-conda", "-F"])
--- bio/bwa/mem/wrapper.py
+++ bio/bwa/mem/wrapper.py
@@ -1,14 +1,85 @@
-__author__ = "Johannes Köster"
-__copyright__ = "Copyright 2016, Johannes Köster"
-__email__ = "koester@jimmy.harvard.edu"
+__author__ = "Johannes Köster, Julian de Ruiter"
+__copyright__ = "Copyright 2016, Johannes Köster and Julian de Ruiter"
+__email__ = "koester@jimmy.harvard.edu, julianderuiter@gmail.com"
 __license__ = "MIT"
 
+import errno
+import os
 
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell()
 
-shell(
-    "(bwa mem {snakemake.params} -t {snakemake.threads} "
-    "{snakemake.input.ref} {snakemake.input.sample} "
-    "| samtools view -Sbh -o {snakemake.output[0]} -) {log}")
+n = len(snakemake.input)
+assert n == 1 or n == 2, "Input must have 1 (single-end) or 2 (paired-end) elements."
+
+extra = snakemake.params.get('extra', '')
+
+sort = snakemake.params.get('sort', 'none')
+sort_order = snakemake.params.get('sort_order', 'coordinate')
+sort_extra = snakemake.params.get('sort_extra', '')
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+if sort == 'none':
+
+    # Run bwa mem, piped into samtools view.
+    shell(
+        "(bwa mem"
+        " -t {snakemake.threads}"
+        " {extra}"
+        " {snakemake.params.index}"
+        " {snakemake.input} |"
+        " samtools view -Sbh -o {snakemake.output[0]} -"
+        ") {log}")
+
+elif sort == 'samtools':
+
+    # Create output directory if it doesn't exist.
+    output_dir = os.path.dirname(snakemake.output[0])
+
+    try:
+        os.makedirs(output_dir)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+    # Add name flag if needed.
+    if sort_order == 'queryname':
+        sort_extra += ' -n'
+
+    # Run bwa mem, piped into samtools sort. Temp prefix
+    # is needed to avoid issue with samtools not being
+    # able to open the output files.
+    temp_prefix = os.path.splitext(snakemake.output[0])[0]
+
+    shell(
+        "(bwa mem"
+        " -t {snakemake.threads}"
+        " {extra}"
+        " {snakemake.params.index}"
+        " {snakemake.input} |"
+        " samtools sort"
+        " {sort_extra}"
+        " -o {snakemake.output[0]}"
+        " -T {temp_prefix}"
+        " -"
+        ") {log}")
+
+elif sort == 'picard':
+
+    # Run bwa mem, piped into picard SortSam.
+    shell(
+        "(bwa mem"
+        " -t {snakemake.threads}"
+        " {extra}"
+        " {snakemake.params.index}"
+        " {snakemake.input} |"
+        " picard SortSam"
+        " {sort_extra}"
+        " INPUT=/dev/stdin"
+        " OUTPUT={snakemake.output[0]}"
+        " SORT_ORDER={sort_order}"
+        ") {log}")
+
+else:
+    raise ValueError('Unknown value for params.sort ({})'.format(sort))
