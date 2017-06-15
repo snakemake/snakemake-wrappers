acdc43ceb7fdbd04ef7e5cd626066578d169357a
Julian de Ruiter <julianderuiter@gmail.com>
2017-06-15 09:39:37 +0200

    Add samtools/picard bwa mem wrappers.

---
+++ bio/bwa/mem-sorted-picard/wrapper.py
@@ -0,0 +1,26 @@
+"""Snakemake wrapper for aligning reads using bwa mem and sorting the
+alignments using picard SortSam."""
+
+__author__ = "Julian de Ruiter"
+__copyright__ = "Copyright 2017, Julian de Ruiter"
+__email__ = "julianderuiter@gmail.com"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+sort_order = snakemake.params.get("sort_order", "coordinate")
+log = snakemake.log_fmt_shell(stderr=True)
+
+shell(
+    "(bwa mem"
+    " -t {snakemake.threads}"
+    " {snakemake.params.bwa_extra}"
+    " {snakemake.input.ref}"
+    " {snakemake.input.sample} |"
+    " picard SortSam"
+    " {snakemake.params.sort_extra}"
+    " INPUT=/dev/stdin"
+    " OUTPUT={snakemake.output[0]}"
+    " SORT_ORDER={sort_order}"
+    ") {log}")
--- bio/bwa/mem-sorted/wrapper.py
+++ bio/bwa/mem-sorted/wrapper.py
@@ -1,15 +1,29 @@
 """Snakemake wrapper for aligning reads using bwa mem and sorting the
-alignments using  picard SortSam."""
+alignments using samtools."""
 
 __author__ = "Julian de Ruiter"
 __copyright__ = "Copyright 2017, Julian de Ruiter"
 __email__ = "julianderuiter@gmail.com"
 __license__ = "MIT"
 
+import errno
+import os
 
 from snakemake.shell import shell
 
-sort_order = snakemake.params.get("sort_order", "coordinate")
+
+# Create output directory if it doesn't exist.
+output_dir = os.path.dirname(snakemake.output[0])
+
+try:
+    os.makedirs(output_dir)
+except OSError as exception:
+    if exception.errno != errno.EEXIST:
+        raise
+
+# Run command. Temp prefix is needed to avoid issue with
+# samtools not being able to open the output files.
+temp_prefix = os.path.splitext(snakemake.output[0])[0]
 log = snakemake.log_fmt_shell(stderr=True)
 
 shell(
@@ -18,9 +32,9 @@ shell(
     " {snakemake.params.bwa_extra}"
     " {snakemake.input.ref}"
     " {snakemake.input.sample} |"
-    " picard SortSam"
+    " samtools sort"
     " {snakemake.params.sort_extra}"
-    " INPUT=/dev/stdin"
-    " OUTPUT={snakemake.output[0]}"
-    " SORT_ORDER={sort_order}"
-    ") {log}")
+    " -o {snakemake.output[0]}"
+    " -T {temp_prefix}"
+    " -"
+    ") {log} ")
