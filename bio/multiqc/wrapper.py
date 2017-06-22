82dc64ce1bfd4f2f2b23c86546c7bf72562841cd
Julian de Ruiter <julianderuiter@gmail.com>
2017-06-22 18:30:05 +0200

    Change input syntax for multiqc wrapper.

--- bio/multiqc/wrapper.py
+++ bio/multiqc/wrapper.py
@@ -11,6 +11,7 @@ from os import path
 from snakemake.shell import shell
 
 
+input_dirs = set(path.dirname(fp) for fp in snakemake.input)
 output_dir = path.dirname(snakemake.output[0])
 output_name = path.basename(snakemake.output[0])
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
@@ -21,5 +22,5 @@ shell(
     " --force"
     " -o {output_dir}"
     " -n {output_name}"
-    " {snakemake.input.directory}"
+    " {input_dirs}"
     " {log}")
