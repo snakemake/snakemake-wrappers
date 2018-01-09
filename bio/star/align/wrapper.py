8d9031482adc214c77a12b17538f679dc27bba73
arnar.flatberg <arnar.flatberg@ntnu.no>
2018-01-09 07:34:42 +0100

    removed input file sort and added comments about input

--- bio/star/align/wrapper.py
+++ bio/star/align/wrapper.py
@@ -17,8 +17,8 @@ fq2 =  snakemake.input.get("fq2")
 if fq2:
     fq2 = [snakemake.input.fq2] if isinstance(snakemake.input.fq2, str) else snakemake.input.fq2
     assert(len(fq1) == len(fq2), "input-> equal number of files required for fq1 and fq2")
-input_str_fq1 = ",".join(sorted(fq1))
-input_str_fq2 = ",".join(sorted(fq2)) if fq2 is not None else ""
+input_str_fq1 = ",".join(fq1)
+input_str_fq2 = ",".join(fq2) if fq2 is not None else ""
 input_str =  " ".join([input_str_fq1, input_str_fq2])
 
 if fq1[0].endswith(".gz"):
