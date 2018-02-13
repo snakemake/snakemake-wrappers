3ad6a63a94d3a1537997ff0cc2fc539e9c16fbb6
Patrik Smeds <patrik.smeds@gmail.com>
2018-02-13 08:23:22 +0100

    Bcftools merge wrapper.

---
+++ bio/bcftools/merge/wrapper.py
@@ -0,0 +1,11 @@
+__author__ = "Patrik Smeds"
+__copyright__ = "Copyright 2018, Patrik Smeds"
+__email__ = "patrik.smeds@gmail.com"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+shell(
+    "bcftools merge {snakemake.params} -o {snakemake.output[0]} "
+    "{snakemake.input.calls}")
