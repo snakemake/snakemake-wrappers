8acd6dc7bee6e9f986e0f9e6d47d453b32d2dc92
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-02-15 11:29:40 +0100

    Fix assertions.

--- bio/star/align/wrapper.py
+++ bio/star/align/wrapper.py
@@ -11,12 +11,12 @@ extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 fq1 = snakemake.input.get("fq1")
-assert(fq1 is not None, "input-> fq1 is a required input parameter")
+assert fq1 is not None, "input-> fq1 is a required input parameter"
 fq1 = [snakemake.input.fq1] if isinstance(snakemake.input.fq1, str) else snakemake.input.fq1
 fq2 =  snakemake.input.get("fq2")
 if fq2:
     fq2 = [snakemake.input.fq2] if isinstance(snakemake.input.fq2, str) else snakemake.input.fq2
-    assert(len(fq1) == len(fq2), "input-> equal number of files required for fq1 and fq2")
+    assert len(fq1) == len(fq2), "input-> equal number of files required for fq1 and fq2"
 input_str_fq1 = ",".join(fq1)
 input_str_fq2 = ",".join(fq2) if fq2 is not None else ""
 input_str =  " ".join([input_str_fq1, input_str_fq2])
