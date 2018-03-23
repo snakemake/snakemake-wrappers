237812aa2aad59b922b78000b54b61b9cd074bc6
Bradford Powell <bpow@drpowell.org>
2018-03-23 12:16:30 -0400

    use context manager to make sure temp directory is deleted

--- bio/snpeff/wrapper.py
+++ bio/snpeff/wrapper.py
@@ -10,6 +10,9 @@ import shutil
 import tempfile
 
 shell.executable("bash")
+shell_command =("(snpEff {dataDir} {statsOpt} {extra}"
+                " {snakemake.params.reference} {snakemake.input}"
+                " > {snakemake.output.vcf}) {log}")
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
@@ -22,26 +25,21 @@ if dataDir:
 stats = snakemake.output.get("stats", "")
 genes = snakemake.output.get("genes", "")
 if stats or genes:
-    stats_tempdir = tempfile.mkdtemp()
-    if not stats:
-        if genes.endswith('.genes.txt'):
-            stats = genes[:-10] + '.html'
-        else:
-            stats = genes + '.html'
-    if not genes:
-        if stats.endswith('.html'):
-            genes = stats[:-5] + '.genes.txt'
-        else:
-            genes = stats + '.genes.txt'
-    statsOpt = '-stats "%s"'%path.join(stats_tempdir, 'stats')
+    with tempfile.TemporaryDirectory() as stats_tempdir:
+        if not stats:
+            if genes.endswith('.genes.txt'):
+                stats = genes[:-10] + '.html'
+            else:
+                stats = genes + '.html'
+        if not genes:
+            if stats.endswith('.html'):
+                genes = stats[:-5] + '.genes.txt'
+            else:
+                genes = stats + '.genes.txt'
+        statsOpt = '-stats "%s"'%path.join(stats_tempdir, 'stats')
+        shell(shell_command)
+        shutil.move(path.join(stats_tempdir, 'stats'), stats)
+        shutil.move(path.join(stats_tempdir, 'stats.genes.txt'), genes)
 else:
-    stats_tempdir = None
     statsOpt = '-noStats'
-
-shell("(snpEff {dataDir} {statsOpt} {extra} {snakemake.params.reference} {snakemake.input}"
-      " > {snakemake.output.vcf}) {log}")
-
-if stats_tempdir:
-    shutil.move(path.join(stats_tempdir, 'stats'), stats)
-    shutil.move(path.join(stats_tempdir, 'stats.genes.txt'), genes)
-    shutil.rmtree(stats_tempdir)
+    shell(shell_command)
