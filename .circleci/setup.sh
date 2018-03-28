44356c2ee170d0561a9fbab276170903995578a8
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-03-28 14:12:03 +0200

    Fix env creation.

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -6,4 +6,4 @@ source .circleci/common.sh
 if type conda > /dev/null; then exit 0; fi
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p miniconda
-conda env create --name snakemake snakemake
+conda create --name snakemake snakemake
