aaabbc502b3d09649b61147e6b057b7d0549ce72
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-05-17 14:20:59 +0200

    Add cairosvg wrapper.

---
+++ utils/cairosvg/wrapper.py
@@ -0,0 +1,12 @@
+__author__ = "Johannes Köster"
+__copyright__ = "Copyright 2017, Johannes Köster"
+__email__ = "johannes.koester@protonmail.com"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+shell("cairosvg -f {snakemake.wildcards.fmt} {snakemake.input} -o {snakemake.output}")
