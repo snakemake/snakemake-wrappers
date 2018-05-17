599af6b9c0e405b5065b6b2bdb022c13dce3962b
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-05-17 14:42:32 +0200

    Infer fmt from extension.

--- utils/cairosvg/wrapper.py
+++ utils/cairosvg/wrapper.py
@@ -3,10 +3,16 @@ __copyright__ = "Copyright 2017, Johannes Köster"
 __email__ = "johannes.koester@protonmail.com"
 __license__ = "MIT"
 
-
+import os
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-shell("cairosvg -f {snakemake.wildcards.fmt} {snakemake.input} -o {snakemake.output}")
+_, ext = os.path.splitext(snakemake.output[0])
+
+if ext not in (".png", ".pdf", ".ps", ".svg"):
+    raise ValueError("invalid file extension: '{}'".format(ext))
+fmt = ext[1:]
+
+shell("cairosvg -f {fmt} {snakemake.input[0]} -o {snakemake.output[0]}")
