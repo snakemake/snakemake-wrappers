733219a8e76115856a56b82823f9c6a5eb263624
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-06-06 14:18:42 +0200

    update blacklist

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 WRAPPER_DIR = os.path.dirname(BASE_DIR)
 OUTPUT_DIR = os.path.join(BASE_DIR, "wrappers")
 SCRIPTS = {"wrapper.py", "wrapper.R"}
-BLACKLIST = {".circleci", "wercker.yml", "docs", "__pycache__", "environment.yaml", ".git", ".gitignore", "README.md", ".cache", "__init__.py", "test.py"} | SCRIPTS
+BLACKLIST = {".circleci", "wercker.yml", "docs", "__pycache__", "environment.yaml", ".git", ".gitignore", "README.md", ".cache", "__init__.py", "test.py", ".pytest_cache"} | SCRIPTS
 TAG = subprocess.check_output(["git", "describe", "--tags"]).decode().strip()
 
 
