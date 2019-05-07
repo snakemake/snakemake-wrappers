72ff5cab3d52694adc3369c634cfe139a8e7ae57
tdayris <thibault.dayris@outlook.fr>
2019-05-07 12:39:06 +0200

    Pipe forgotten

--- bio/samtools/bam2fq/separate/wrapper.py
+++ bio/samtools/bam2fq/separate/wrapper.py
@@ -9,8 +9,8 @@ from snakemake.shell import shell
 
 prefix = os.path.splitext(snakemake.output[0])[0]
 threads = (
-    "" if snakemake.threads <= 1
-    else " -@ {} ".format(snakemake.threads - 1)
+    "" if snakemake.threads <= 2
+    else " -@ {} ".format(snakemake.threads - 2)
 )
 
 shell(
