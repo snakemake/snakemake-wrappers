594ed39cc3ac221d497115da6e3dbddf86e936bc
tdayris <thibault.dayris@outlook.fr>
2019-07-15 10:52:18 +0200

    Fixed: text formatting issue

--- bio/varscan/mpileup2snp/wrapper.py
+++ bio/varscan/mpileup2snp/wrapper.py
@@ -12,19 +12,18 @@ from snakemake.utils import makedirs
 # Gathering extra parameters and logging behaviour
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 extra = snakemake.params.get("extra", "")
-java_opt = snakemake.params.get("java_opt", "")
 
-pileup = (" <( cat {snakemake.input[0]} ) "
+pileup = (" cat {} ".format(snakemake.input[0])
           if not snakemake.input[0].endswith("gz")
-          else " <(zcat {snakemake.input[0]}) ")
+          else " zcat {} ".format(snakemake.input[0]))
 
 # Building output directories
 makedirs(op.dirname(snakemake.output[0]))
 
 shell(
     "varscan mpileup2snp "       # Tool and its subprocess
-    "{pileup} "                  # Path to pileup file
     "{extra} "                   # Extra parameters
-    " > {snakemake.output[0]} "  # Path to vcf file
+    "<( {pileup} ) "
+    "> {snakemake.output[0]} "  # Path to vcf file
     "{log}"                      # Logging behaviour
 )
