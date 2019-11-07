e68137ea2cada958a7bfd2a92e50aac638889d9b
Tessa Pierce <ntpierce@gmail.com>
2019-11-06 21:02:17 -0800

    working longorfs

--- bio/transdecoder/longorfs/wrapper.py
+++ bio/transdecoder/longorfs/wrapper.py
@@ -1,7 +1,7 @@
-"""Snakemake wrapper for Transdecoder."""
+"""Snakemake wrapper for Transdecoder"""
 
 __author__ = "N Tessa Pierce"
-__copyright__ = "Copyright 2010, Tessa Pierce"
+__copyright__ = "Copyright 2019, Tessa Pierce"
 __email__ = "ntpierce@gmail.com"
 __license__ = "MIT"
 
@@ -10,18 +10,15 @@ from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 
-# infer seqtype from input files:
-
-outdir = path.dirname(snakemake.output[0])
-assert "trinity" in outdir, "output directory name must contain 'trinity'"
-
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-shell(
-    " TransDecoder.LongOrfs -t {snakemake.input.fasta} --gene_trans_map {snakemake.input.tsv} {log}
-shell(
-    "Trinity {input_cmd} --CPU {snakemake.threads} "
-    " --max_memory {max_memory} --seqType {seqtype} "
-    " --output {outdir} {snakemake.params.extra} "
-    " {log}"
-)
+gtm_cmd = ""
+gtm = snakemake.input.get("gene_trans_map", "")
+if gtm:
+    gtm_cmd = " --gene_trans_map " + gtm
+
+output_dir = path.dirname(str(snakemake.output))
+
+# transdecoder fails if output already exists. No force option available
+shell("rm -rf {output_dir}")
+shell("TransDecoder.LongOrfs -t {snakemake.input.fasta} {gtm_cmd} {log}")
--- test.py
+++ test.py
@@ -559,3 +559,7 @@ def test_paladin_align():
     run("bio/paladin/align",
         ["snakemake", "paladin_mapped/a.bam", "--use-conda", "-F"])
 
+def test_transdecoder_longorfs():
+    run("bio/transdecoder/longorfs",
+        ["snakemake", "test.fa.transdecoder_dir/longest_orfs.pep", "--use-conda", "-F"])
+
