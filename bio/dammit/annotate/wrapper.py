88fa264724648d96104cca7ecc8a1acff54a1e42
Tessa Pierce <ntpierce@gmail.com>
2019-11-08 07:43:17 -0800

    format with black

--- bio/dammit/annotate/wrapper.py
+++ bio/dammit/annotate/wrapper.py
@@ -12,18 +12,24 @@ from snakemake.shell import shell
 db_dir = snakemake.params.get("database_dir")
 db_dir = path.abspath(db_dir)
 busco_dbs = snakemake.params.get("busco_dbs", [])
-db_extra = snakemake.params.get("db_extra", "--quick") # default so db install is *just* to check installs have already happened
+db_extra = snakemake.params.get(
+    "db_extra", "--quick"
+)  # default so db install is *just* to check installs have already happened
 
 if db_dir:
     db_dir = path.expanduser(db_dir)
-    db_cmd = ' --database-dir ' + db_dir # if db_dir is not None else ""
+    db_cmd = " --database-dir " + db_dir  # if db_dir is not None else ""
 else:
     db_cmd = ""
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 busco_dbs = [busco_dbs] if isinstance(busco_dbs, str) else busco_dbs
-busco_cmd = ' --busco-group ' + ' --busco-group '.join(busco_dbs) if busco_dbs is not None else ""
+busco_cmd = (
+    " --busco-group " + " --busco-group ".join(busco_dbs)
+    if busco_dbs is not None
+    else ""
+)
 
 # handle annotate params
 outdir = path.dirname(snakemake.output[0])
@@ -31,9 +37,9 @@ annot_extra = snakemake.params.get("annot_extra", "")
 
 # make informative output dir for *all* dammit output
 assembly_name = path.basename(str(snakemake.input.fasta))
-dammit_dir =  path.join(outdir, assembly_name + '.dammit')
-dammit_fasta = path.join(dammit_dir, assembly_name + '.dammit.fasta')
-dammit_gff3 = path.join(dammit_dir, assembly_name + '.dammit.gff3')
+dammit_dir = path.join(outdir, assembly_name + ".dammit")
+dammit_fasta = path.join(dammit_dir, assembly_name + ".dammit.fasta")
+dammit_gff3 = path.join(dammit_dir, assembly_name + ".dammit.gff3")
 
 ## Note: databases can be installed using the databases wrapper standalone. In that case, these install cmds
 # will just check that databases are properly installed. They will _not_ reinstall.
@@ -45,12 +51,15 @@ shell("dammit databases --install {db_cmd} {db_extra} {log}")
 
 # busco groups need to be installed separately
 for busco_grp in busco_dbs:
-    shell("dammit databases --install {db_cmd} --busco-group {busco_grp} {db_extra} {log}")
+    shell(
+        "dammit databases --install {db_cmd} --busco-group {busco_grp} {db_extra} {log}"
+    )
 
 # run annotation
-shell("dammit annotate {snakemake.input.fasta} {db_cmd} --no-rename --n_threads {snakemake.threads} --output-dir {dammit_dir} {annot_extra} {log}")
+shell(
+    "dammit annotate {snakemake.input.fasta} {db_cmd} --no-rename --n_threads {snakemake.threads} --output-dir {dammit_dir} {annot_extra} {log}"
+)
 
 # cp final dammit annot files to desired location / names
 shell("cp {dammit_fasta} {snakemake.output.fasta}")
 shell("cp {dammit_gff3} {snakemake.output.gff3}")
-
--- bio/dammit/databases/wrapper.py
+++ bio/dammit/databases/wrapper.py
@@ -13,25 +13,33 @@ db_dir = snakemake.params.get("database_dir")
 db_dir = path.abspath(db_dir)
 busco_dbs = snakemake.params.get("busco_dbs", [])
 db_extra = snakemake.params.get("db_extra", "")
-db_only = snakemake.params.get('db_install_only', False)
+db_only = snakemake.params.get("db_install_only", False)
 
 if db_dir:
     db_dir = path.expanduser(db_dir)
-    db_cmd = ' --database-dir ' + db_dir
+    db_cmd = " --database-dir " + db_dir
 else:
     db_cmd = ""
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 busco_dbs = [busco_dbs] if isinstance(busco_dbs, str) else busco_dbs
-busco_cmd = ' --busco-group ' + ' --busco-group '.join(busco_dbs) if busco_dbs is not None else ""
+busco_cmd = (
+    " --busco-group " + " --busco-group ".join(busco_dbs)
+    if busco_dbs is not None
+    else ""
+)
 
 # databases breaks with multiple threads https://github.com/dib-lab/dammit/issues/140
 # only a single busco group can be installed at once: https://github.com/dib-lab/dammit/issues/141
 
 # run installation of everything *except* busco groups
-shell("dammit databases --install {db_cmd} {db_extra} {log}") #--n_threads {snakemake.threads}
+shell(
+    "dammit databases --install {db_cmd} {db_extra} {log}"
+)  # --n_threads {snakemake.threads}
 
 # busco groups need to be installed separately
 for busco_grp in busco_dbs:
-    shell("dammit databases --install {db_cmd} --busco-group {busco_grp} {db_extra} {log}")
+    shell(
+        "dammit databases --install {db_cmd} --busco-group {busco_grp} {db_extra} {log}"
+    )
