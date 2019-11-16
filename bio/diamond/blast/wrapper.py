0d110909fffc98eb7a70edfc948043e4254a599f
Tessa Pierce <ntpierce@gmail.com>
2019-11-16 15:27:36 -0800

    finish blast wrapper

--- bio/diamond/blast/wrapper.py
+++ bio/diamond/blast/wrapper.py
@@ -9,11 +9,13 @@ from snakemake.shell import shell
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-pep_query=snakemake.input.get("pep")
-nucl_query=snakemake.input.get("nucl")
-#assert pep_query is not None or nucl_query is not None, "either pep_query or nucl_query are required as input"
+pep_query = snakemake.input.get("pep_query")
+nucl_query = snakemake.input.get("nucl_query")
+# assert pep_query is not None or nucl_query is not None, "either pep_query or nucl_query are required as input"
 queryfiles = [pep_query, nucl_query]
-assert (list(map(bool, queryfiles)).count(True) == 1), "one query is required: either a peptide query (pep_query) for diamond blastp OR a nucleotide query (nucl_query) for diamond blastx"
+assert (
+    list(map(bool, queryfiles)).count(True) == 1
+), "one query is required: either a peptide query (pep_query) for diamond blastp OR a nucleotide query (nucl_query) for diamond blastx"
 
 if pep_query:
     query = pep_query
@@ -22,25 +24,34 @@ elif nucl_query:
     query = nucl_query
     prog = "blastx"
 
-taxonlist = snakemake.input.get("taxonlist")
-#Comma-separated list of NCBI taxonomic IDs to filter the database by. Any taxonomic rank can be used, and only reference sequences matching one of the specified taxon ids will be searched against. Using this option requires setting the --taxonmap and --taxonnodes parameters for makedb.
+taxonlist = snakemake.params.get("taxonlist")
+# Comma-separated list of NCBI taxonomic IDs to filter the database by. Any taxonomic rank can be used, and only reference sequences matching one of the specified taxon ids will be searched against. Using this option requires setting the --taxonmap and --taxonnodes parameters for makedb.
+txlist_cmd = ""
 if taxonlist:
-    txlist_cmd = f" --taxonlist {taxonlist} "
+    taxonlist = str(taxonlist)
+    assert (taxonlist !="0" and taxonlist !="1"), "Option --taxonlist cannot be used with invalid argument (0 or 1)"
+    txlist_cmd = f" --taxonlist {str(taxonlist)} "
 
-#Set strand of query to align for translated searches. By default both strands are searched.
-strand=snakemake.params.get("strand", "both") #both, plus, minus
+# Set strand of query to align for translated searches. By default both strands are searched.
+strand = snakemake.params.get("strand", "both")  # both, plus, minus
 
-#The default mode is mainly designed for short read alignment, i.e. finding significant matches of >50 bits on 30-40aa fragments.
-sensitivity=snakemake.params.get("alignment_sensitivity") # optionally increase sensitivity: sensitive, more-sensitive
+# The default mode is mainly designed for short read alignment, i.e. finding significant matches of >50 bits on 30-40aa fragments.
+sensitivity = snakemake.params.get(
+    "alignment_sensitivity"
+)  # optionally increase sensitivity: sensitive, more-sensitive
 s_cmd = ""
 if sensitivity:
     if sensitivity == "sensitive":
-       # this mode is a lot more sensitive that the default and generally recommended for aligning longer sequences.
+        # this mode is a lot more sensitive that the default and generally recommended for aligning longer sequences.
         s_cmd = " --sensitive "
     elif sensitivity == "more-sensitive":
-       # more-sensitive: This mode provides some additional sensitivity compared to the sensitive mode.
+        # more-sensitive: This mode provides some additional sensitivity compared to the sensitive mode.
         s_cmd = " --more-sensitive "
     else:
-        raise ValueError("alignment sensitivity must be either 'sensitive' or 'more-sensitive'")
+        raise ValueError(
+            "alignment sensitivity must be either 'sensitive' or 'more-sensitive'"
+        )
 
-shell("diamond {prog} -db {snakemake.input.db} -q {query} -o {output} --threads {snakemake.threads} {txlist_cmd} {s_cmd} {extra} {log}")
+shell(
+    "diamond {prog} --db {snakemake.input.db} -q {query} -o {snakemake.output} --threads {snakemake.threads} {txlist_cmd} {s_cmd} {extra} {log}"
+)
--- bio/diamond/makedb/wrapper.py
+++ bio/diamond/makedb/wrapper.py
@@ -9,16 +9,18 @@ from snakemake.shell import shell
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-#Path to mapping file that maps NCBI protein accession numbers to taxon ids (gzip com- pressed). This parameter is optional and needs to be supplied in order to provide taxonomy features.
+# Path to mapping file that maps NCBI protein accession numbers to taxon ids (gzip com- pressed). This parameter is optional and needs to be supplied in order to provide taxonomy features.
 taxonmap = snakemake.input.get("taxonmap")
-txmap_cmd=""
+txmap_cmd = ""
 if taxonmap:
     txmap_cmd = f" --taxonmap {taxonmap} "
 
-#Path to the nodes.dmp file from the NCBI taxonomy. This parameter is optional and needs to be supplied in order to provide taxonomy features.
+# Path to the nodes.dmp file from the NCBI taxonomy. This parameter is optional and needs to be supplied in order to provide taxonomy features.
 taxonnodes = snakemake.input.get("taxonnodes")
-txnode_cmd=""
+txnode_cmd = ""
 if taxonnodes:
     txnode_cmd = f" --taxonnodes {taxonnodes} "
 
-shell("diamond makedb --in {snakemake.input.db} --db {snakemake.output} --threads {snakemake.threads} {txmap_cmd} {txnode_cmd} {extra} {log}")
+shell(
+    "diamond makedb --in {snakemake.input.db} --db {snakemake.output} --threads {snakemake.threads} {txmap_cmd} {txnode_cmd} {extra} {log}"
+)
