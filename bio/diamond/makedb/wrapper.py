32dc8e5ee046a3285a30154f06336b31c8aa2ee2
Tessa Pierce <ntpierce@gmail.com>
2019-11-16 08:57:15 -0800

    init dmnd

---
+++ bio/diamond/makedb/wrapper.py
@@ -0,0 +1,12 @@
+__author__ = "N. Tessa Pierce"
+__copyright__ = "Copyright 2019, N. Tessa Pierce"
+__email__ = "ntpierce@gmail.com"
+__license__ = "MIT"
+
+from os import path
+from snakemake.shell import shell
+
+extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+shell("diamond makedb --in {snakemake.input} --db {snakemake.output} {log}")
