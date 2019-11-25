c2967379b919213457c511ec6254787f3b1c5031
Johannes Köster <johannes.koester@uni-due.de>
2019-11-25 13:49:09 +0100

    fmt

--- bio/reference/dbsnp/wrapper.py
+++ bio/reference/dbsnp/wrapper.py
@@ -2,5 +2,9 @@ from ftplib import FTP
 
 with FTP("ftp.ncbi.nlm.nih.gov") as ftp, open(snakemake.output[0], "wb") as out:
     ftp.login()
-    ftp.storbinary("STOR snp/organisms/{params.organism}/VCF/All_{params.release}.vcf.gz".format(params=snakemake.params), out)
-
+    ftp.storbinary(
+        "STOR snp/organisms/{params.organism}/VCF/All_{params.release}.vcf.gz".format(
+            params=snakemake.params
+        ),
+        out,
+    )
