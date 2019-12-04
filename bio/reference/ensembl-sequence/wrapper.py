33596a4e0966f20a81dbb3943676d79b1e1073ca
Fritjof <f.lammers@dkfz.de>
2019-12-04 15:29:44 +0100

    added a checksum method

--- bio/reference/ensembl-sequence/wrapper.py
+++ bio/reference/ensembl-sequence/wrapper.py
@@ -4,6 +4,9 @@ __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
 
 from ftplib import FTP
+from io import StringIO
+from subprocess import run
+from os.path import basename
 
 species = snakemake.params.species.lower()
 release = snakemake.params.release
@@ -11,6 +14,27 @@ build = snakemake.params.build
 
 suffixes = ""
 datatype = snakemake.params.get("datatype", "")
+
+
+def checksum():
+    lines = r.getvalue().strip().split("\n")
+    for line in lines:
+        fields = line.strip().split()
+        cksum = int(fields[0])
+        filename = fields[2]
+        if filename == basename(snakemake.output[0]):
+            cksum_local = int(run(["sum", snakemake.output[0]], capture_output=True).stdout.strip().split()[0])
+            if cksum_local == cksum:
+                print("CHECKSUM OK: %s" % snakemake.output[0])
+                exit(0)
+            else:
+                print("CHECKSUM FAILED: %s" % snakemake.output[0])
+                exit(1)
+        else:
+            # print("No matching file for CHECKSUM test found")
+            continue
+
+
 if datatype == "dna":
     suffixes = ["dna.primary_assembly.fa.gz", "dna.toplevel.fa.gz"]
 elif datatype == "cdna":
@@ -43,6 +67,19 @@ with FTP("ftp.ensembl.org") as ftp, open(snakemake.output[0], "wb") as out:
 
         ftp.retrbinary("RETR " + url, out.write)
         success = True
+        # retrieve CHECKSUMS file
+        r = StringIO()
+        ftp.retrlines(
+            "RETR pub/release-{release}/fasta/{species}/{datatype}/CHECKSUMS".format(
+                release=release,
+                species=species,
+                datatype=datatype
+            ),
+            lambda s, w=r.write: w(s + '\n'),
+        )
+        # use StringIO instance for callback, add "\n" because ftplib.retrlines omits newlines
+
+    checksum()
 
 if not success:
     raise ValueError(
