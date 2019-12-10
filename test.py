3528240bcf4b6bca37711af02a3b44d21db4fec8
Roman Chernyatchik <chukovskij@gmail.com>
2019-12-10 14:24:52 +0300

    2bit related UCSC tools wrappers (#3)
    * 2bit related UCSC tools wrappers added (related to https://github.com/snakemake/snakemake-wrappers/issues/1)
    
    * file reformatted by black
    
    * let's rename to lowercase as sonarcloud suggests

--- test.py
+++ test.py
@@ -902,6 +902,26 @@ def test_plass_single():
     run("bio/plass", ["snakemake", "plass/prot_single.fasta", "--use-conda", "-F"])
 
 
+def test_ucsc_bedgraphtobigwig():
+    run("bio/ucsc/bedGraphToBigWig",
+        ["snakemake", "a.bw", "--use-conda", "-F"])
+
+
+def test_ucsc_fatotwobit():
+    run("bio/ucsc/faToTwoBit",
+        ["snakemake", "genome.2bit", "genome_gz.2bit", "--use-conda", "-F"])
+
+
+def test_ucsc_twobitinfo():
+    run("bio/ucsc/twoBitInfo",
+        ["snakemake", "genome.chrom.sizes", "--use-conda", "-F"])
+
+
+def test_ucsc_twobittofa():
+    run("bio/ucsc/twoBitToFa",
+        ["snakemake", "genome.fa", "--use-conda", "-F"])
+
+
 def test_ensembl_sequence():
     run("bio/reference/ensembl-sequence", ["snakemake", "--use-conda", "-F"])
 
