da9eab899061417d087ddcfae21bb9b1e93d3c9d
Johannes Köster <johannes.koester@uni-due.de>
2019-12-12 16:53:51 +0100

    fit variation file suffix

--- bio/reference/ensembl-variation/wrapper.py
+++ bio/reference/ensembl-variation/wrapper.py
@@ -14,14 +14,14 @@ log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 if type == "all":
     if species == "homo_sapiens":
         suffixes = [
-            "chr{}".format(chrom) for chrom in list(range(1, 23)) + ["X", "Y", "MT"]
+            "_chr{}".format(chrom) for chrom in list(range(1, 23)) + ["X", "Y", "MT"]
         ]
     else:
         suffixes = [""]
 elif type == "somatic":
-    suffixes = ["somatic"]
+    suffixes = ["_somatic"]
 elif type == "structural_variations":
-    suffixes = ["structural_variations"]
+    suffixes = ["_structural_variations"]
 else:
     raise ValueError(
         "Unsupported type {} (only all, somatic, structural_variations are allowed)".format(
@@ -30,7 +30,7 @@ else:
     )
 
 urls = [
-    "ftp://ftp.ensembl.org/pub/release-{release}/variation/vcf/{species}/{species}_{suffix}.vcf.gz".format(
+    "ftp://ftp.ensembl.org/pub/release-{release}/variation/vcf/{species}/{species}{suffix}.vcf.gz".format(
         release=release, species=species, suffix=suffix
     )
     for suffix in suffixes
