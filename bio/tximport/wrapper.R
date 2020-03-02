30d38e54f0222d9a6879a2ad892deb106059626d
tdayris <thibault.dayris@outlook.fr>
2020-03-02 17:25:38 +0100

    [fix] (tr2gene): importer error

--- bio/tximport/wrapper.R
+++ bio/tximport/wrapper.R
@@ -19,11 +19,11 @@ extra <- base::paste0('files = c("', samples_paths, '")');
 
 # Check if user provided optional transcript to gene table
 if ("tx_to_gene" %in% names(snakemake@input)) {
+  tx2gene <- readr::read_tsv(snakemake@input[["tx_to_gene"]]);
   extra <- base::paste(
-    extra,                                         # Foreward existing arguments
-    ', tx2gene = "',
-    snakemake@input[["tx_to_gene"]],               # Add tx2gene to parameters
-    '"'                                   # Field separator
+    extra,                 # Foreward existing arguments
+    ", tx2gene = ",
+    tx2gene,               # Add tx2gene to parameters
   );
 }
 
