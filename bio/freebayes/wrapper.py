09e77a9fc30b72d8fb594dfa5ac0917215c64585
Felix Mölder <felix.moelder@uni-due.de>
2020-04-18 10:42:47 +0200

    Reverted tab-parsing changes

--- bio/freebayes/wrapper.py
+++ bio/freebayes/wrapper.py
@@ -1,6 +1,6 @@
-__author__ = "Johannes Köster"
+__author__ = "Johannes Köster, Felix Mölder"
 __copyright__ = "Copyright 2017, Johannes Köster"
-__email__ = "johannes.koester@protonmail.com"
+__email__ = "johannes.koester@protonmail.com, felix.moelder@uni-due.de"
 __license__ = "MIT"
 
 
@@ -26,9 +26,9 @@ else:
     if snakemake.input.get("regions", ""):
         regions = (
             "<(bedtools intersect -a "
-            "<(sed 's/:\([0-9]*\)-\([0-9]*\)$/\\t\\1\\t\\2/' "
+            "<(sed \"s/:\([0-9]*\)-\([0-9]*\)$/$(printf '\\t')\\1$(printf '\\t')\\2/g\" "
             "{regions}) -b {snakemake.input.regions} | "
-            "sed 's/\\t\([0-9]*\)\\t\([0-9]*\)$/:\\1-\\2/')"
+            "sed \"s/$(printf '\\t')\([0-9]*\)$(printf '\\t')\([0-9]*\)$/:\\1-\\2/g\")"
         ).format(regions=regions, snakemake=snakemake)
     freebayes = ("freebayes-parallel {regions} {snakemake.threads}").format(
         snakemake=snakemake, regions=regions
