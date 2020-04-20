bc53e84627ed5859ef924080a68f1143480d8612
Felix Mölder <felix.moelder@uni-due.de>
2020-04-20 14:58:39 +0200

    Changed syntax

--- bio/freebayes/wrapper.py
+++ bio/freebayes/wrapper.py
@@ -27,9 +27,9 @@ else:
     if snakemake.input.get("regions", ""):
         regions = (
             "<(bedtools intersect -a "
-            "<(sed 's/:\([0-9]*\)-\([0-9]*\)$/\\t\\1\\t\\2/g' "
+            r"<(sed 's/:\([0-9]*\)-\([0-9]*\)$/\t\1\t\2/g' "
             "{regions}) -b {snakemake.input.regions} | "
-            'sed "s/\\t\([0-9]*\)\\t\([0-9]*\)$/:\\1-\\2/g")'
+            r"sed 's/\t\([0-9]*\)\t\([0-9]*\)$/:\1-\2/g')"
         ).format(regions=regions, snakemake=snakemake)
     freebayes = ("freebayes-parallel {regions} {snakemake.threads}").format(
         snakemake=snakemake, regions=regions
