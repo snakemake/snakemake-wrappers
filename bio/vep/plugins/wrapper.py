ff1cdaec295c8da8d0968a56348f64dccc87d53e
Johannes Köster <johannes.koester@uni-due.de>
2020-05-05 12:59:53 +0200

    download all plugins

--- bio/vep/plugins/wrapper.py
+++ bio/vep/plugins/wrapper.py
@@ -3,17 +3,31 @@ __copyright__ = "Copyright 2020, Johannes Köster"
 __email__ = "johannes.koester@uni-due.de"
 __license__ = "MIT"
 
+import sys
 from pathlib import Path
-from snakemake.shell import shell
+from urllib.request import urlretrieve
+from zipfile import ZipFile
+from tempfile import NamedTemporaryFile
 
-extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+if snakemake.log:
+    sys.stderr = open(snakemake.log[0], "w")
 
-plugins = ",".join(snakemake.params.plugins)
+outdir = Path(snakemake.output[0])
+outdir.mkdir()
 
-shell(
-    "vep_install --AUTO p "
-    "--PLUGINS {plugins} "
-    "--PLUGINSDIR {snakemake.output} "
-    "{log}"
-)
+with NamedTemporaryFile() as tmp:
+    urlretrieve("https://github.com/Ensembl/VEP_plugins/archive/release/{release}.zip".format(release=snakemake.params.release), tmp.name)
+    
+    with ZipFile(tmp.name) as f:
+        for member in f.infolist():
+            memberpath = Path(member.filename)
+            if len(memberpath.parts) == 1:
+                # skip root dir
+                continue
+            targetpath = outdir / memberpath.relative_to(memberpath.parts[0])
+            if member.is_dir():
+                targetpath.mkdir()
+            else:
+                with open(targetpath, "wb") as out:
+                    out.write(f.read(member.filename))
+
