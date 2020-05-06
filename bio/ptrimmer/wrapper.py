47286440e52ccb92c2fbb500cd875b881af96b78
Felix Mölder <felix.moelder@uni-due.de>
2020-05-06 12:55:57 +0200

    Cleanup

--- bio/ptrimmer/wrapper.py
+++ bio/ptrimmer/wrapper.py
@@ -25,13 +25,14 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 ptrimmer_params = "-s {mode} {reads} -a {primers} -o {out}".format(mode=seqmode, reads=reads, primers=primers, out=outdir)
 
-process_r1 = "mv {out}/{prefix}_trim_R1.fq > {out}".format(out=outdir, prefix=read_prefix, out=intermediate_output_r1)
+process_r1 = "mv {outdir}/{prefix}_trim_R1.fq {out}".format(outdir=outdir, prefix=read_prefix, out=intermediate_output_r1)
 if snakemake.output.r1.endswith(".gz"):
-    process_r1 = "{process_r1} && gzip -9 -c {intermediate_out}> {out}".format(process_r1=process_r1,intermediate_out=intermediate_output_r1, out=snakemake.output.r1)
+    process_r1 = "{process_r1} && gzip -9 {out}".format(process_r1=process_r1,out=intermediate_output_r1)
 
+process_r2 = ""
 if snakemake.input.get("r2", ""):
-    precess_r2 = "&& mv {out}/{prefix}_trim_R2.fq > {out}".format(out=outdir, prefix=read_prefix, out=intermediate_output_r2)
+    process_r2 = "&& mv {outdir}/{prefix}_trim_R2.fq {out}".format(outdir=outdir, prefix=read_prefix, out=intermediate_output_r2)
     if snakemake.output.r2.endswith(".gz"):
-        process_r2 = "{process_r2} && gzip -9 -c {intermediate_out}> {out}".format(process_r2=process_r2,intermediate_out=intermediate_output_r2, out=snakemake.output.r2)
+        process_r2 = "{process_r2} && gzip -9 {out}".format(process_r2=process_r2, out=intermediate_output_r2)
 
 shell("ptrimmer {ptrimmer_params} && {process_r1} {process_r2} {log}")
