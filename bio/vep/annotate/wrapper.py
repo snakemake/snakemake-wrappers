745596b7d10bcda6d77487916bf0f9eed60ca4de
Johannes Köster <johannes.koester@uni-due.de>
2020-05-06 17:54:02 +0200

    fix vep annotate

--- bio/vep/annotate/wrapper.py
+++ bio/vep/annotate/wrapper.py
@@ -7,11 +7,12 @@ from pathlib import Path
 from snakemake.shell import shell
 
 
-def get_first_child_dir(path):
-    for p in path.iterdir():
-        if not p.is_dir():
-            continue
-        return p
+def get_only_child_dir(path):
+    children = [child for child in path.iterdir() if child.is_dir()]
+    assert (
+        len(children) == 1
+    ), "Invalid VEP cache directory, only a single entry is allowed, make sure that cache was created with the snakemake VEP cache wrapper"
+    return children[0]
 
 
 extra = snakemake.params.get("extra", "")
@@ -22,7 +23,7 @@ stats = snakemake.output.stats
 cache = snakemake.input.cache
 plugins = snakemake.input.plugins
 
-entrypath = get_first_child_dir(get_first_child_dir(Path(cache)))
+entrypath = get_only_child_dir(get_only_child_dir(Path(cache)))
 species = entrypath.parent.name
 release, build = entrypath.name.split("_")
 
