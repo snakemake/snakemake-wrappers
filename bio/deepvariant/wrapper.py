a97bfea4224ec13f1646cfbe9c4d6522250d4d43
Tetsuro Hisayoshi <hisayoshi0530@gmail.com>
2020-06-16 21:35:47 +0900

    create a temporary directory by python

--- bio/deepvariant/wrapper.py
+++ bio/deepvariant/wrapper.py
@@ -4,37 +4,37 @@ __email__ = "hisayoshi0530@gmail.com"
 __license__ = "MIT"
 
 import os
+import tempfile
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
 
-log_dir = os.path.dirname(str(snakemake.log))
-output_dir = os.path.dirname(str(snakemake.output))
+log_dir = os.path.dirname(snakemake.log[0])
+output_dir = os.path.dirname(snakemake.output[0])
 
 # sample basename
-basename = os.path.splitext(os.path.basename(str(snakemake.input.bam)))[0]
+basename = os.path.splitext(os.path.basename(snakemake.input.bam[0]))[0]
 
 
-shell(
-    "(mkdir -p {output_dir}/{basename} \n"
-    "dv_make_examples.py "
-    "--cores {snakemake.threads} "
-    "--ref {snakemake.input.ref} "
-    "--reads {snakemake.input.bam} "
-    "--sample {basename} "
-    "--examples {output_dir}/{basename} "
-    "--logdir {log_dir} "
-    "{extra} \n"
-    "dv_call_variants.py "
-    "--cores {snakemake.threads} "
-    "--outfile {output_dir}/{basename}/{basename}.tmp "
-    "--sample {basename} "
-    "--examples {output_dir}/{basename} "
-    "--model {snakemake.params.model} \n"
-    "dv_postprocess_variants.py "
-    "--ref {snakemake.input.ref} "
-    "--infile {output_dir}/{basename}/{basename}.tmp "
-    "--outfile {snakemake.output.vcf} \n"
-    "rm -rf {output_dir}/{basename} ) {log}"
-)
+with tempfile.TemporaryDirectory() as tmp_dir:
+    shell(
+        "(dv_make_examples.py "
+        "--cores {snakemake.threads} "
+        "--ref {snakemake.input.ref} "
+        "--reads {snakemake.input.bam} "
+        "--sample {basename} "
+        "--examples {tmp_dir} "
+        "--logdir {log_dir} "
+        "{extra} \n"
+        "dv_call_variants.py "
+        "--cores {snakemake.threads} "
+        "--outfile {tmp_dir}/{basename}.tmp "
+        "--sample {basename} "
+        "--examples {tmp_dir} "
+        "--model {snakemake.params.model} \n"
+        "dv_postprocess_variants.py "
+        "--ref {snakemake.input.ref} "
+        "--infile {tmp_dir}/{basename}.tmp "
+        "--outfile {snakemake.output.vcf} ) {log}"
+    )
