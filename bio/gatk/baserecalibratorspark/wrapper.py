024d30761bd96021d913323cf667942a83d7b8af
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-06-24 21:11:52 +0000

    seperate apply and recalibrate

--- bio/gatk/baserecalibratorspark/wrapper.py
+++ bio/gatk/baserecalibratorspark/wrapper.py
@@ -4,7 +4,6 @@ __email__ = "christopher.schroeder@tu-dortmund.de"
 __license__ = "MIT"
 
 
-from tempfile import TemporaryDirectory
 import os
 
 from snakemake.shell import shell
@@ -17,24 +16,15 @@ spark_master = snakemake.params.get(
 spark_extra = snakemake.params.get("spark_extra", "")
 java_opts = snakemake.params.get("java_opts", "")
 
-with TemporaryDirectory() as tmpdir:
-    recal_table = os.path.join(tmpdir, "recal_table.grp")
-    log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-    known = snakemake.input.get("known", "")
-    if known:
-        known = "--known-sites {}".format(known)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+known = snakemake.input.get("known", "")
+if known:
+    known = "--known-sites {}".format(known)
 
-    shell(
-        "gatk --java-options '{java_opts}' BaseRecalibratorSpark {extra} "
-        "-R {snakemake.input.ref} -I {snakemake.input.bam} "
-        "-O {recal_table} {known} "
-        "-- --spark-runner {spark_runner} --spark-master {spark_master} {spark_extra} "
-        "{log}"
-    )
-
-    log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
-    shell(
-        "gatk --java-options '{java_opts}' ApplyBQSR -R {snakemake.input.ref} -I {snakemake.input.bam} "
-        "--bqsr-recal-file {recal_table} "
-        "-O {snakemake.output.bam} {log}"
-    )
+shell(
+    "gatk --java-options '{java_opts}' BaseRecalibratorSpark {extra} "
+    "-R {snakemake.input.ref} -I {snakemake.input.bam} "
+    "-O {snakemake.output.recal_table} {known} "
+    "-- --spark-runner {spark_runner} --spark-master {spark_master} {spark_extra} "
+    "{log}"
+)
