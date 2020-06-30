19e7ad691928b0f2ae60274362b51dce4fdec7a6
AntonieV <antonie.vietor@gmx.de>
2020-06-30 18:20:37 +0200

    integration of the -ibam option permanently in the bam files part of the wrapper

--- bio/bedtools/genomecov/wrapper.py
+++ bio/bedtools/genomecov/wrapper.py
@@ -12,8 +12,7 @@ genome = ""
 input_file = ""
 
 if (os.path.splitext(snakemake.input[0])[-1]) == ".bam":
-    if "-ibam" not in (snakemake.params):
-        input_file = "-ibam " + snakemake.input[0]
+    input_file = "-ibam " + snakemake.input[0]
 
 if len(snakemake.input) > 1:
     if (os.path.splitext(snakemake.input[0])[-1]) == ".bed":
