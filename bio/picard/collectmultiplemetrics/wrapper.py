97ba092f2d503fc0bf93067d1fbb659a79af4a83
AntonieV <antonie.vietor@gmx.de>
2020-07-02 16:41:24 +0200

    Update for wrapper Picard-CollectMultipleMetrics: extensions and their tools created as dict in wrapper.py

--- bio/picard/collectmultiplemetrics/wrapper.py
+++ bio/picard/collectmultiplemetrics/wrapper.py
@@ -12,56 +12,45 @@ res = snakemake.resources.get("mem_gb", "3")
 if not res or res is None:
     res = 3
 
+exts_to_prog = {
+    ".alignment_summary_metrics": "CollectAlignmentSummaryMetrics",
+    ".insert_size_metrics": "CollectInsertSizeMetrics",
+    ".insert_size_histogram.pdf": "CollectInsertSizeMetrics",
+    ".quality_distribution_metrics": "QualityScoreDistribution",
+    ".quality_distribution.pdf": "QualityScoreDistribution",
+    ".quality_by_cycle_metrics": "MeanQualityByCycle",
+    ".quality_by_cycle.pdf": "MeanQualityByCycle",
+    ".base_distribution_by_cycle_metrics": "CollectBaseDistributionByCycle",
+    ".base_distribution_by_cycle.pdf": "CollectBaseDistributionByCycle",
+    ".gc_bias.detail_metrics": "CollectGcBiasMetrics",
+    ".gc_bias.summary_metrics": "CollectGcBiasMetrics",
+    ".gc_bias.pdf": "CollectGcBiasMetrics",
+    ".rna_metrics": "RnaSeqMetrics",
+    ".bait_bias_detail_metrics": "CollectSequencingArtifactMetrics",
+    ".bait_bias_summary_metrics": "CollectSequencingArtifactMetrics",
+    ".error_summary_metrics": "CollectSequencingArtifactMetrics",
+    ".pre_adapter_detail_metrics": "CollectSequencingArtifactMetrics",
+    ".pre_adapter_summary_metrics": "CollectSequencingArtifactMetrics",
+    ".quality_yield_metrics": "CollectQualityYieldMetrics",
+}
 progs = set()
-extensions = set()
 
 for file in snakemake.output:
-    if "alignment_summary" in file:
-        progs.add("CollectAlignmentSummaryMetrics ")
-        extensions.add(".alignment_summary_metrics")
-    elif "insert_size" in file:
-        progs.add("CollectInsertSizeMetrics ")
-        extensions.add(".insert_size_metrics")
-        extensions.add(".insert_size_histogram.pdf")
-    elif "quality_distribution" in file:
-        progs.add("QualityScoreDistribution ")
-        extensions.add(".quality_distribution_metrics")
-        extensions.add(".quality_distribution.pdf")
-    elif "quality_by_cycle" in file:
-        progs.add("MeanQualityByCycle ")
-        extensions.add(".quality_by_cycle_metrics")
-        extensions.add(".quality_by_cycle.pdf")
-    elif "base_distribution_by_cycle" in file:
-        progs.add("CollectBaseDistributionByCycle ")
-        extensions.add(".base_distribution_by_cycle_metrics")
-        extensions.add(".base_distribution_by_cycle.pdf")
-    elif "gc_bias" in file:
-        progs.add("CollectGcBiasMetrics ")
-        extensions.add(".gc_bias.detail_metrics")
-        extensions.add(".gc_bias.summary_metrics")
-        extensions.add(".gc_bias.pdf")
-    elif "rna_metrics" in file:
-        progs.add("RnaSeqMetrics ")
-        extensions.add(".rna_metrics")
-    elif "bait_bias" in file or "error_summary" in file or "pre_adapter" in file:
-        progs.add("CollectSequencingArtifactMetrics ")
-        extensions.add(".bait_bias_detail_metrics")
-        extensions.add(".bait_bias_summary_metrics")
-        extensions.add(".error_summary_metrics")
-        extensions.add(".pre_adapter_detail_metrics")
-        extensions.add(".pre_adapter_summary_metrics")
-    elif "quality_yield" in file:
-        progs.add("CollectQualityYieldMetrics ")
-        extensions.add(".quality_yield_metrics")
-    else:
+    matched = False
+    for ext in exts_to_prog:
+        if file.endswith(ext):
+            progs.add(exts_to_prog[ext])
+            matched = True
+    if not matched:
         sys.exit(
             "Unknown type of metrics file requested, for possible metrics files, see https://snakemake-wrappers.readthedocs.io/en/stable/wrappers/picard/collectmultiplemetrics.html"
         )
-programs = " PROGRAM=" + "PROGRAM=".join(progs)
+
+programs = " PROGRAM=" + " PROGRAM=".join(progs)
 
 out = str(snakemake.wildcards.sample)  # as default
 output_file = str(snakemake.output[0])
-for ext in extensions:
+for ext in exts_to_prog:
     if ext in output_file:
         if output_file.endswith(ext):
             out = output_file[: -len(ext)]
