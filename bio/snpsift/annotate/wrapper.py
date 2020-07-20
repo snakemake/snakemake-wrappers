640656f37d2d1a5e50b0c6746a5359a72e80f48f
tdayris <thibault.dayris@gustaveroussy.fr>
2020-07-20 11:24:52 +0200

    [dev] (SnpSift): Fit SNakemakeWrappers requirements

--- bio/snpsift/annotate/wrapper.py
+++ bio/snpsift/annotate/wrapper.py
@@ -5,20 +5,42 @@ __copyright__ = "Copyright 2020, Dayris Thibault"
 __email__ = "thibault.dayris@gustaveroussy.fr"
 __license__ = "MIT"
 
-from os.path import dirname
 from snakemake.shell import shell
-from snakemake.utils import makedirs
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
-
 extra = snakemake.params.get("extra", "")
-makedirs(dirname(snakemake.output.vcf))
+min_threads = 1
+
+incall = snakemake.input["call"]
+if snakemake.input["call"].endswith("bcf"):
+    min_threads += 1
+    incall = "< <(bcftools view {})".format(incall)
+elif snakemake.input["call"].endswith("gz"):
+    min_threads += 1
+    incall = "< <(gunzip -c {})".format(incall)
+
+outcall = snakemake.output["call"]
+if snakemake.output["call"].endswith("gz"):
+    min_threads += 1
+    outcall = "| gzip -c > {}".format(outcall)
+elif snakemake.output["call"].endswith("bcf"):
+    min_threads += 1
+    outcall = "| bcftools view > {}".format(outcall)
+else:
+    outcall = "> {}".format(outcall)
+
+if snakemake.threads < min_threads:
+    raise ValueError(
+        "At least {} threads required, {} provided".format(
+            min_threads, snakemake.threads
+        )
+    )
 
 shell(
     "SnpSift annotate"  # Tool and its subcommand
     " {extra}"  # Extra parameters
     " {snakemake.input.database}"  # Path to annotation vcf file
-    " {snakemake.input.vcf}"  # Path to input vcf file
-    " > {snakemake.output.vcf}"  # Path to output vcf file
+    " {incall} "  # Path to input vcf file
+    " {outcall} "  # Path to output vcf file
     " {log}"  # Logging behaviour
 )
