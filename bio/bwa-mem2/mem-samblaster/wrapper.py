0000853fecc5e86b3ce6b3bd5b2c89644679e81a
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-07-27 18:23:05 +0000

    remove unused chars

--- bio/bwa-mem2/mem-samblaster/wrapper.py
+++ bio/bwa-mem2/mem-samblaster/wrapper.py
@@ -21,7 +21,7 @@ if not isinstance(snakemake.input.reads, str) and len(snakemake.input.reads) not
     1,
     2,
 }:
-    raise ValueError("input must have 1 (single-end) or " "2 (paired-end) elements")
+    raise ValueError("input must have 1 (single-end) or 2 (paired-end) elements")
 
 shell(
     "(bwa-mem2 mem"
