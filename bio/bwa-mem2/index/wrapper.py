5707a4bf1185c071a8c97869963c4a95b6b15faa
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-07-27 12:20:36 +0000

    index and minor changes

---
+++ bio/bwa-mem2/index/wrapper.py
@@ -0,0 +1,32 @@
+__author__ = "Christopher Schröder, Patrik Smeds"
+__copyright__ = "Copyright 2020, Christopher Schröder, Patrik Smeds"
+__email__ = "christopher.schroeder@tu-dortmund.de, patrik.smeds@gmail.com"
+__license__ = "MIT"
+
+from os import path
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Check inputs/arguments.
+if len(snakemake.input) == 0:
+    raise ValueError("A reference genome has to be provided!")
+elif len(snakemake.input) > 1:
+    raise ValueError("Only one reference genome can be inputed!")
+
+# Prefix that should be used for the database
+prefix = snakemake.params.get("prefix", "")
+
+if len(prefix) > 0:
+    prefix = "-p " + prefix
+
+# Contrunction algorithm that will be used to build the database, default is bwtsw
+construction_algorithm = snakemake.params.get("algorithm", "")
+
+if len(construction_algorithm) != 0:
+    construction_algorithm = "-a " + construction_algorithm
+
+shell(
+    "bwa-mem2 index" " {prefix}" " {construction_algorithm}" " {snakemake.input[0]}" " {log}"
+)
