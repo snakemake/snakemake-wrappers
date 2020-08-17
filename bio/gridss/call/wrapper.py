7a474bc89150f6998ef0532645746f965b72dfd0
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-08-17 13:37:54 +0000

    setupreference test

---
+++ bio/gridss/call/wrapper.py
@@ -0,0 +1,43 @@
+"""Snakemake wrapper for gridss call"""
+
+__author__ = "Christopher Schröder"
+__copyright__ = "Copyright 2020, Christopher Schröder"
+__email__ = "christopher.schroede@tu-dortmund.de"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+from os import path
+
+# Creating log
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+# Placeholder for optional parameters
+extra = snakemake.params.get("extra", "")
+
+# Check inputs/arguments.
+reference = snakemake.input.get("reference")
+dictionary = snakemake.input.get("dictionary")
+if not snakemake.params.workingdir: 
+    raise ValueError("Please set params.workingdir to provide a working directory.")
+
+if not snakemake.input.reference:
+    raise ValueError("Please set input.reference to provide reference genome.")
+
+for ending in (".amb", ".ann", ".bwt", ".pac", ".sa"):
+    if not path.exists(f"{reference}{ending}"):
+        raise ValueError(f"{reference}{ending} missing. Please make sure the reference was properly indexed by bwa.")
+
+if not path.exists(f"{dictionary}"):
+    raise ValueError(f"{reference}{ending} missing. Please make sure the reference dictionary was properly created. This can be accomplished for example by CreateSequenceDictionary.jar from Picard")
+
+shell(
+    "export JAVA_OPTS='-XX:ActiveProcessorCount=8' & "
+    "gridss -s call " # Tool
+    "--reference {reference} " # Reference
+    "--threads {snakemake.threads} " # Threads
+    "--workingdir {snakemake.params.workingdir} " # Working directory
+    "--assembly {snakemake.input.assembly} " # Assembly input from gridss assemble
+    "--output {snakemake.output} " # Assembly vcf
+    "{snakemake.input.bams} "
+    "{extra}"
+)
---
+++ bio/gridss/setupreference/wrapper.py
@@ -0,0 +1,34 @@
+"""Snakemake wrapper for gridss setupreference"""
+
+__author__ = "Christopher Schröder"
+__copyright__ = "Copyright 2020, Christopher Schröder"
+__email__ = "christopher.schroede@tu-dortmund.de"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+from os import path
+
+# Creating log
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+# Placeholder for optional parameters
+extra = snakemake.params.get("extra", "")
+
+# Check inputs/arguments.
+reference = snakemake.input.get("reference")
+dictionary = snakemake.input.get("dictionary")
+if not snakemake.input.reference:
+    raise ValueError("A reference genome has to be provided!")
+
+for ending in (".amb", ".ann", ".bwt", ".pac", ".sa"):
+    if not path.exists(f"{reference}{ending}"):
+        raise ValueError(f"{reference}{ending} missing. Please make sure the reference was properly indexed by bwa.")
+
+if not path.exists(f"{dictionary}"):
+    raise ValueError(f"{reference}{ending} missing. Please make sure the reference dictionary was properly created. This can be accomplished for example by CreateSequenceDictionary.jar from Picard")
+
+shell(
+    "(gridss -s setupreference " # Tool
+    "--reference {reference} " # Reference
+    "{extra}) {log}"
+)
