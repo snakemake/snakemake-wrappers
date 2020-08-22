8046d8173c34eadb97cb2eda386817453eabe5c7
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-08-22 11:05:54 +0200

    Update bio/gridss/setupreference/wrapper.py
    Co-authored-by: Jan Forster <31272349+jafors@users.noreply.github.com>

--- bio/gridss/setupreference/wrapper.py
+++ bio/gridss/setupreference/wrapper.py
@@ -21,7 +21,7 @@ if not snakemake.input.reference:
     raise ValueError("A reference genome has to be provided!")
 
 for ending in (".amb", ".ann", ".bwt", ".pac", ".sa"):
-    if not path.exists(f"{reference}{ending}"):
+    if not path.exists("{}{}".format(reference, ending)):
         raise ValueError(
             "{reference}{ending} missing. Please make sure the reference was properly indexed by bwa.".format(
                 reference=reference, ending=ending
