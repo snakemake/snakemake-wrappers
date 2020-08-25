29266d82cd8a8847ba12b0e6572fff923c3faf01
kiesers <silas.kieser@gmail.com>
2020-08-25 13:30:41 +0200

    black formated

--- bio/eggNOGmapper/annotate_hits_table/wrapper.py
+++ bio/eggNOGmapper/annotate_hits_table/wrapper.py
@@ -15,29 +15,29 @@ log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 data_base_dir = os.path.dirname(snakemake.input.eggnog_db)
 
-out_extension= '.emapper.annotations'
+out_extension = ".emapper.annotations"
 
-assert snakemake.output[0].endswith(out_extension), f"expect first outptu file to end with {out_extension}"
-output_prefix= snakemake.output[0].replace(out_extension,'')
+assert snakemake.output[0].endswith(
+    out_extension
+), f"expect first outptu file to end with {out_extension}"
+output_prefix = snakemake.output[0].replace(out_extension, "")
 
 
-if hasattr(params,'use_virtual_disk') and params['use_virtual_disk']:
+if hasattr(params, "use_virtual_disk") and params["use_virtual_disk"]:
 
-
-    virtual_disk=snakemake.config.get('virtual_disk','/dev/shm/')
+    virtual_disk = snakemake.config.get("virtual_disk", "/dev/shm/")
     assert os.path.exists(virtual_disk), "Virtual disk {virtual_disk} doesn't exits"
-
 
     shell("cp {snakemake.input.eggnog_db} {virtual_disk}/eggnog.db 2> {log}")
 
-    data_base_dir= virtual_disk
+    data_base_dir = virtual_disk
 
 
 shell(
-      " emapper.py --annotate_hits_table"
-      " {snakemake.input.seed} "
-      " {extra} "
-      " -o {output_prefix} "
-      " --cpu {snakemake.threads} "
-      "--data_dir {data_base_dir} 2> {log}"
-      )
+    " emapper.py --annotate_hits_table"
+    " {snakemake.input.seed} "
+    " {extra} "
+    " -o {output_prefix} "
+    " --cpu {snakemake.threads} "
+    "--data_dir {data_base_dir} 2> {log}"
+)
