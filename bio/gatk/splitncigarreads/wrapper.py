855d57c7a63b2acf6b132fe9c1247c9acc7d80e1
tdayris <tdayris@gustaveroussy.fr>
2020-09-22 09:19:12 +0200

    [dev] (splitncigarreads): Automatic Xmx fill

--- bio/gatk/splitncigarreads/wrapper.py
+++ bio/gatk/splitncigarreads/wrapper.py
@@ -9,6 +9,8 @@ from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 java_opts = snakemake.params.get("java_opts", "")
+if "mem_mb" in snakemake.resources.keys() and not "-Xmx" in java_opts:
+    java_opts += " -Xmx{}M".format(snakemake.resources["mem_mb"])
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 shell(
