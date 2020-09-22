321a5dabb6636b3f6293db16c1e95ff802666846
tdayris <tdayris@gustaveroussy.fr>
2020-09-22 11:07:09 +0200

    [dev] (Mutect2): Java options added, with automatic Xmx fill

--- bio/gatk/mutect/wrapper.py
+++ bio/gatk/mutect/wrapper.py
@@ -11,9 +11,12 @@ from snakemake.utils import makedirs
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 extra = snakemake.params.get("extra", "")
+java_opts = snakemake.params.get("java_opts", "")
+if "mem_mb" in snakemake.resources.keys() and "-Xmx" not in java_opts:
+    java_opts += " -Xmx{}M".format(snakemake.resources["mem_mb"])
 
 shell(
-    "gatk Mutect2 "  # Tool and its subprocess
+    "gatk --java-options '{java_opts}' Mutect2 "  # Tool and its subprocess
     "--input {snakemake.input.map} "  # Path to input mapping file
     "--output {snakemake.output.vcf} "  # Path to output vcf file
     "--reference {snakemake.input.fasta} "  # Path to reference fasta file
