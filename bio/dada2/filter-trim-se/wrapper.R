4b1a6a97af8d35e6309308fafde3a2f67af5c153
cpauvert <cpauvert@users.noreply.github.com>
2020-10-02 17:49:27 +0200

    Added dada2 wrapper for filterAndTrim (se version)

---
+++ bio/dada2/filter-trim-se/wrapper.R
@@ -0,0 +1,35 @@
+# __author__ = "Charlie Pauvert"
+# __copyright__ = "Copyright 2020, Charlie Pauvert"
+# __email__ = "cpauvert@protonmail.com"
+# __license__ = "MIT"
+
+# Snakemake wrapper for filtering single-end reads using dada2 filterAndTrim function."
+
+library(dada2, quietly=TRUE)
+
+# Sink the stderr and stdout to the snakemake log file
+log.file<-file(snakemake@log[[1]],open="wt")
+sink(log.file)
+sink(log.file,type="message")
+
+# Prepare arguments (no matter the order)
+args<-list(
+        fwd = snakemake@input[["fwd"]],
+        filt = snakemake@output[["filt"]],
+        multithread=snakemake@threads
+)
+
+# Check if extra params are passed
+if(length(snakemake@params[[1]]) > 1){
+# Add them to the list of arguments
+   args<-c(args,snakemake@params[[1]])
+}
+
+# Call the function with arguments
+filt.stats<-do.call(filterAndTrim, args)
+
+# Write processed reads report
+write.table(filt.stats, snakemake@output[["stats"]], sep="\t", quote=F)
+# Close the connection for the log file
+sink(type="message")
+sink()
