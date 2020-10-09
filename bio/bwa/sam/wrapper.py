ea03f2e2424ce624a66ca5f926d549fc0044ae03
fgvieira <fgarrettvieira@gmail.com>
2020-10-09 15:07:05 +0200

    Added wrapper for both bwa samse and sampe

---
+++ bio/bwa/sam/wrapper.py
@@ -0,0 +1,71 @@
+"""Snakemake wrapper for both bwa samse and sampe."""
+
+__author__ = "Julian de Ruiter"
+__copyright__ = "Copyright 2017, Julian de Ruiter"
+__email__ = "julianderuiter@gmail.com"
+__license__ = "MIT"
+
+
+from os import path
+
+from snakemake.shell import shell
+
+
+# Check inputs.
+fastq = snakemake.input.fastq if isinstance(snakemake.input.fastq, list) else [snakemake.input.fastq]
+sai = snakemake.input.sai if isinstance(snakemake.input.sai, list) else [snakemake.input.sai]
+if len(fastq) == 1 and len(sai) == 1:
+    alg = "samse"
+elif len(fastq) == 2 and len(sai) == 2:
+    alg = "sampe"
+else:
+    raise ValueError("input.fastq and input.sai must have 1 or 2 elements each")
+
+# Extract arguments.
+extra = snakemake.params.get("extra", "")
+
+sort = snakemake.params.get("sort", "none")
+sort_order = snakemake.params.get("sort_order", "coordinate")
+sort_extra = snakemake.params.get("sort_extra", "")
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+# Determine which pipe command to use for converting to bam or sorting.
+if sort == "none":
+
+    # Simply convert to bam using samtools view.
+    pipe_cmd = "samtools view -Sbh -o {snakemake.output[0]} -"
+
+elif sort == "samtools":
+
+    # Sort alignments using samtools sort.
+    pipe_cmd = "samtools sort {sort_extra} -o {snakemake.output[0]} -"
+
+    # Add name flag if needed.
+    if sort_order == "queryname":
+        sort_extra += " -n"
+
+    # Use prefix for temp.
+    prefix = path.splitext(snakemake.output[0])[0]
+    sort_extra += " -T " + prefix + ".tmp"
+
+elif sort == "picard":
+
+    # Sort alignments using picard SortSam.
+    pipe_cmd = (
+        "picard SortSam {sort_extra} INPUT=/dev/stdin"
+        " OUTPUT={snakemake.output[0]} SORT_ORDER={sort_order}"
+    )
+
+else:
+    raise ValueError("Unexpected value for params.sort ({})".format(sort))
+
+# Run command.
+shell(
+    "(bwa {alg}"
+    " {extra}"
+    " {snakemake.params.index}"
+    " {snakemake.input.sai}"
+    " {snakemake.input.fastq}"
+    " | " + pipe_cmd + ") {log}"
+)
