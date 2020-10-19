8e4a9f3b4dc707ad255fe6a06931e78cdafba79b
tdayris <tdayris@gustaveroussy.fr>
2020-10-19 08:21:47 +0200

    [dev] (Jannovar): Automatic Xmx fill

--- bio/jannovar/wrapper.py
+++ bio/jannovar/wrapper.py
@@ -11,6 +11,8 @@ shell.executable("bash")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 extra = snakemake.params.get("extra", "")
+if "mem_mb" in snakemake.resources.keys() and "-Xmx" not in extra:
+    extra += " -Xmx{}M".format(snakemake.resources["mem_mb"])
 
 pedigree = snakemake.input.get("pedigree", "")
 if pedigree:
