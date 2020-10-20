f176831f7a5dbe0ad45d10b5f03a9217b3f934b5
tdayris <tdayris@gustaveroussy.fr>
2020-10-20 10:33:17 +0200

    [dev] (snpeff): Update with comments from @dlaehnemann and java temp

--- bio/snpeff/annotate/wrapper.py
+++ bio/snpeff/annotate/wrapper.py
@@ -25,8 +25,23 @@ if incalls.endswith(".bcf"):
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 extra = snakemake.params.get("extra", "")
+java_opts = ""
+# Getting memory in megabytes, if java opts is not filled with -Xmx parameter
+# By doing so, backward compatibility is preserved
 if "mem_mb" in snakemake.resources.keys() and "-Xmx" not in extra:
-    extra += " -Xmx{}M".format(snakemake.resources["mem_mb"])
+    java_opts += " -Xmx{}M".format(snakemake.resources["mem_mb"])
+
+# Getting memory in gigabytes, for user convenience. Please prefer the use
+# of mem_mb over mem_gb as advised in documentation.
+elif "mem_gb" in snakemake.resources.keys() and "-Xmx" not in extra:
+    java_opts += " -Xmx{}G".format(snakemake.resources["mem_gb"])
+
+# Getting java temp directory from output files list, if -Djava.io.tmpdir
+# is not provided in java parameters. By doing so, backward compatibility is
+# not broken.
+if "java_temp" in snakemake.output.keys() and "-Djava.io.tmpdir" not in extra:
+    java_opts += " -Djava.io.tmpdir={}".format(snakemake.output["java_temp"])
+
 
 data_dir = Path(snakemake.input.db).parent.resolve()
 
@@ -38,7 +53,8 @@ stats_opt = "-noStats" if not stats else "-stats {}".format(stats)
 reference = path.basename(snakemake.input.db)
 
 shell(
-    "snpEff -dataDir {data_dir} {stats_opt} {csvstats_opt} {extra} "
+    "snpEff {java_opts} -dataDir {data_dir} "
+    "{stats_opt} {csvstats_opt} {extra} "
     "{reference} {incalls} "
     "{outprefix} > {outcalls} {log}"
 )
