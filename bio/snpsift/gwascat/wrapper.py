b23777ca688b55183154bfc16b736fefe1047ab4
tdayris <tdayris@gustaveroussy.fr>
2020-10-20 10:35:52 +0200

    [dev] (snpsift): Update with comments from @dlaehnemann and java temp

--- bio/snpsift/gwascat/wrapper.py
+++ bio/snpsift/gwascat/wrapper.py
@@ -9,8 +9,23 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 extra = snakemake.params.get("extra", "")
+java_opts = ""
+# Getting memory in megabytes, if java opts is not filled with -Xmx parameter
+# By doing so, backward compatibility is preserved
 if "mem_mb" in snakemake.resources.keys() and "-Xmx" not in extra:
-    extra += " -Xmx{}M".format(snakemake.resources["mem_mb"])
+    java_opts += " -Xmx{}M".format(snakemake.resources["mem_mb"])
+
+# Getting memory in gigabytes, for user convenience. Please prefer the use
+# of mem_mb over mem_gb as advised in documentation.
+elif "mem_gb" in snakemake.resources.keys() and "-Xmx" not in extra:
+    java_opts += " -Xmx{}G".format(snakemake.resources["mem_gb"])
+
+# Getting java temp directory from output files list, if -Djava.io.tmpdir
+# is not provided in java parameters. By doing so, backward compatibility is
+# not broken.
+if "java_temp" in snakemake.output.keys() and "-Djava.io.tmpdir" not in extra:
+    java_opts += " -Djava.io.tmpdir={}".format(snakemake.output["java_temp"])
+
 
 min_threads = 1
 
@@ -46,7 +61,7 @@ if snakemake.threads < min_threads:
 
 shell(
     "SnpSift gwasCat "  # Tool and its subcommand
-    " {extra} "  # Extra parameters
+    " {java_opts} {extra} "  # Extra parameters
     " -db {snakemake.input.gwascat} "  # Path to gwasCat file
     " {incall} "  # Path to input vcf file
     " {outcall} "  # Path to output vcf file
