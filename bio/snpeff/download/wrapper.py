6e95a9231a8b79b19d439dc18cfd4d39cec44ba2
tdayris <tdayris@gustaveroussy.fr>
2020-10-20 12:34:28 +0200

    [fix] (snpeff-download): Useless extra parameters removed

--- bio/snpeff/download/wrapper.py
+++ bio/snpeff/download/wrapper.py
@@ -10,23 +10,22 @@ reference = snakemake.params.reference
 outdir = Path(snakemake.output[0]).parent.resolve()
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
-extra = snakemake.params
 java_opts = ""
 # Getting memory in megabytes, if java opts is not filled with -Xmx parameter
 # By doing so, backward compatibility is preserved
-if "mem_mb" in snakemake.resources.keys() and "-Xmx" not in extra:
+if "mem_mb" in snakemake.resources.keys():
     java_opts += " -Xmx{}M".format(snakemake.resources["mem_mb"])
 
 # Getting memory in gigabytes, for user convenience. Please prefer the use
 # of mem_mb over mem_gb as advised in documentation.
-elif "mem_gb" in snakemake.resources.keys() and "-Xmx" not in extra:
+elif "mem_gb" in snakemake.resources.keys():
     java_opts += " -Xmx{}G".format(snakemake.resources["mem_gb"])
 
 # Getting java temp directory from output files list, if -Djava.io.tmpdir
 # is not provided in java parameters. By doing so, backward compatibility is
 # not broken.
-if "java_temp" in snakemake.output.keys() and "-Djava.io.tmpdir" not in extra:
+if "java_temp" in snakemake.output.keys():
     java_opts += " -Djava.io.tmpdir={}".format(snakemake.output["java_temp"])
 
 
-shell("snpEff download {java_opts} {extra} -dataDir {outdir} {reference} {log}")
+shell("snpEff download {java_opts} -dataDir {outdir} {reference} {log}")
