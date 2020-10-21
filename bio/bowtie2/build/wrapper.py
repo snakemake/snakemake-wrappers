a690de00d3af9ddf26081a1d2b8cc6a973ac4578
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2020-10-21 09:27:20 -0400

    Troubleshoot wrapper

--- bio/bowtie2/build/wrapper.py
+++ bio/bowtie2/build/wrapper.py
@@ -8,7 +8,7 @@ from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-indexbase = output[0].replace(".1.bt2", "")
+indexbase = snakemake.output[0].replace(".1.bt2", "")
 shell(
     "bowtie2-build --threads {snakemake.threads} {snakemake.params.extra} "
     "{snakemake.input.reference} {indexbase}"
