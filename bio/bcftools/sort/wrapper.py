472fa2a77cddb166313f2d631fe5df114b7ed6b1
fgvieira <fgarrettvieira@gmail.com>
2020-10-22 22:35:15 +0200

    Added option for mem_gb

--- bio/bcftools/sort/wrapper.py
+++ bio/bcftools/sort/wrapper.py
@@ -3,16 +3,21 @@ __copyright__ = "Copyright 2021, Filipe G. Vieira"
 __license__ = "MIT"
 
 
+from os import path
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-mem_mb = snakemake.resources.get("mem_mb", "")
-if mem_mb:
-    mem_mb = "--max-mem {}M".format(mem_mb)
+max_mem = snakemake.resources.get("mem_mb", "")
+if max_mem:
+    max_mem = "--max-mem {}M".format(max_mem)
 else:
-    mem_mb = ""
+    max_mem = snakemake.resources.get("mem_gb", "")
+    if max_mem:
+        max_mem = "--max-mem {}G".format(max_mem)
+    else:
+        max_mem = ""
 
 tmp_dir = snakemake.params.get("tmp_dir", "")
 if tmp_dir:
@@ -22,5 +27,5 @@ else:
 
 
 shell(
-    "bcftools sort {mem_mb} {tmp_dir} {extra} -o {snakemake.output[0]} {snakemake.input[0]} {log}"
+    "bcftools sort {max_mem} {tmp_dir} {extra} --output-file {snakemake.output[0]} {snakemake.input[0]} {log}"
 )
