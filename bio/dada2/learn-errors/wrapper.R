7e226fd484610157ab3a886a34fe34cda643a687
cpauvert <cpauvert@users.noreply.github.com>
2020-10-23 16:00:56 +0200

    Changes concerning optional parameters and pe version
    * Provide warnings for incorrect syntax for passing parameters
    * Add an all rule to illustrate the case of paired end sequences

--- bio/dada2/learn-errors/wrapper.R
+++ bio/dada2/learn-errors/wrapper.R
@@ -19,10 +19,17 @@ args<-list(
            )
 # Check if extra params are passed
 if(length(snakemake@params) > 0 ){
-       if(length(snakemake@params[[1]]) > 1){
+       if(is.list(snakemake@params[[1]])){
            # Add them to the list of arguments
            args<-c(args,snakemake@params[[1]])
+       } else{
+           message("Optional R parameters should be passed as Python dictionary")
+           message("in the Snakefile. Check the example below:")
+           message("\tparams: { 'verbose': True, 'foo': [1,42] }")
+           message("Using defaults parameters from dada2::learnErrors()")
        }
+} else{
+    message("No optional parameters. Using defaults parameters from dada2::learnErrors()")
 }
 
 # Learn errors rates for both read types
