6d0caa3963c90710f4b08b7b369727c99e2b1878
cpauvert <cpauvert@users.noreply.github.com>
2020-10-27 14:41:41 +0100

    Add source for sink code and explicited the usage

--- bio/dada2/quality-profile/wrapper.R
+++ bio/dada2/quality-profile/wrapper.R
@@ -6,6 +6,7 @@
 # Snakemake wrapper for plotting the quality profile of reads using dada2 plotQualityProfile function.
 
 # Sink the stderr and stdout to the snakemake log file
+# https://stackoverflow.com/a/48173272
 log.file<-file(snakemake@log[[1]],open="wt")
 sink(log.file)
 sink(log.file,type="message")
@@ -19,6 +20,7 @@ pquality<-plotQualityProfile(unlist(snakemake@input))
 library(ggplot2)
 ggsave(snakemake@output[[1]], pquality, width = 4, height = 3, dpi = 300)
 
-# Close the connection for the log file
+# Proper syntax to close the connection for the log file
+# but could be optional for Snakemake wrapper
 sink(type="message")
 sink()
