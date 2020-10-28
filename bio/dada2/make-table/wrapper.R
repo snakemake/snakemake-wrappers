e3a22fb16127b3c88f68501adbb1058a293c7a19
cpauvert <cpauvert@users.noreply.github.com>
2020-10-28 15:54:10 +0100

    English correction

--- bio/dada2/make-table/wrapper.R
+++ bio/dada2/make-table/wrapper.R
@@ -38,10 +38,10 @@ if(length(snakemake@params) > 1 ){
            message("Optional R parameters should be passed as named Python arguments")
            message("in the Snakefile. Check the example below:")
            message("params:\n\tverbose=True, foo=[1,42]")
-           message("Using defaults parameters from dada2::makeSequenceTable()")
+           message("Using default parameters from dada2::makeSequenceTable()")
        }
 } else{
-    message("No optional parameters. Using defaults parameters from dada2::makeSequenceTable()")
+    message("No optional parameters. Using default parameters from dada2::makeSequenceTable()")
 }
 
 # Make table
