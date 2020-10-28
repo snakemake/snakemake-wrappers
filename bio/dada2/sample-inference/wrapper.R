50bb7e72ba1aae4624fa528179a6c84984fb3700
cpauvert <cpauvert@users.noreply.github.com>
2020-10-28 13:17:06 +0100

    Change the way to pass optional parameters

--- bio/dada2/sample-inference/wrapper.R
+++ bio/dada2/sample-inference/wrapper.R
@@ -5,12 +5,13 @@
 
 # Snakemake wrapper for inferring sample composition using dada2 dada function.
 
-library(dada2, quietly=TRUE)
-
 # Sink the stderr and stdout to the snakemake log file
+# https://stackoverflow.com/a/48173272
 log.file<-file(snakemake@log[[1]],open="wt")
 sink(log.file)
 sink(log.file,type="message")
+
+library(dada2)
 
 # Prepare arguments (no matter the order)
 args<-list(
@@ -20,10 +21,19 @@ args<-list(
            )
 # Check if extra params are passed
 if(length(snakemake@params) > 0 ){
-    if(length(snakemake@params[[1]]) > 1){
-        # Add them to the list of arguments
-        args<-c(args,snakemake@params[[1]])
-    }
+       # Keeping only the named elements of the list for do.call()
+       extra<-snakemake@params[ names(snakemake@params) != "" ]
+       if(is.list(extra)){
+           # Add them to the list of arguments
+           args<-c(args, extra)
+       } else{
+           message("Optional R parameters should be passed as named Python arguments")
+           message("in the Snakefile. Check the example below:")
+           message("params:\n\tverbose=True, foo=[1,42]")
+           message("Using defaults parameters from dada2::dada()")
+       }
+} else{
+    message("No optional parameters. Using defaults parameters from dada2::dada()")
 }
 
 # Learn errors rates for both read types
@@ -32,6 +42,7 @@ inferred_composition<-do.call(dada, args)
 # Store the inferred sample composition as RDS files
 saveRDS(inferred_composition, snakemake@output[[1]],compress = T)
 
-# Close the connection for the log file
+# Proper syntax to close the connection for the log file
+# but could be optional for Snakemake wrapper
 sink(type="message")
 sink()
