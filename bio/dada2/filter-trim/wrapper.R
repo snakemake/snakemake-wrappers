021745700e4e7a8be40d4330d26a98e1c2c50fdb
cpauvert <cpauvert@users.noreply.github.com>
2020-10-28 15:57:52 +0100

    English correction

--- bio/dada2/filter-trim/wrapper.R
+++ bio/dada2/filter-trim/wrapper.R
@@ -37,7 +37,7 @@ if(length(snakemake@params) > 0 ){
         message("Optional R parameters should be passed as named Python arguments")
         message("in the Snakefile. Check the example below:")
         message("params:\n\tverbose=True, foo=[1,42]")
-        message("Using defaults parameters from dada2::filterAndTrim()")
+        message("Using default parameters from dada2::filterAndTrim()")
     }
 } else{
     message("No optional parameters. Using default parameters from dada2::filterAndTrim()")
