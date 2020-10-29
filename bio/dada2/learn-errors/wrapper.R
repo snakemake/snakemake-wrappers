b5f8d6225b2ce79da0a3254a1919a26891aa8ece
Charlie Pauvert <cpauvert@users.noreply.github.com>
2020-10-29 12:00:19 +0100

    Minor changes and removal of unneeded test (#243)
    To be homogeneous with the others proposed dada2 wrappers
    
    Co-authored-by: David Laehnemann <david.laehnemann@hhu.de>
    
    Co-authored-by: David Laehnemann <david.laehnemann@hhu.de>

--- bio/dada2/learn-errors/wrapper.R
+++ bio/dada2/learn-errors/wrapper.R
@@ -22,15 +22,8 @@ args<-list(
 if(length(snakemake@params) > 0 ){
        # Keeping only the named elements of the list for do.call()
        extra<-snakemake@params[ names(snakemake@params) != "" ]
-       if(is.list(extra)){
-           # Add them to the list of arguments
-           args<-c(args, extra)
-       } else{
-           message("Optional R parameters should be passed as named Python arguments")
-           message("in the Snakefile. Check the example below:")
-           message("params:\n\tverbose=True, foo=[1,42]")
-           message("Using defaults parameters from dada2::learnErrors()")
-       }
+       # Add them to the list of arguments
+       args<-c(args, extra)
 } else{
     message("No optional parameters. Using defaults parameters from dada2::learnErrors()")
 }
