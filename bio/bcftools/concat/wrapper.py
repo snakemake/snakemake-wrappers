cbae70c59876c58e9722fee474a3af8de4e1d6fb
Johannes Köster <johannes.koester@tu-dortmund.de>
2020-11-18 21:04:01 +0100

    fix lint

--- bio/bcftools/concat/wrapper.py
+++ bio/bcftools/concat/wrapper.py
@@ -6,8 +6,9 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 shell(
     "bcftools concat {snakemake.params} -o {snakemake.output[0]} "
-    "{snakemake.input.calls}"
+    "{snakemake.input.calls} 2> {log}"
 )
