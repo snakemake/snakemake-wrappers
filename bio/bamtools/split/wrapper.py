299b67ad5f228fe3d3f07a13eb8fca936557bcc0
Patrik Smeds <patrik.smeds@gmail.com>
2021-01-25 11:06:43 +0100

    Bamtools split wrapper.

---
+++ bio/bamtools/split/wrapper.py
@@ -0,0 +1,16 @@
+__author__ = "Patrik Smeds"
+__copyright__ = "Copyright 2021, Patrik Smeds"
+__email__ = "koester@jimmy.harvard.edu"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+split_type = snakemake.params.get("split_type",'-reference')
+
+if len(snakemake.input) != 1:
+    raise ValueError("One bam input file expected, got: " + str(len(snakemake.input)))
+
+shell("bamtools split -in {snakemake.input} " + split_type + " {log}")
