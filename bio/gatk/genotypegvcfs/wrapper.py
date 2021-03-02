ab74fd8780377a149440c18f139223889abb1ca5
fgvieira <fgarrettvieira@gmail.com>
2021-03-02 18:30:30 +0100

    Added GenomicsDBImport

--- bio/gatk/genotypegvcfs/wrapper.py
+++ bio/gatk/genotypegvcfs/wrapper.py
@@ -14,9 +14,15 @@ extra = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+input_gvcf = snakemake.input.gvcf
+if os.path.isdir(input_gvcf):
+    input_gvcf = "gendb://" + input_gvcf
+
+
 shell(
     "gatk --java-options '{java_opts}' GenotypeGVCFs {extra} "
-    "-V {snakemake.input.gvcf} "
+    "-V {input_gvcf} "
     "-R {snakemake.input.ref} "
     "-O {snakemake.output.vcf} {log}"
 )
