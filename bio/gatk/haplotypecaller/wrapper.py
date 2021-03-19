6f9870a29e210325e837ee138d4d08c5f857d0c5
Filipe G. Vieira <fgarrettvieira@gmail.com>
2021-03-19 12:56:41 +0100

    Added bam-output option (#296)
    * Added bam_out option
    
    * Fixed option name
    
    * Fixed variable name
    
    * Fix format issue
    
    * Added bam_output option to example Snakefile

--- bio/gatk/haplotypecaller/wrapper.py
+++ bio/gatk/haplotypecaller/wrapper.py
@@ -11,7 +11,11 @@ from snakemake_wrapper_utils.java import get_java_opts
 
 known = snakemake.input.get("known", "")
 if known:
-    known = "--dbsnp " + known
+    known = "--dbsnp " + str(known)
+
+bam_output = snakemake.output.get("bam", "")
+if bam_output:
+    bam_output = "--bam-output " + str(bam_output)
 
 extra = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
@@ -25,6 +29,6 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 shell(
     "gatk --java-options '{java_opts}' HaplotypeCaller {extra} "
     "-R {snakemake.input.ref} {bams} "
-    "-ERC GVCF "
+    "-ERC GVCF {bam_output} "
     "-O {snakemake.output.gvcf} {known} {log}"
 )
