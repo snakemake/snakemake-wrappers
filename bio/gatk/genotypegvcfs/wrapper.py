b4a51a367e946997448b41ca4e17907bfb0a1cbd
Brett Copeland <brcopeland@eng.ucsd.edu>
2021-03-23 03:06:23 -0700

    Added the ability to run gatk GenotypeGVCFs on a GenomicsDB data store. (#310)
    * Added the ability to run gatk GenotypeGVCFs on a GenomicsDB data store.
    Additionally added parameters to handle:
    1. an input interval file,
    2. dbSNP,
    3. --tmp-dir
    
    * Implemented changes to genotypegvcfs per suggestions of @jafors.
    
    Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

--- bio/gatk/genotypegvcfs/wrapper.py
+++ bio/gatk/genotypegvcfs/wrapper.py
@@ -12,17 +12,37 @@ from snakemake_wrapper_utils.java import get_java_opts
 
 extra = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
+interval_file = snakemake.input.get("interval_file", "")
+if interval_file:
+    interval_file = "-L {}".format(interval_file)
+dbsnp = snakemake.input.get("known", "")
+if dbsnp:
+    dbsnp = "-D {}".format(dbsnp)
+
+# Allow for either an input gvcf or GenomicsDB
+gvcf = snakemake.input.get("gvcf", "")
+genomicsdb = snakemake.input.get("genomicsdb", "")
+if gvcf:
+    if genomicsdb:
+        raise Exception("Only input.gvcf or input.genomicsdb expected, got both.")
+    input_string = gvcf
+else:
+    if genomicsdb:
+        input_string = "gendb://{}".format(genomicsdb)
+    else:
+        raise Exception("Expected input.gvcf or input.genomicsdb.")
+tmp_dir = snakemake.params.get("tmp_dir", "")
+if tmp_dir:
+    tmp_dir = "--tmp-dir={}".format(tmp_dir)
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-input_gvcf = snakemake.input.gvcf
-if os.path.isdir(input_gvcf):
-    input_gvcf = "gendb://" + input_gvcf
-
-
 shell(
     "gatk --java-options '{java_opts}' GenotypeGVCFs {extra} "
-    "-V {input_gvcf} "
+    "-V {input_string} "
     "-R {snakemake.input.ref} "
+    "{dbsnp} "
+    "{interval_file} "
+    "{tmp_dir} "
     "-O {snakemake.output.vcf} {log}"
 )
