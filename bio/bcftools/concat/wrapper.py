3ec9ee10b3336d7a9c5d7644011c50d88edde7ba
fgvieira <fgarrettvieira@gmail.com>
2021-03-30 01:04:18 +0200

    Automatically detect threads and output format

--- bio/bcftools/concat/wrapper.py
+++ bio/bcftools/concat/wrapper.py
@@ -4,12 +4,37 @@ __email__ = "koester@jimmy.harvard.edu"
 __license__ = "MIT"
 
 
+from os import path
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+threads = "" if snakemake.threads <= 1 else "--threads {}".format(snakemake.threads - 1)
+
+
+uncompressed_bcf = snakemake.params.get("uncompressed_bcf", False)
+
+
+out_name, out_ext = path.splitext(snakemake.output[0])
+if out_ext == ".vcf":
+    out_format = "v"
+elif out_ext == ".bcf":
+    if uncompressed_bcf:
+        out_format = "u"
+    else:
+        out_format = "b"
+elif out_ext == ".gz":
+    out_name, out_ext = path.splitext(out_name)
+    if out_ext == ".vcf":
+        out_format = "z"
+    else:
+        raise ValueError("output file with invalid extension (.vcf, .vcf.gz, .bcf).")
+else:
+    raise ValueError("output file with invalid extension (.vcf, .vcf.gz, .bcf).")
+
+
 shell(
-    "bcftools concat {snakemake.params} -o {snakemake.output[0]} "
+    "bcftools concat {threads} {snakemake.params} --output-type {out_format} -o {snakemake.output[0]} "
     "{snakemake.input.calls} "
     "{log}"
 )
