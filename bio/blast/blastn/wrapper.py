5742730e80bb521c7576f2f184c5857abf91e2f8
David Laehnemann <david.laehnemann@hhu.de>
2021-03-31 15:39:02 +0200

    remove pathlib dependency

--- bio/blast/blastn/wrapper.py
+++ bio/blast/blastn/wrapper.py
@@ -5,13 +5,12 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 from os import path
-from pathlib import Path
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 format = snakemake.params.get("format", "")
 blastdb = snakemake.input.get("blastdb", "")[0]
-db_name = "{}/{}".format(path.dirname(blastdb), Path(path.basename(blastdb)).stem)
+db_name = path.splitext(blastdb)[0]
 
 if format:
     out_format = " -outfmt '{}'".format(format)
