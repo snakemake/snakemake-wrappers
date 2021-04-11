d8750774edf0bee698677219a329c44ee96c2b82
fgvieira <fgarrettvieira@gmail.com>
2021-04-12 00:41:03 +0200

    Changed wrapper to work with the new snakemake_wrapper_utils.bcftools

--- bio/bcftools/concat/wrapper.py
+++ bio/bcftools/concat/wrapper.py
@@ -6,35 +6,15 @@ __license__ = "MIT"
 
 from os import path
 from snakemake.shell import shell
+from snakemake_wrapper_utils.bcftools import get_bcftools_opts
 
+
+bcftools_opts = get_bcftools_opts(snakemake)
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
-threads = "" if snakemake.threads <= 1 else "--threads {}".format(snakemake.threads - 1)
-
-
-uncompressed_bcf = snakemake.params.get("uncompressed_bcf", False)
-
-
-out_name, out_ext = path.splitext(snakemake.output[0])
-if out_ext == ".vcf":
-    out_format = "v"
-elif out_ext == ".bcf":
-    if uncompressed_bcf:
-        out_format = "u"
-    else:
-        out_format = "b"
-elif out_ext == ".gz":
-    out_name, out_ext = path.splitext(out_name)
-    if out_ext == ".vcf":
-        out_format = "z"
-    else:
-        raise ValueError("output file with invalid extension (.vcf, .vcf.gz, .bcf).")
-else:
-    raise ValueError("output file with invalid extension (.vcf, .vcf.gz, .bcf).")
 
 
 shell(
-    "bcftools concat {threads} {snakemake.params} --output-type {out_format} -o {snakemake.output[0]} "
+    "bcftools concat {snakemake.params.extra} {bcftools_opts} -o {snakemake.output[0]} "
     "{snakemake.input.calls} "
     "{log}"
 )
