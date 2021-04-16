e472f191333c91a83521d46a2d7d65c5197b3cb4
nikostr <nikostr@users.noreply.github.com>
2021-04-16 15:20:49 +0200

    Adds optional sample_name parameter to deepvariant

--- bio/deepvariant/wrapper.py
+++ bio/deepvariant/wrapper.py
@@ -14,14 +14,16 @@ log_dir = os.path.dirname(snakemake.log[0])
 output_dir = os.path.dirname(snakemake.output[0])
 
 # sample basename
-basename = os.path.splitext(os.path.basename(snakemake.input.bam))[0]
+sample_name = snakemake.params.get('sample_name',
+    os.path.splitext(os.path.basename(snakemake.input.bam))[0])
+
 
 make_examples_gvcf = postprocess_gvcf = ""
 gvcf = snakemake.output.get("gvcf", None)
 if gvcf:
     make_examples_gvcf = "--gvcf {tmp_dir} "
     postprocess_gvcf = (
-        "--gvcf_infile {tmp_dir}/{basename}.gvcf.tfrecord@{snakemake.threads}.gz "
+        "--gvcf_infile {tmp_dir}/{sample_name}.gvcf.tfrecord@{snakemake.threads}.gz "
         "--gvcf_outfile {snakemake.output.gvcf} "
     )
 
@@ -31,18 +33,18 @@ with tempfile.TemporaryDirectory() as tmp_dir:
         "--cores {snakemake.threads} "
         "--ref {snakemake.input.ref} "
         "--reads {snakemake.input.bam} "
-        "--sample {basename} "
+        "--sample {sample_name} "
         "--examples {tmp_dir} "
         "--logdir {log_dir} " + make_examples_gvcf + "{extra} \n"
         "dv_call_variants.py "
         "--cores {snakemake.threads} "
-        "--outfile {tmp_dir}/{basename}.tmp "
-        "--sample {basename} "
+        "--outfile {tmp_dir}/{sample_name}.tmp "
+        "--sample {sample_name} "
         "--examples {tmp_dir} "
         "--model {snakemake.params.model} \n"
         "dv_postprocess_variants.py "
         "--ref {snakemake.input.ref} "
         + postprocess_gvcf
-        + "--infile {tmp_dir}/{basename}.tmp "
+        + "--infile {tmp_dir}/{sample_name}.tmp "
         "--outfile {snakemake.output.vcf} ) {log}"
     )
