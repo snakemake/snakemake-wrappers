f9a582a7d3ef218ab5a63fa4fdfe533c99b028f9
nikostr <nikostr@users.noreply.github.com>
2021-04-20 08:51:42 +0200

    Resolves comments, fixes download_path problem

--- bio/busco/wrapper.py
+++ bio/busco/wrapper.py
@@ -17,19 +17,20 @@ lineage = snakemake.params.get("lineage_path")
 assert lineage is not None, "please input the path to a lineage for busco assessment"
 
 # busco does not allow you to direct output location: handle this by moving output
-outdir = snakemake.output[0]
+stripped_output = snakemake.output[0].rstrip("/")
+out = path.basename(stripped_output)
+out_dirname = path.dirname(stripped_output)
+out_path = " --out_path {} ".format(out_dirname) if out_dirname else ""
+
 download_path_dir = snakemake.params.get("download_path", "")
-download_path = " --download_path {download_path_dir} " if download_path_dir else ""
+download_path = " --download_path {} ".format(download_path_dir) if download_path_dir else ""
 
 # note: --force allows snakemake to handle rewriting files as necessary
 # without needing to specify *all* busco outputs as snakemake outputs
-with tempfile.TemporaryDirectory(prefix="busco_", dir="") as tmp_dir:
-    shell(
-        "busco --in {snakemake.input} --out {tmp_dir} --force "
-        "--cpu {snakemake.threads} --mode {mode} --lineage {lineage} "
-        "{download_path} "
-        "{extra} {log}"
-    )
-
-    # move to intended location
-    shell("cp -r {tmp_dir} {outdir}")
+shell(
+    "busco --in {snakemake.input} --out {out} --force "
+    "{out_path} "
+    "--cpu {snakemake.threads} --mode {mode} --lineage {lineage} "
+    "{download_path} "
+    "{extra} {log}"
+)
