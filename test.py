1c7f59e64a25a1fd498691c8944e3bbc1a310314
Brett Copeland <brcopeland@gmail.com>
2021-05-06 11:43:43 -0700

    Added wrapper for gtfToGenePred.

--- test.py
+++ test.py
@@ -3658,3 +3658,10 @@ def test_nextflow():
         "utils/nextflow",
         ["snakemake", "--cores", "1", "--use-conda", "-F", "--show-failed-logs"]
     )
+
+@skip_if_not_modified
+def test_gtftogenepred():
+    run(
+        "bio/ucsc/gtfToGenePred",
+        ["snakemake", "--cores", "1", "--use-conda", "-F"],
+    )
