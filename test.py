e536f3bd47a19be2ce6d69ea61f1fa8c00f2f47f
Brett Copeland <brcopeland@gmail.com>
2021-05-11 17:20:14 -0700

    Added CollectRnaSeqMetrics wrapper.

--- test.py
+++ test.py
@@ -3658,3 +3658,10 @@ def test_nextflow():
         "utils/nextflow",
         ["snakemake", "--cores", "1", "--use-conda", "-F", "--show-failed-logs"]
     )
+
+@skip_if_not_modified
+def test_collectrnaseqmetrics():
+    run(
+        "bio/picard/collectrnaseqmetrics",
+        ["snakemake", "--cores", "1", "--use-conda", "-F"],
+    )
