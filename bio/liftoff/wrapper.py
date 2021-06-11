8c1b1016141333ccd84efd86d04a9d1220045219
Tomás Di Domenico <tdido@tdido.ar>
2021-06-11 11:33:06 +0200

    Reformatted with black

--- bio/liftoff/wrapper.py
+++ bio/liftoff/wrapper.py
@@ -12,12 +12,12 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
 
 shell(
-    "liftoff " # tool
-    "-g {snakemake.input.ann} " # annotation file to lift over in GFF or GTF format
-    "-o {snakemake.output.main} " # main output
-    "-u {snakemake.output.unmapped} " # unmapped output
+    "liftoff "  # tool
+    "-g {snakemake.input.ann} "  # annotation file to lift over in GFF or GTF format
+    "-o {snakemake.output.main} "  # main output
+    "-u {snakemake.output.unmapped} "  # unmapped output
     "{extra} "  # optional parameters
-    "{snakemake.input.tgt} " # target fasta genome to lift genes to
-    "{snakemake.input.ref} " # reference fasta genome to lift genes from 
+    "{snakemake.input.tgt} "  # target fasta genome to lift genes to
+    "{snakemake.input.ref} "  # reference fasta genome to lift genes from
     "{log}"  # Logging
 )
