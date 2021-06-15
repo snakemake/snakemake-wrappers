904e618f80e9552d9adaec5d525fc71630993bf3
maxlcummins <max.l.cummins@gmail.com>
2021-06-16 09:59:50 +1000

    change log file to capture stdout and stderr rather than just stderr

--- bio/mlst/wrapper.py
+++ bio/mlst/wrapper.py
@@ -6,7 +6,7 @@ __license__ = "MIT"
 from snakemake.shell import shell
 from os import path
 
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
     "mlst"
