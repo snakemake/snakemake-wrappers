659fe02ef23a390a31908d05271d97ce701e43b9
nikostr <nikostr@users.noreply.github.com>
2021-06-24 16:46:59 +0200

    add parsing of extra params

--- bio/bcftools/view/wrapper.py
+++ bio/bcftools/view/wrapper.py
@@ -8,10 +8,12 @@ from snakemake.shell import shell
 from snakemake_wrapper_utils.bcftools import get_bcftools_opts
 
 bcftools_opts = get_bcftools_opts(snakemake)
+extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
     "bcftools view {bcftools_opts} "
+    "{extra} "
     "{snakemake.input[0]} "
     "-o {snakemake.output} "
     "{log}"
