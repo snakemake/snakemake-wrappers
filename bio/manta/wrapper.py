c739a58f07d192dcecc5d40cf3afc051d1f1c2fb
fgvieira <fgarrettvieira@gmail.com>
2021-11-05 14:29:28 +0100

    Fixed format

--- bio/manta/wrapper.py
+++ bio/manta/wrapper.py
@@ -36,45 +36,57 @@ shell(
 # Copy outputs into proper position.
 results_base = Path(snakemake.output.outdir) / "results" / "variants"
 
+
 def infer_vcf_ext(vcf):
     if vcf.endswith(".vcf.gz"):
         return "z"
     elif vcf.endswith(".bcf"):
         return "b"
     else:
-        raise ValueError(
-            "invalid file extension ('.vcf.gz', '.bcf')."
-        )
+        raise ValueError("invalid file extension ('.vcf.gz', '.bcf').")
+
 
 vcf = snakemake.output.get("vcf", "")
 vcf_path = results_base / "diploidSV.vcf.gz"
 if vcf and vcf != vcf_path:
     vcf_format = infer_vcf_ext(vcf)
-    shell("bcftools view --threads {snakemake.threads} --output-file {vcf:q} --output-type {vcf_format} {vcf_path:q}")
+    shell(
+        "bcftools view --threads {snakemake.threads} --output-file {vcf:q} --output-type {vcf_format} {vcf_path:q}"
+    )
 
     idx = snakemake.output.get("idx", "")
     idx_path = results_base / "diploidSV.vcf.gz.tbi"
     if idx and idx != idx_path:
-        shell("bcftools index --threads {snakemake.threads} --output-file {idx:q} {vcf:q}")
+        shell(
+            "bcftools index --threads {snakemake.threads} --output-file {idx:q} {vcf:q}"
+        )
 
 cand_indel_vcf = snakemake.output.get("cand_indel_vcf", "")
 cand_indel_vcf_path = results_base / "candidateSmallIndels.vcf.gz"
 if cand_indel_vcf and cand_indel_vcf != cand_indel_vcf_path:
     cand_indel_vcf_format = infer_vcf_ext(cand_indel_vcf)
-    shell("bcftools view --threads {snakemake.threads} --output-file {cand_indel_vcf:q} --output-type {cand_indel_vcf_format} {cand_indel_vcf_path:q}")
+    shell(
+        "bcftools view --threads {snakemake.threads} --output-file {cand_indel_vcf:q} --output-type {cand_indel_vcf_format} {cand_indel_vcf_path:q}"
+    )
 
     cand_indel_idx = snakemake.output.get("cand_indel_idx", "")
     cand_indel_idx_path = results_base / "candidateSmallIndels.vcf.gz.tbi"
     if cand_indel_idx and cand_indel_idx != cand_indel_idx_path:
-        shell("bcftools index --threads {snakemake.threads} --output-file {cand_indel_idx:q} {cand_indel_vcf:q}")
+        shell(
+            "bcftools index --threads {snakemake.threads} --output-file {cand_indel_idx:q} {cand_indel_vcf:q}"
+        )
 
 cand_sv_vcf = snakemake.output.get("cand_sv_vcf", "")
 cand_sv_vcf_path = results_base / "candidateSV.vcf.gz"
 if cand_sv_vcf and cand_sv_vcf != cand_sv_vcf_path:
     cand_sv_vcf_format = infer_vcf_ext(cand_sv_vcf)
-    shell("bcftools view --threads {snakemake.threads} --output-file {cand_sv_vcf:q} --output-type {cand_sv_vcf_format} {cand_sv_vcf_path:q}")
+    shell(
+        "bcftools view --threads {snakemake.threads} --output-file {cand_sv_vcf:q} --output-type {cand_sv_vcf_format} {cand_sv_vcf_path:q}"
+    )
 
     cand_sv_idx = snakemake.output.get("cand_sv_idx", "")
     cand_sv_idx_path = results_base / "candidateSV.vcf.gz.tbi"
     if cand_sv_idx and cand_sv_idx != cand_sv_idx_path:
-        shell("bcftools index --threads {snakemake.threads} --output-file {cand_sv_idx:q} {cand_sv_vcf:q}")
+        shell(
+            "bcftools index --threads {snakemake.threads} --output-file {cand_sv_idx:q} {cand_sv_vcf:q}"
+        )
