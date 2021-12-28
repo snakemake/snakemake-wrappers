e98dc14d089f52763bccfd66b71e0c5bcb3e7597
fgvieira <fgarrettvieira@gmail.com>
2021-12-28 23:46:16 +0100

    Increased max number of open files to avoid error

--- bio/manta/wrapper.py
+++ bio/manta/wrapper.py
@@ -50,6 +50,8 @@ with TemporaryDirectory() as tempdir:
     bams = list(map("--normalBam {}".format, bams))
 
     shell(
+        # Increase max number of open files (https://github.com/Illumina/manta/issues/245)
+        "ulimit -n 10240; "
         # Configure Manta
         "configManta.py {extra_cfg} {bams} --referenceFasta {snakemake.input.ref} {bed} --runDir {run_dir} {log}; "
         # Run Manta
