e69987780cbf23586cb17cb6109d080c287bfdcc
fgvieira <fgarrettvieira@gmail.com>
2022-01-05 09:35:30 +0100

    Memory parsing function

--- bio/sra-tools/fasterq-dump/wrapper.py
+++ bio/sra-tools/fasterq-dump/wrapper.py
@@ -12,28 +12,32 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
 
 
+# Parse memory
+mem = ""
+mem_mb = get_mem(snakemake, "MB")
+
+
+# Outdir
 outdir = os.path.dirname(snakemake.output[0])
 if outdir:
     outdir = f"--outdir {outdir}"
 
 
+# Output compression
 compress = ""
+if mem_mb:
+    mem = f"-m{mem_mb}"
+
 for output in snakemake.output:
     out_name, out_ext = os.path.splitext(output)
     if out_ext == ".gz":
         compress += f"pigz -p {snakemake.threads} {out_name}; "
     elif out_ext == ".bz2":
-        compress += f"pbzip2 -p{snakemake.threads} {out_name}; "
+        compress += f"pbzip2 -p{snakemake.threads} {mem} {out_name}; "
 
 
-mem = snakemake.resources.get("mem_gb", "")
-if mem:
-    mem = f"--mem {mem}G"
-else:
-    mem = snakemake.resources.get("mem_mb", "")
-    if mem:
-        mem = f"--mem {mem}M"
-
+if mem_mb:
+    mem = f"--mem_mb {mem_mb}M"
 
 with tempfile.TemporaryDirectory() as tmp:
     shell(
@@ -42,3 +46,17 @@ with tempfile.TemporaryDirectory() as tmp:
         "{compress}"
         ") {log}"
     )
+
+
+def get_mem(snakemake, out_unit="MB"):
+    # Store resources.mem in MB
+    mem_mb = snakemake.resources.get("mem_gb", 0) * 1024
+    if not mem_mb:
+        mem_mb = snakemake.resources.get("mem_mb", 0)
+
+    if out_unit == "MB":
+        return mem_mb
+    elif out_unit == "GB":
+        return mem_mb / 1024
+    else:
+        raise valueError("invalid out_unit, only MB and GB supported.")
