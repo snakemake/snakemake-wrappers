37c2d08895efe1a596b2f9e1b76ab4150d39f2cf
Filipe G. Vieira <fgarrettvieira@gmail.com>
2022-01-26 16:08:23 +0100

    feat: parse mem for samtools sort (#442)
    * Fixed tmp prefix
    
    * Parsing memory
    
    * Removed redundant random number on prefix
    
    * Code clean up
    
    * Added missing import
    
    * Fixed bug with int and str
    
    * Tweaked mem format
    
    * FIxed typo

--- bio/samtools/fastq/separate/wrapper.py
+++ bio/samtools/fastq/separate/wrapper.py
@@ -6,35 +6,41 @@ __license__ = "MIT"
 
 import os
 import tempfile
+from pathlib import Path
 from snakemake.shell import shell
+from snakemake_wrapper_utils.snakemake import get_mem
 
 params_sort = snakemake.params.get("sort", "")
 params_fastq = snakemake.params.get("fastq", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
-prefix = os.path.splitext(snakemake.output[0])[0]
 
 # Samtools takes additional threads through its option -@
 # One thread is used bu Samtools sort
 # One thread is used by Samtools fastq
 # So snakemake.threads has to take them into account
 # before allowing additional threads through samtools sort -@
-threads = "" if snakemake.threads <= 2 else " -@ {} ".format(snakemake.threads - 2)
+threads = 0 if snakemake.threads <= 2 else snakemake.threads - 2
 
-with tempfile.NamedTemporaryFile() as tmpfile:
+mem = get_mem(snakemake, "MiB")
+mem = "-m {0:.0f}M".format(mem / threads) if mem and threads else ""
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    tmp_prefix = Path(tmpdir) / "samtools_fastq.sort_"
+
     shell(
-        "(samtools sort -n "
-        " {threads} "
-        " -T {tmpfile.name} "
-        " {params_sort} "
+        "(samtools sort -n"
+        " --threads {threads}"
+        " {mem}"
+        " -T {tmp_prefix}"
+        " {params_sort}"
         " {snakemake.input[0]} | "
-        "samtools fastq "
-        " {params_fastq} "
-        " -1 {snakemake.output[0]} "
-        " -2 {snakemake.output[1]} "
-        " -0 /dev/null "
-        " -s /dev/null "
-        " -F 0x900 "
+        "samtools fastq"
+        " {params_fastq}"
+        " -1 {snakemake.output[0]}"
+        " -2 {snakemake.output[1]}"
+        " -0 /dev/null"
+        " -s /dev/null"
+        " -F 0x900"
         " - "
         ") {log}"
     )
