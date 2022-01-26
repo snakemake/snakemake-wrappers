55e672a893d135d439a972c12a0811ec860ffb78
Filipe G. Vieira <fgarrettvieira@gmail.com>
2022-01-26 21:57:24 +0100

    feat!: added tempdir, updated version and metadata to all picard wrappers (#443)
    * Added tempdir, updated version and metadata
    
    * Updated test rules with new option formats
    
    * Several typos fixed
    
    * Changed file names so they don not get parsed by picard
    
    * Fixed typo
    
    * Add extra param on all wrappers

--- bio/picard/collectinsertsizemetrics/wrapper.py
+++ bio/picard/collectinsertsizemetrics/wrapper.py
@@ -4,17 +4,23 @@ __email__ = "johannes.koester@protonmail.com"
 __license__ = "MIT"
 
 
+import tempfile
 from snakemake.shell import shell
 from snakemake_wrapper_utils.java import get_java_opts
 
 log = snakemake.log_fmt_shell()
 
-extra = snakemake.params
+extra = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
 
 
-shell(
-    "picard CollectInsertSizeMetrics {java_opts} {extra} "
-    "INPUT={snakemake.input} OUTPUT={snakemake.output.txt} "
-    "HISTOGRAM_FILE={snakemake.output.pdf} {log}"
-)
+with tempfile.TemporaryDirectory() as tmpdir:
+    shell(
+        "picard CollectInsertSizeMetrics"
+        " {java_opts} {extra}"
+        " --INPUT {snakemake.input}"
+        " --TMP_DIR {tmpdir}"
+        " --OUTPUT {snakemake.output.txt}"
+        " --Histogram_FILE {snakemake.output.pdf}"
+        " {log}"
+    )
--- bio/picard/collecttargetedpcrmetrics/wrapper.py
+++ bio/picard/collecttargetedpcrmetrics/wrapper.py
@@ -4,6 +4,7 @@ __email__ = "patrik.smeds@mail.com"
 __license__ = "MIT"
 
 
+import tempfile
 from snakemake.shell import shell
 from snakemake_wrapper_utils.java import get_java_opts
 
@@ -13,12 +14,14 @@ log = snakemake.log_fmt_shell()
 extra = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
 
-shell(
-    "picard CollectTargetedPcrMetrics "
-    "{java_opts} {extra} "
-    "INPUT={snakemake.input.bam} "
-    "OUTPUT={snakemake.output[0]} "
-    "AMPLICON_INTERVALS={snakemake.input.amplicon_intervals} "
-    "TARGET_INTERVALS={snakemake.input.target_intervals} "
-    "{log}"
-)
+with tempfile.TemporaryDirectory() as tmpdir:
+    shell(
+        "picard CollectTargetedPcrMetrics"
+        " {java_opts} {extra}"
+        " --INPUT {snakemake.input.bam}"
+        " --TMP_DIR {tmpdir}"
+        " --OUTPUT {snakemake.output[0]}"
+        " --AMPLICON_INTERVALS {snakemake.input.amplicon_intervals}"
+        " --TARGET_INTERVALS {snakemake.input.target_intervals}"
+        " {log}"
+    )
