fedc09f98ccb281e256ca5cd57b16d2f96c0d933
fgvieira <fgarrettvieira@gmail.com>
2022-02-08 22:30:28 +0100

    Added handling of tempdir

--- bio/subread/featurecounts/wrapper.py
+++ bio/subread/featurecounts/wrapper.py
@@ -3,6 +3,7 @@ __copyright__ = "Copyright 2020, Antonie Vietor"
 __email__ = "antonie.v@gmx.de"
 __license__ = "MIT"
 
+import tempfile
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
@@ -11,25 +12,23 @@ extra = snakemake.params.get("extra", "")
 # optional input files and directories
 fasta = snakemake.input.get("fasta", "")
 chr_names = snakemake.input.get("chr_names", "")
-tmp_dir = snakemake.params.get("tmp_dir", "")
 r_path = snakemake.params.get("r_path", "")
 
 if fasta:
     extra += " -G {}".format(fasta)
 if chr_names:
     extra += " -A {}".format(chr_names)
-if tmp_dir:
-    extra += " --tmpDir {}".format(tmp_dir)
 if r_path:
     extra += " --Rpath {}".format(r_path)
 
-shell(
-    "(featureCounts"
-    " {extra}"
-    " -T {snakemake.threads}"
-    " -J"
-    " -a {snakemake.input.annotation}"
-    " -o {snakemake.output[0]}"
-    " {snakemake.input.sam})"
-    " {log}"
-)
+with tempfile.TemporaryDirectory() as tmpdir:    
+    shell(
+        "featureCounts"
+        " -T {snakemake.threads}"
+        " -a {snakemake.input.annotation}"
+        " {extra}"
+        " --tmpDir {tmpdir}"
+        " -o {snakemake.output[0]}"
+        " {snakemake.input.samples}"
+        " {log}"
+    )
