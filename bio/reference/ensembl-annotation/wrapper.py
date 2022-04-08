3e93fbc1d73afb69fc27365a5ed91469f3260fd6
fgvieira <fgarrettvieira@gmail.com>
2022-04-08 14:34:36 +0200

    Added support for gzip output files

--- bio/reference/ensembl-annotation/wrapper.py
+++ bio/reference/ensembl-annotation/wrapper.py
@@ -7,27 +7,37 @@ import subprocess
 import sys
 from snakemake.shell import shell
 
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+
+
 species = snakemake.params.species.lower()
+build = snakemake.params.build
 release = int(snakemake.params.release)
 fmt = snakemake.params.fmt
-build = snakemake.params.build
-flavor = snakemake.params.get("flavor", "")
+
 
 branch = ""
 if release >= 81 and build == "GRCh37":
     # use the special grch37 branch for new releases
     branch = "grch37/"
 
+
+flavor = snakemake.params.get("flavor", "")
 if flavor:
     flavor += "."
 
-log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 suffix = ""
-if fmt == "gtf":
+if fmt == "gtf" or fmt == "gtf.gz":
     suffix = "gtf.gz"
-elif fmt == "gff3":
+elif fmt == "gff3" or fmt == "gff3.gz":
     suffix = "gff3.gz"
+else:
+    raise ValueError(
+        "invalid format specified. Only 'gtf[.gz]' and 'gff3[.gz]' are currently supported."
+    )
+
 
 url = "ftp://ftp.ensembl.org/pub/{branch}release-{release}/{fmt}/{species}/{species_cap}.{build}.{release}.{flavor}{suffix}".format(
     release=release,
@@ -40,8 +50,12 @@ url = "ftp://ftp.ensembl.org/pub/{branch}release-{release}/{fmt}/{species}/{spec
     branch=branch,
 )
 
+
 try:
-    shell("(curl -L {url} | gzip -d > {snakemake.output[0]}) {log}")
+    if fmt.endswith(".gz"):
+        shell("(curl -L {url} > {snakemake.output[0]}) {log}")
+    else:
+        shell("(curl -L {url} | gzip -d > {snakemake.output[0]}) {log}")
 except subprocess.CalledProcessError as e:
     if snakemake.log:
         sys.stderr = open(snakemake.log[0], "a")
