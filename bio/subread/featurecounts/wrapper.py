6ed8ca4053831a585b4adc22e257b8cf8f4fc6b0
fgvieira <fgarrettvieira@gmail.com>
2022-04-08 12:57:01 +0200

    Added specific param for strandness

--- bio/subread/featurecounts/wrapper.py
+++ bio/subread/featurecounts/wrapper.py
@@ -10,22 +10,30 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
 
 # optional input files and directories
-fasta = snakemake.input.get("fasta", "")
-chr_names = snakemake.input.get("chr_names", "")
-r_path = snakemake.params.get("r_path", "")
+strand = snakemake.params.get("strand", 0)
 
+fasta = snakemake.input.get("fasta", "")
 if fasta:
-    extra += " -G {}".format(fasta)
+    fasta = f"-G {fasta}"
+
+chr_names = snakemake.input.get("chr_names", "")
 if chr_names:
-    extra += " -A {}".format(chr_names)
+    chr_names = f"-A {chr_names}"
+
+r_path = snakemake.params.get("r_path", "")
 if r_path:
-    extra += " --Rpath {}".format(r_path)
+    r_path = f"--Rpath {r_path}"
+
 
 with tempfile.TemporaryDirectory() as tmpdir:
     shell(
         "featureCounts"
         " -T {snakemake.threads}"
+        " -s {strand}"
         " -a {snakemake.input.annotation}"
+        " {fasta}"
+        " {chr_names}"
+        " {r_path}"
         " {extra}"
         " --tmpDir {tmpdir}"
         " -o {snakemake.output[0]}"
