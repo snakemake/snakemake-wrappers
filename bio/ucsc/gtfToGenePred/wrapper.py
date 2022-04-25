46bb5c7bee0ea5476b90521be57d5e0714e0addc
fgvieira <fgarrettvieira@gmail.com>
2022-04-25 14:38:51 +0200

    Fixed default conversion

--- bio/ucsc/gtfToGenePred/wrapper.py
+++ bio/ucsc/gtfToGenePred/wrapper.py
@@ -8,15 +8,19 @@ from snakemake.shell import shell
 
 
 extra = snakemake.params.get("extra", "")
-convert_out = snakemake.params.get("convert_out")
+convert_out = snakemake.params.get("convert_out", "raw")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 
 pipes = ""
-if convert_out == "PicardCollectRnaSeqMetrics":
+if convert_out == "raw":
+    pipes = ""
+elif convert_out == "PicardCollectRnaSeqMetrics":
     pipes += " | csvcut -t -c 12,1-10 | csvformat -T"
 else:
-    raise ValueError(f"Unsupported conversion mode {convert_out}. Please check wrapper documentation.")
+    raise ValueError(
+        f"Unsupported conversion mode {convert_out}. Please check wrapper documentation."
+    )
 
 
 shell(
