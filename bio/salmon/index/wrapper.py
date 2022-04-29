90454074225d0d152a78a889129fe27e1b0120e4
tdayris <thibault.dayris@outlook.fr>
2022-04-29 11:05:00 +0200

    update salmon version, wrappers and documentation

--- bio/salmon/index/wrapper.py
+++ bio/salmon/index/wrapper.py
@@ -6,11 +6,24 @@ __email__ = "ntpierce@gmail.com"
 __license__ = "MIT"
 
 from snakemake.shell import shell
+from tempfile import TemporaryDirectory
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
 
-shell(
-    "salmon index -t {snakemake.input} -i {snakemake.output} "
-    " --threads {snakemake.threads} {extra} {log}"
-)
+decoys = ""
+if "decoys" in snakemake.input.keys():
+    decoys = "--decoys {}".format(snakemake.input["decoys"])
+
+resources_tmp = snakemake.resources.get("tmpdir", None)
+with TemporaryDirectory(dir=resources_tmp) as tempdir:
+    shell(
+        "salmon index "
+        "--transcripts {snakemake.input.sequences} "
+        "--index {snakemake.output} "
+        "--threads {snakemake.threads} "
+        "--tmpdir {tempdir} "
+        "{decoys} "
+        "{extra} "
+        "{log}"
+    )
