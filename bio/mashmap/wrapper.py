2e080600b2dc6a01630f83843f847cee442e080e
tdayris <thibault.dayris@outlook.fr>
2022-05-05 10:59:50 +0200

    mashmap initial commit

---
+++ bio/mashmap/wrapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.8
+# coding: utf-8
+
+""" Snakemake wrapper for MashMap """
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Copyright 2022, Thibault Dayris"
+__email__ = "thibault.dayris@gustaveroussy.fr"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+extra = snakemake.params.get("extra", "")
+
+# Handling input file types (either a fasta file, or a text file with a list of paths to fasta files)
+ref = snakemake.input["ref"]
+if ref.endswith((".fa", ".fna")):
+    ref = f"--ref {ref}"
+else:
+    ref = f"--refList {ref}"
+
+shell(
+    "mashmap "
+    "{ref} "
+    "--query {snakemake.input.query} "
+    "--output {snakemake.output} "
+    "--threads {snakemake.threads} "
+    "{extra} "
+    "{log}"
+)
