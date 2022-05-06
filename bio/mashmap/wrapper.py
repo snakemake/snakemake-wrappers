65d20e7aaf66128e5e8f3fc41de3a0a8f9f4f10a
tdayris <thibault.dayris@outlook.fr>
2022-05-06 09:47:54 +0200

    Gzipped fasta support + query list as text file

--- bio/mashmap/wrapper.py
+++ bio/mashmap/wrapper.py
@@ -12,18 +12,36 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
+max_threads = snakemake.threads
 
 # Handling input file types (either a fasta file, or a text file with a list of paths to fasta files)
 ref = snakemake.input["ref"]
-if ref.endswith((".fa", ".fna", ".fasta")):
-    ref = f"--ref {ref}"
-else:
+if ref.endswith(".txt"):
     ref = f"--refList {ref}"
+elif ref.endswith(".gz"):
+    ref = f"--ref <(gzip --decompress --stdout {ref})"
+    max_threads -= 1
+else:
+    ref = f"--ref {ref}"
+
+if max_threads < 1:
+    raise ValueError(
+        "Reference fasta on-the-fly g-unzipping consumed one thread."
+        f" Please increase the number of available threads by {1 - num_threads}."
+    )
+
+
+# Handling query file format (either a fastq file or a text file with a list of fastq files)
+query = snakemake.input["query"]
+if query.endswith(".txt"):
+    query = f"--queryList {query}"
+else:
+    query = f"--query {query}"
 
 shell(
     "mashmap "
     "{ref} "
-    "--query {snakemake.input.query} "
+    "{query} "
     "--output {snakemake.output} "
     "--threads {snakemake.threads} "
     "{extra} "
