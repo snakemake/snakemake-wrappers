7e12af5bf156e153b7af8fb9f16f6ec00cc4b0d4
Patrik Smeds <patrik.smeds@gmail.com>
2022-05-12 15:23:41 +0200

    feat: make it possible to set input as files or dirs to multiqc
    make it possible to use the actual input from the rule as input to multiqc, instead of converting it to dirs that are then searched.

--- bio/multiqc/wrapper.py
+++ bio/multiqc/wrapper.py
@@ -11,17 +11,26 @@ from os import path
 from snakemake.shell import shell
 
 
-input_dirs = set(path.dirname(fp) for fp in snakemake.input)
+extra = snakemake.params.get("extra", "")
+# Set this to False if multiqc should use the actual input directly
+# instead of parsing the folders where the provided files are located
+convert_file_input_to_dirs = snakemake.params.get("use_files_as_input", True)
+
+if convert_file_input_to_dirs:
+    input_data = set(path.dirname(fp) for fp in snakemake.input)
+else:
+    input_data = set(snakemake.input)
+
 output_dir = path.dirname(snakemake.output[0])
 output_name = path.basename(snakemake.output[0])
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
     "multiqc"
-    " {snakemake.params}"
+    " {extra}"
     " --force"
     " -o {output_dir}"
     " -n {output_name}"
-    " {input_dirs}"
+    " {input_data}"
     " {log}"
 )
