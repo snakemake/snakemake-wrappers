53ffdee911797ffdf6b9cfc37b9851c00551d571
David Laehnemann <david.laehnemann@hhu.de>
2022-06-17 12:02:12 +0200

    fix: allow for arbitrary filename prefixes in the optitype wrapper

--- bio/optitype/wrapper.py
+++ bio/optitype/wrapper.py
@@ -10,6 +10,7 @@ from snakemake.shell import shell
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 outdir = os.path.dirname(snakemake.output[0])
+prefix = os.path.basename(snakemake.output[0]).replace("_coverage_plot.pdf", "").replace("_result.tsv","")
 
 # get sequencing type
 seq_type = snakemake.params.get("sequencing_type", "dna")
@@ -24,7 +25,7 @@ shell(
     "(OptiTypePipeline.py"
     " --input {snakemake.input.reads}"
     " --outdir {outdir}"
-    " --prefix {snakemake.wildcards.sample}"
+    " --prefix {prefix}"
     " {seq_type}"
     " {config}"
     " {extra})"
