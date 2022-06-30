9f334650eff218fc26bd203cde9b2396c935efc8
ljmesi <37740329+ljmesi@users.noreply.github.com>
2022-06-30 15:23:31 +0200

    refactor: simplify getting output file basename

--- bio/gatk/depthofcoverage/wrapper.py
+++ bio/gatk/depthofcoverage/wrapper.py
@@ -6,17 +6,14 @@ __license__ = "MIT"
 import tempfile
 from snakemake.shell import shell
 from snakemake_wrapper_utils.java import get_java_opts
-from pathlib import Path
+from os import path
 
 java_opts = get_java_opts(snakemake)
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 # Extract basename from the output file names
-out_file = [
-    Path(o) for o in snakemake.output if Path(o).suffix == ".sample_interval_summary"
-][0]
-out_basename = str(out_file.parent / out_file.stem)
+out_basename = path.commonprefix(snakemake.output).rstrip(".")
 
 with tempfile.TemporaryDirectory() as tmpdir:
     shell(
