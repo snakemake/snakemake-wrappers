d074c010b5e184d206ed45ccd712b3c834e5d710
tdayris <tdayris@gustaveroussy.fr>
2022-07-12 13:24:37 +0200

    Update test case

--- test.py
+++ test.py
@@ -3290,6 +3290,18 @@ def test_gatk_filtermutectcalls():
         ],
     )
 
+    run(
+        "bio/gatk/filtermutectcalls",
+        [
+            "snakemake",
+            "--cores",
+            "1",
+            "calls/snvs.mutect.filtered.b.vcf",
+            "--use-conda",
+            "-F",
+        ],
+    )
+
 
 @skip_if_not_modified
 def test_gatk_selectvariants():
