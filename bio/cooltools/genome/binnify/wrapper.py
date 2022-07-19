5acd02dc00283924e738dd86d616f97d7b1eb466
Phlya <flyamer@gmail.com>
2022-07-19 17:08:37 +0200

    Small fixes in gc and binnify wrappers

--- bio/cooltools/genome/binnify/wrapper.py
+++ bio/cooltools/genome/binnify/wrapper.py
@@ -8,9 +8,8 @@ from snakemake.shell import shell
 ## Extract arguments
 binsize = snakemake.params.get("binsize", snakemake.wildcards.get("binsize", 0))
 if not binsize:
-    raise ValueError("Please specify resolution either as a wildcard or as a parameter")
-if not binsize:
-    raise ValueError("binsize is required")
+    raise ValueError("Please specify binsize either as a wildcard or as a parameter")
+
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
--- bio/cooltools/genome/gc/wrapper.py
+++ bio/cooltools/genome/gc/wrapper.py
@@ -6,14 +6,10 @@ __license__ = "MIT"
 from snakemake.shell import shell
 
 ## Extract arguments
-bins = snakemake.input.get("bins", "")
-if not bins:
-    raise ValueError("bins are required")
-fasta = snakemake.input.get("fasta", "")
-if not fasta:
-    raise ValueError("fasta is required")
 extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
-
-shell("(cooltools genome gc {bins} {fasta} {extra} > {snakemake.output}) {log}")
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+shell(
+    "(cooltools genome gc"
+    " {snakemake.input.bins} {snakemake.input.fasta} {extra} > {snakemake.output})"
+    " {log} "
+)
