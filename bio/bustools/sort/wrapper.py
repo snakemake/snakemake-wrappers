8a02633b7bcb0aec79a37de879e01dcb727a0304
tdayris <thibault.dayris@gustaveroussy.fr>
2022-07-20 23:36:59 +0200

    memory and tempdir used

--- bio/bustools/sort/wrapper.py
+++ bio/bustools/sort/wrapper.py
@@ -9,6 +9,8 @@ __email__ = "thibault.dayris@gustaveroussy.fr"
 __license__ = "MIT"
 
 from snakemake.shell import shell
+from tempfile import TemporaryDirectory
+from snakemake_wrapper_utils.snakemake import get_mem
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
@@ -17,13 +19,16 @@ bus_files = snakemake.input
 if isinstance(bus_files, list):
     bus_files = " ".join(bus_files)
 
+mem = get_mem(snakemake, "MiB")
 
-shell(
-    "bustools sort "
-    "--memory {snakemake.resources.mem_mb} "
-    "--temp {snakemake.resources.tmpdir} "
-    "--threads {snakemake.threads} "
-    "--output {snakemake.output[0]} "
-    "{bus_files} "
-    "{log}"
-)
+
+with TemporaryDirectory() as tempdir:
+    shell(
+        "bustools sort "
+        "--memory {mem} "
+        "--temp {tmpdir} "
+        "--threads {snakemake.threads} "
+        "--output {snakemake.output[0]} "
+        "{bus_files} "
+        "{log}"
+    )
