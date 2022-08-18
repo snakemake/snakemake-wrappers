27c7239e95ef26fac8b59cf54185ffebd90b93c9
fgvieira <fgarrettvieira@gmail.com>
2022-08-18 12:34:30 +0200

    Updated wrapper-utils

--- bio/delly/wrapper.py
+++ bio/delly/wrapper.py
@@ -4,34 +4,28 @@ __email__ = "koester@jimmy.harvard.edu"
 __license__ = "MIT"
 
 
-import tempfile
 from snakemake.shell import shell
 from snakemake_wrapper_utils.bcftools import get_bcftools_opts
 
 
-bcftools_opts = get_bcftools_opts(snakemake, parse_memory=False)
+bcftools_opts = get_bcftools_opts(snakemake, parse_ref=False, parse_memory=False)
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 
-exclude = (
-    "-x {}".format(snakemake.input.exclude)
-    if snakemake.input.get("exclude", "")
-    else ""
+exclude = snakemake.input.get("exclude", "")
+if exclude:
+    exclude = f"-x {exclude}"
+
+
+shell(
+    "(OMP_NUM_THREADS={snakemake.threads} delly call"
+    " -g {snakemake.input.ref}"
+    " {exclude}"
+    " {extra}"
+    " {snakemake.input.alns} | "
+    # Convert output to specified format
+    "bcftools view"
+    " {bcftools_opts}"
+    ") {log}"
 )
-
-
-with NamedTemporaryFile().name as tmpout:
-    shell(
-        "(OMP_NUM_THREADS={snakemake.threads} delly call"
-        " -g {snakemake.input.ref}"
-        " -o {tmpout}"
-        " {exclude}"
-        " {extra}"
-        " {snakemake.input[0]};"
-        # Convert output to specified format
-        "bcftools view"
-        " {bcftools_opts}"
-        " {tmpout}"
-        ") {log}"
-    )
