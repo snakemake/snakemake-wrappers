6d9e111f3bf8a92111e16febde82b897bd80dbf3
Niklas Mähler <niklas.mahler@gmail.com>
2022-10-13 12:45:09 +0200

    feat: have bed files as input files

--- bio/pindel/call/wrapper.py
+++ bio/pindel/call/wrapper.py
@@ -3,45 +3,33 @@ __copyright__ = "Copyright 2016, Johannes Köster"
 __email__ = "koester@jimmy.harvard.edu"
 __license__ = "MIT"
 
-import os
 from snakemake.shell import shell
-import sys
 
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+include_bed = snakemake.input.get("include_bed", None)
+exclude_bed = snakemake.input.get("exclude_bed", None)
 
-def check_file_exists(filename):
-    if filename is not None and not os.path.exists(filename):
-        raise FileNotFoundError(filename)
+if include_bed is not None and exclude_bed is not None:
+    raise Exception("supply either include_bed or exclude_bed, not both")
 
+include_arg = ""
+if include_bed is not None:
+    include_arg = "-j {}".format(include_bed)
 
-def check_bedfiles(argstring):
-    include_bedfile = None
-    exclude_bedfile = None
-    extra_params = argstring.split()
+exclude_arg = ""
+if exclude_bed is not None:
+    exclude_arg = "-J {}".format(exclude_bed)
 
-    # Get bed file arguments, if supplied
-    for i, arg in enumerate(extra_params):
-        if arg == "-j" or arg == "--include":
-            include_bedfile = extra_params[i + 1]
-            continue
-        if arg == "-J" or arg == "--exclude":
-            exclude_bedfile = extra_params[i + 1]
-            continue
-
-    check_file_exists(include_bedfile)
-    check_file_exists(exclude_bedfile)
-
-
-try:
-    check_bedfiles(extra)
-except FileNotFoundError as e:
-    with open(str(snakemake.log), "a") as logfile:
-        print("error: file not found: {}".format(e), file=logfile)
-    sys.exit(1)
 
 shell(
-    "pindel -T {snakemake.threads} {snakemake.params.extra} -i {snakemake.input.config} "
-    "-f {snakemake.input.ref} -o {snakemake.params.prefix} {log}"
+    "pindel "
+    "-T {snakemake.threads} "
+    "{extra} "
+    "{include_arg} "
+    "{exclude_arg} "
+    "-i {snakemake.input.config} "
+    "-f {snakemake.input.ref} "
+    "-o {snakemake.params.prefix} {log}"
 )
--- test.py
+++ test.py
@@ -2765,9 +2765,7 @@ def test_pindel_call_include():
             "snakemake",
             "--cores",
             "1",
-            "pindel/all_D",
-            "pindel/all_j_D",
-            "pindel/all_include_D",
+            "pindel/all_included_D",
             "--use-conda",
             "-F",
         ],
@@ -2786,88 +2784,10 @@ def test_pindel_call_exclude():
             "snakemake",
             "--cores",
             "1",
-            "pindel/all_D",
-            "pindel/all_J_D",
-            "pindel/all_exclude_D",
+            "pindel/all_excluded_D",
             "--use-conda",
             "-F",
         ],
-    )
-
-
-@skip_if_not_modified
-def test_pindel_call_j_fail():
-    def check_log(log):
-        assert "error: file not found: missing_regions.bed" in log
-
-    run(
-        "bio/pindel/call",
-        [
-            "snakemake",
-            "--cores",
-            "1",
-            "pindel/all_j_fails_D",
-            "--use-conda",
-            "-F",
-        ],
-        check_log=check_log,
-    )
-
-
-@skip_if_not_modified
-def test_pindel_call_include_fail():
-    def check_log(log):
-        assert "error: file not found: missing_regions.bed" in log
-
-    run(
-        "bio/pindel/call",
-        [
-            "snakemake",
-            "--cores",
-            "1",
-            "pindel/all_include_fails_D",
-            "--use-conda",
-            "-F",
-        ],
-        check_log=check_log,
-    )
-
-
-@skip_if_not_modified
-def test_pindel_call_J_fail():
-    def check_log(log):
-        assert "error: file not found: missing_regions.bed" in log
-
-    run(
-        "bio/pindel/call",
-        [
-            "snakemake",
-            "--cores",
-            "1",
-            "pindel/all_J_fails_D",
-            "--use-conda",
-            "-F",
-        ],
-        check_log=check_log,
-    )
-
-
-@skip_if_not_modified
-def test_pindel_call_exclude_fail():
-    def check_log(log):
-        assert "error: file not found: missing_regions.bed" in log
-
-    run(
-        "bio/pindel/call",
-        [
-            "snakemake",
-            "--cores",
-            "1",
-            "pindel/all_exclude_fails_D",
-            "--use-conda",
-            "-F",
-        ],
-        check_log=check_log,
     )
 
 
