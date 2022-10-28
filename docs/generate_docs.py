728ab4470fac7a51cdc58c4120abfeb2034bd81c
Johannes Köster <johannes.koester@uni-due.de>
2022-10-28 11:20:18 +0200

    docs: fix source rendering

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -66,8 +66,8 @@ def render_snakefile(path):
     with open(os.path.join(path, "test", "Snakefile")) as snakefile:
         lines = filter(lambda line: re.search(r"# ?\[hide\]", line) is None, snakefile)
         snakefile = textwrap.indent(
-            "\n".join(l.rstrip() for l in lines), "    "
-        ).replace("master", TAG).strip()
+            "\n".join(l.rstrip() for l in lines).strip(), "    "
+        ).replace("master", TAG)
         return snakefile
 
 
