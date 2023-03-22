62ed822f063ed67de8bd6626f255c05fa865b2c5
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2023-03-22 09:48:55 +0100

    Automatic inference of output file

--- bio/vsearch/wrapper.py
+++ bio/vsearch/wrapper.py
@@ -6,6 +6,8 @@ from snakemake.shell import shell
 
 
 extra = snakemake.params.get("extra", "")
+if snakemake.log:
+    log = f"--log {snakemake.log}"
 
 
 fasta_arg = snakemake.params.get("fasta_arg", "")
@@ -23,13 +25,22 @@ if db:
     db = f"--db {db}"
 
 
-out_arg = snakemake.params.get("out_arg", "")
-if out_arg:
-    out_arg = out_arg.replace("{", "{snakemake.")
+out_list = list()
+for key, value in snakemake.output.items():
+    if value.endswith(".gz"):
+        out_list.append(f"--{key} /dev/stdout | gzip > {value}")
+    elif value.endswith(".bz2"):
+        out_list.append(f"--{key} /dev/stdout | bzip2 > {value}")
+    else:
+        out_list.append(f"--{key} {value}")
+ 
+# Check which output files are to be compressed
+out_gz = [out.endswith(".gz") for out in out_list]
+out_bz2 = [out.endswith(".bz2") for out in out_list]
+assert sum(out_gz + out_bz2) <= 1, "only one output can be compressed"
 
-
-if snakemake.log:
-    log = f"--log {snakemake.log}"
+# Move compressed file (if any) to last
+output = [out for _, out in sorted(zip(out_gz or out_bz2, out_list))]
 
 
 shell(
@@ -39,5 +50,5 @@ shell(
     " {db}"
     " {extra}"
     " {log}"
-    " " + out_arg
+    " {output}"
 )
