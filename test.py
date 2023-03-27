e2215f2e3cc18ed0ee165a440c3bb1752babab59
tgroth97 <tobingroth1@gmail.com>
2023-03-27 12:14:34 -0700

    feat: Lofreq indelqual wrapper (#1166)
    ### Description
    Snakemake wrapper for the tool lofreq. Specifically the lofreq indelqual
    portion of the tool.
    
    ### QC
    
    * [x] I confirm that:
    
    For all wrappers added by this PR, 
    
    * there is a test case which covers any introduced changes,
    * `input:` and `output:` file paths in the resulting rule can be changed
    arbitrarily,
    * either the wrapper can only use a single core, or the example rule
    contains a `threads: x` statement with `x` being a reasonable default,
    * rule names in the test case are in
    [snake_case](https://en.wikipedia.org/wiki/Snake_case) and somehow tell
    what the rule is about or match the tools purpose or name (e.g.,
    `map_reads` for a step that maps reads),
    * all `environment.yaml` specifications follow [the respective best
    practices](https://stackoverflow.com/a/64594513/2352071),
    * wherever possible, command line arguments are inferred and set
    automatically (e.g. based on file extensions in `input:` or `output:`),
    * all fields of the example rules in the `Snakefile`s and their entries
    are explained via comments (`input:`/`output:`/`params:` etc.),
    * `stderr` and/or `stdout` are logged correctly (`log:`), depending on
    the wrapped tool,
    * temporary files are either written to a unique hidden folder in the
    working directory, or (better) stored where the Python function
    `tempfile.gettempdir()` points to (see
    [here](https://docs.python.org/3/library/tempfile.html#tempfile.gettempdir);
    this also means that using any Python `tempfile` default behavior
    works),
    * the `meta.yaml` contains a link to the documentation of the respective
    tool or command,
    * `Snakefile`s pass the linting (`snakemake --lint`),
    * `Snakefile`s are formatted with
    [snakefmt](https://github.com/snakemake/snakefmt),
    * Python wrapper scripts are formatted with
    [black](https://black.readthedocs.io).
    * Conda environments use a minimal amount of channels, in recommended
    ordering. E.g. for bioconda, use (conda-forge, bioconda, nodefaults, as
    conda-forge should have highest priority and defaults channels are
    usually not needed because most packages are in conda-forge nowadays).
    
    ---------
    
    Co-authored-by: Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>

--- test.py
+++ test.py
@@ -138,7 +138,20 @@ def run(wrapper, cmd, check_log=None):
             # go back to original directory
             os.chdir(origdir)
 
+@skip_if_not_modified
+def test_indelqual():
+    run(
+        "bio/lofreq/indelqual",
+        [
+            "snakemake",
+            "--cores",
+            "1",
+            "--use-conda",
+            "-F",
+            "out/indelqual/a.indel.bam"
 
+        ]
+    )
 
 @skip_if_not_modified
 def test_loglog():
