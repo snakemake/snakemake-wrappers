1c9987155288f55e10d00eed6534b9ff6519d715
Austin Hovland <austin@sansterbioanalytics.com>
2023-03-27 02:06:14 +0000

    fix: Allow for large assets in refgenie

--- bio/refgenie/wrapper.py
+++ bio/refgenie/wrapper.py
@@ -15,7 +15,8 @@ conf_path = os.environ["REFGENIE"]
 rgc = refgenconf.RefGenConf(conf_path, writable=True)
 
 # pull asset if necessary
-gat, archive_data, server_url = rgc.pull(genome, asset, tag, force=False)
+# BUG Times out when pulling large assets
+gat, archive_data, server_url = rgc.pull(genome, asset, tag, force=False, force_large=True)
 
 for seek_key, out in snakemake.output.items():
     path = rgc.seek(genome, asset, tag_name=tag, seek_key=seek_key, strict_exists=True)
