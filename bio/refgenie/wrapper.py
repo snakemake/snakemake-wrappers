4d3758c0048e0cecdd4ea96c588698f7857641e3
Austin Hovland <austin@sansterbioanalytics.com>
2023-03-28 00:17:17 +0000

    fix: handle refgenconf write lock error

--- bio/refgenie/wrapper.py
+++ bio/refgenie/wrapper.py
@@ -5,6 +5,7 @@ __license__ = "MIT"
 
 import os
 import refgenconf
+from refgenconf.exceptions import RefgenconfError
 
 genome = snakemake.params.genome
 asset = snakemake.params.asset
@@ -12,10 +13,17 @@ tag = snakemake.params.tag
 
 conf_path = os.environ["REFGENIE"]
 
-rgc = refgenconf.RefGenConf(conf_path, writable=False)
-
+# BUG If there are multiple concurrent refgenie commands, this will fail due to
+# unable to acquire lock of the config file.
+try:
+    rgc = refgenconf.RefGenConf(conf_path, writable=True)
+except RefgenconfError:
+    # If read lock timeout, attempt to skip the read lock
+    rgc = refgenconf.RefGenConf(conf_path,
+                                writable=True,
+                                skip_read_lock=True,
+                                genome_exact=False)
 # pull asset if necessary
-# BUG Times out when pulling large assets
 gat, archive_data, server_url = rgc.pull(genome, asset, tag, force=False, force_large=True)
 
 for seek_key, out in snakemake.output.items():
