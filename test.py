b4d9426810ca6ccffcee35ad470b606698e1d205
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-04-10 23:45:07 +0200

    Code clean-up

--- test.py
+++ test.py
@@ -2436,8 +2436,6 @@ def test_freebayes_bcf():
                 "calls/a.bcf",
                 "--use-conda",
                 "-F",
-                "-s",
-                "Snakefile_bcf",
             ],
         )
 
@@ -2451,11 +2449,9 @@ def test_freebayes_bed():
                 "snakemake",
                 "--cores",
                 str(c),
-                "calls/a.bcf",
+                "calls/a.vcf.gz",
                 "--use-conda",
                 "-F",
-                "-s",
-                "Snakefile_bed",
             ],
         )
 
