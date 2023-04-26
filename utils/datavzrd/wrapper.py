669f6ed34c89e1cbbaf6798c28d323ca3f737487
Felix Mölder <felix.moelder@uni-due.de>
2023-04-26 09:16:34 +0200

    feat: Update datavzrd 2.18.4

--- utils/datavzrd/wrapper.py
+++ utils/datavzrd/wrapper.py
@@ -7,4 +7,4 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-shell("datavzrd {snakemake.input.config} --output {snakemake.output[0]} {log}")
+shell("datavzrd {snakemake.input.config} {snakemake.params.extra} --output {snakemake.output[0]} {log}")
