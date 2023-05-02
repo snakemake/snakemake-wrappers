a8411be8daab71ff3dddeeece3e28253ab4a890b
Silas Kieser <silas.kieser@gmail.com>
2023-05-02 16:04:34 +0200

    undo wrong tests

--- test.py
+++ test.py
@@ -210,21 +210,6 @@ def test_tadpole():
     )
 
 
-@skip_if_not_modified
-def test_loglog():
-    run(
-        "bio/bbtools/generic",
-        [
-            "snakemake",
-            "--cores",
-            "2",
-            "--use-conda",
-            "-F",
-            "logs/se/a.log",
-            "logs/pe/a.log",
-        ],
-    )
-
 
 @skip_if_not_modified
 def test_bbgeneric():
@@ -253,10 +238,7 @@ def test_seqkit_stats():
             "2",
             "--use-conda",
             "-F",
-            "--resources mem_mb=1000",
-            "covstats/sample1.tsv",
-            "covstats/sample2.tsv",
-            "logs/bbmap/sample1.out"
+            "out/stats/a.tsv",
         ],
     )
 
