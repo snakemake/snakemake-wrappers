836f8aaaac1e5229c6f2b68b0472add6aad43036
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-05-10 11:46:01 +0200

    Fixed bug

--- bio/gatk/denoisereadcounts/wrapper.py
+++ bio/gatk/denoisereadcounts/wrapper.py
@@ -9,14 +9,15 @@ from snakemake.shell import shell
 from snakemake_wrapper_utils.java import get_java_opts
 
 
-panel_of_normal = ""
-if snakemake.input.get("pon", None):
-    panel_of_normal = "--count-panel-of-normals {snakemake.input.pon}"
+pon = snakemake.input.get("pon", "")
+if pon:
+    pon = f"--count-panel-of-normals {snakemake.input.pon}"
 
 
-gc_intervals = ""
-if snakemake.input.get("gc_interval", None):
-    gc_intervals = "--annotated-intervals {snakemake.input.gc_interval}"
+gc_interval = snakemake.input.get("gc_interval", "")
+if gc_interval:
+    gc_interval = f"--annotated-intervals {snakemake.input.gc_interval}"
+
 
 extra = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
@@ -26,12 +27,12 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 with tempfile.TemporaryDirectory() as tmpdir:
     shell(
         "gatk --java-options '{java_opts}' DenoiseReadCounts"
-        " -I {snakemake.input.hdf5} "
-        " {panel_of_normal}"
-        " {gc_intervals}"
+        " --input {snakemake.input.hdf5}"
+        " {pon}"
+        " {gc_interval}"
+        " {extra}"
         " --standardized-copy-ratios {snakemake.output.std_copy_ratio}"
         " --denoised-copy-ratios {snakemake.output.denoised_copy_ratio}"
         " --tmp-dir {tmpdir}"
-        " {extra}"
         " {log}"
     )
