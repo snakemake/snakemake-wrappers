140f3705a6b6fd41e5217cd45866fc683d088256
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-06-12 15:55:09 +0200

    Fix issues

--- bio/seqkit/wrapper.py
+++ bio/seqkit/wrapper.py
@@ -11,21 +11,25 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 extra_input = " ".join(
     [
-        f"--{key} {value}" if key in ["bed", "gtf"] else f"--{key}-file {value}"
+        f"--{key.replace('_','-')} {value}"
+        if key in ["bed", "gtf"]
+        else f"--{key.replace('_','-')}-file {value}"
         for key, value in snakemake.input.items()
     ][1:]
 )
 
 extra_output = " ".join(
     [
-        f"--{key} {value}" if key in ["read1", "read2"] else f"--{key}-file {value}"
+        f"--{key.replace('_','-')} {value}"
+        if key in ["read1", "read2"]
+        else f"--{key.replace('_','-')}-file {value}"
         for key, value in snakemake.output.items()
     ][1:]
 )
 
 
 shell(
-    "seqkit {snakemake.params.command}"
+    "seqkit {snakemake.params.cmd}"
     " --threads {snakemake.threads}"
     " {extra_input}"
     " {extra_output}"
