5ae37be394cf516cd693edd2067fcbe5aad4e01a
Curro Campuzano <campuzanocurro@gmail.com>
2023-06-17 19:38:59 +0200

    improve kingdom option

--- bio/barrnap/wrapper.py
+++ bio/barrnap/wrapper.py
@@ -10,8 +10,15 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 extra = snakemake.params.get("extra", "")
-
-assert snakemake.output.keys(), "Output must contain at least one named file."
+kingdom = snakemake.params.get("kingdom", "")
+assert kingdom in [
+    "bac",
+    "arc",
+    "euk",
+    "mito",
+], "Invalid kingdom. Valid kingdoms are: 'bac', 'arc', 'euk', 'mito'. Given: %r." % (
+    kingdom
+)
 
 valid_keys = ["gff", "fasta"]
 for key in snakemake.output.keys():
@@ -20,14 +27,16 @@ for key in snakemake.output.keys():
     ), "Invalid key in output. Valid keys are: %r. Given: %r." % (valid_keys, key)
 
 gff_out = snakemake.output.get("gff")
-fasta_out = snakemake.output.get("fasta")
+assert gff_out, "Output must contain at least a gff file."
 
+fasta_out = snakemake.output.get("fasta")
 if fasta_out:
     extra += f" -o {fasta_out}"
 
 shell(
     "barrnap"
     " --threads {snakemake.threads}"
+    " -k {kingdom}"
     " {extra}"
     " < {snakemake.input}"
     " > {gff_out}"
