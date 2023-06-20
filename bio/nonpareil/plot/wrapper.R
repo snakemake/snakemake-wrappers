f28034781c288b495dde26dc050217d616056935
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-06-20 14:27:05 +0200

    Add support for multiple curves

--- bio/nonpareil/plot/wrapper.R
+++ bio/nonpareil/plot/wrapper.R
@@ -18,54 +18,72 @@ base::message("Libraries loaded")
 
 
 params <- list("label" = ifelse("label" %in% base::names(snakemake@params), snakemake@params[["label"]], NA),
-       	       "col" = ifelse("col" %in% base::names(snakemake@params), snakemake@params[["col"]], NA),
-	       "enforce_consistency" = ifelse("enforce_consistency" %in% base::names(snakemake@params), as.logical(snakemake@params[["enforce_consistency"]]), FALSE),
-	       "star" = ifelse("star" %in% base::names(snakemake@params), snakemake@params[["star"]], 95),
-	       "correction_factor" = ifelse("correction_factor" %in% base::names(snakemake@params), as.logical(snakemake@params[["correction_factor"]]), FALSE),
-	       "weights_exp" = NA,
-	       "skip_model" = ifelse("skip_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["skip_model"]]), FALSE)
-	      )
+               "labels" = NA,
+               "col" = NA,
+               "enforce_consistency" = ifelse("enforce_consistency" %in% base::names(snakemake@params), as.logical(snakemake@params[["enforce_consistency"]]), FALSE),
+               "star" = ifelse("star" %in% base::names(snakemake@params), snakemake@params[["star"]], 95),
+               "correction_factor" = ifelse("correction_factor" %in% base::names(snakemake@params), as.logical(snakemake@params[["correction_factor"]]), FALSE),
+               "weights_exp" = NA,
+               "skip_model" = ifelse("skip_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["skip_model"]]), FALSE)
+              )
 
 # Not sure why, by using "ifelse" only keeps the first element of the vector
+if ("labels" %in% base::names(snakemake@params)) {
+   params[["labels"]] = unlist(strsplit(snakemake@params[["labels"]], ","))
+   }
+
+if ("col" %in% base::names(snakemake@params)) {
+   params[["col"]] = unlist(strsplit(snakemake@params[["col"]], ","))
+   }
+
 if ("weights_exp" %in% base::names(snakemake@params)) {
    params[["weights_exp"]] = as.numeric(unlist(strsplit(snakemake@params[["weights_exp"]], ",")))
    }
 
 
 base::message("Options provided:")
-base::print(params)
+utils::str(params)
 
 
 pdf(snakemake@output[[1]])
-curve <- Nonpareil.curve(file = snakemake@input[[1]],
-		label = params[["label"]],
-		col = params[["col"]],
-		enforce.consistency = params[["enforce_consistency"]],
-		star = params[["star"]],
-		correction.factor = params[["correction_factor"]],
-		weights.exp = params[["weights_exp"]],
-		skip.model = params[["skip_model"]],
-		plot = FALSE
-		)
+curves <- Nonpareil.curve.batch(snakemake@input[["npo"]],
+                                label = params[["label"]],
+                                labels = params[["labels"]],
+                                col = params[["col"]],
+                                enforce.consistency = params[["enforce_consistency"]],
+                                star = params[["star"]],
+                                correction.factor = params[["correction_factor"]],
+                                weights.exp = params[["weights_exp"]],
+                                skip.model = params[["skip_model"]],
+                                plot = FALSE
+                                )
 
 
-# Print	stats to log
-summary(curve)
 # Get stats
-stats <- summary(curve)
+stats <- summary(curves)
+# Fix names
+colnames(stats) <- c("Redundancy", "Avg. coverage", "Seq. effort", "Model correlation", "Required seq. effort", "Diversity")
+
+# If model not infered, set its values to NA
+stats[,4] <- sapply(stats[,4], function(x){if(length(x) == 0){NA} else {x}})
+stats[,5] <- sapply(stats[,5], function(x){if(length(x) == 0){NA} else {x}})
+
 # Convert to Gb
-stats[3] <- stats[3] / 1e9
-stats[5] <- stats[5] / 1e9
+stats[,3] <- stats[,3] / 1e9
+stats[,5] <- stats[,5] / 1e9
 # Round
 stats <- round(stats, digits = 2)
-# Fix names
-names(stats) <-	c("Redundancy", "Avg. coverage", "Seq. effort", "Model correlation", "Required seq. effort", "Diversity")
+# Print stats to log
+base::print(stats)
 
 # Save plot
-plot(curve)
+plot(curves, legend.opts = FALSE)
 
 # Add legend
-legend("bottomright", legend = paste0(paste(names(stats), stats, sep=": "), c("",""," Gb",""," Gb","")), cex = 0.5)
+legend("bottomright", legend = paste0(paste(colnames(stats), t(stats), sep=": "), c("",""," Gb",""," Gb","")), cex = 0.5)
+if (length(snakemake@input[["npo"]]) > 1) {
+  Nonpareil.legend(curves, "topleft", cex = 0.5)
+}
 
 base::message("Nonpareil plot saved")
 dev.off()
