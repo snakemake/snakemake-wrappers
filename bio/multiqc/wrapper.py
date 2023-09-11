c621f0001595ac3a82d00082c58d454a73557515
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-09-11 11:59:41 +0200

    Fix output file issues and version bump

--- bio/multiqc/wrapper.py
+++ bio/multiqc/wrapper.py
@@ -6,31 +6,34 @@ __email__ = "julianderuiter@gmail.com"
 __license__ = "MIT"
 
 
-from os import path
-
+import tempfile
+from pathlib import Path
 from snakemake.shell import shell
 
 
 extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+
 # Set this to False if multiqc should use the actual input directly
 # instead of parsing the folders where the provided files are located
 use_input_files_only = snakemake.params.get("use_input_files_only", False)
-
 if not use_input_files_only:
-    input_data = set(path.dirname(fp) for fp in snakemake.input)
+    input_data = set(Path(fp).parent for fp in snakemake.input)
 else:
     input_data = set(snakemake.input)
 
-output_dir = path.dirname(snakemake.output[0])
-output_name = path.basename(snakemake.output[0])
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-shell(
-    "multiqc"
-    " {extra}"
-    " --force"
-    " -o {output_dir}"
-    " -n {output_name}"
-    " {input_data}"
-    " {log}"
-)
+with tempfile.TemporaryDirectory() as tmpdir:
+    shell(
+        "multiqc"
+        " {extra}"
+        " --outdir {tmpdir}"
+        " --filename out"
+        " {input_data}"
+        " {log}"
+    )
+
+    for output in snakemake.output:
+        ext = Path(output).suffix
+        shell("mv {tmpdir}/out{ext} {output}")
