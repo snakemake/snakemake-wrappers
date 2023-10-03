b1357a6fd246bb270b527f1fbd5d4186b9046bc3
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-10-03 22:12:53 +0200

    Add maximum block size and remove debug print

--- bio/bwa/index/wrapper.py
+++ bio/bwa/index/wrapper.py
@@ -13,10 +13,9 @@ extra = snakemake.params.get("extra", "")
 # Prefix that should be used for the database
 prefix = snakemake.params.get("prefix", splitext(snakemake.output[0])[0])
 
-# Block size should be ~10x length of reference (https://github.com/lh3/bwa/issues/104)
-print(type(snakemake.input).__name__)
+# Block size should be a 10th of the reference length (https://github.com/lh3/bwa/issues/104)
 block_size = Path(snakemake.input[0]).stat().st_size / 1024 / 1024 / 10
-# If GZip, assuming a 4-fold compression:
+# If GZip, assume a 4-fold compression rate:
 # - https://scfbm.biomedcentral.com/articles/10.1186/s13029-019-0073-5/tables/3
 # - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7336184/
 # - https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3866555/bin/supp_btt594_supplement-rev2.pdf
@@ -24,7 +23,7 @@ block_size = Path(snakemake.input[0]).stat().st_size / 1024 / 1024 / 10
 if snakemake.input[0].endswith(".gz"):
     block_size *= 4
 
-# Ensure default minimum block size
-block_size = max(10, int(block_size))
+# Ensure minimum (10 Mb as BWA default) and maximum (50Gb since no apparent gain and to limit memory usage) block size
+block_size = min(50 * 1024, max(10, int(block_size)))
 
 shell("bwa index -b {block_size}M -p {prefix} {extra} {snakemake.input} {log}")
