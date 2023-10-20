5c5f3e69886d2163d39f8c5f39bdf6645f2a5808
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2023-10-20 13:58:56 +0200

    Fix typo

--- bio/cnvkit/export/wrapper.py
+++ bio/cnvkit/export/wrapper.py
@@ -10,12 +10,20 @@ log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 extra = snakemake.params.get("extra", "")
 
-file_extension = splitext(snakemake.output[0])[1][1:]
+
+file_name, file_extension = splitext(snakemake.output[0])
+
+if file_extension == ".gz":
+    file_extension = splitext(file_name)[1][1:]
+    output = f" | bgzip > {snakemake.output}"
+else:
+    file_extension = file_extension[1:]
+    output = f"-o {snakemake.output} "
 
 shell(
     "(cnvkit.py export {file_extension} "
     "{snakemake.input} "
-    "-o {snakemake.output} "
-    "{extra}) "
+    "{extra} "
+    "{output}) "
     "{log}"
 )
