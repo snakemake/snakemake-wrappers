063685d6dd9213d342991ab8a969bc50d3f839fd
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-12-06 08:14:48 +0100

    Add more plotting options

--- bio/nonpareil/plot/wrapper.R
+++ bio/nonpareil/plot/wrapper.R
@@ -27,37 +27,44 @@ base::print(out_pdf)
 
 
 # Set parameters
-params <- list("label" = ifelse("label" %in% base::names(snakemake@params), snakemake@params[["label"]], NA),
+params <- list("in_files" = in_files,
+               "out_pdf" = out_pdf,
                "labels" = NA,
                "col" = NA,
                "enforce_consistency" = ifelse("enforce_consistency" %in% base::names(snakemake@params), as.logical(snakemake@params[["enforce_consistency"]]), FALSE),
                "star" = ifelse("star" %in% base::names(snakemake@params), snakemake@params[["star"]], 95),
                "correction_factor" = ifelse("correction_factor" %in% base::names(snakemake@params), as.logical(snakemake@params[["correction_factor"]]), FALSE),
                "weights_exp" = NA,
-               "skip_model" = ifelse("skip_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["skip_model"]]), FALSE)
+               "skip_model" = ifelse("skip_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["skip_model"]]), FALSE),
+               "plot_observed" = ifelse("plot_observed" %in% base::names(snakemake@params), as.logical(snakemake@params[["plot_observed"]]), TRUE),
+               "plot_model" = ifelse("plot_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["plot_model"]]), TRUE),
+               "plot_dispersion" = ifelse("plot_dispersion" %in% base::names(snakemake@params), snakemake@params[["plot_dispersion"]], FALSE),
+               "plot_diversity" = ifelse("plot_diversity" %in% base::names(snakemake@params), as.logical(snakemake@params[["plot_diversity"]]), FALSE)
               )
 
 # Not sure why, by using "ifelse" only keeps the first element of the vector
 if ("labels" %in% base::names(snakemake@params)) {
-   params[["labels"]] = unlist(strsplit(snakemake@params[["labels"]], ","))
+   params[["labels"]] = snakemake@params[["labels"]]
    }
 
 if ("col" %in% base::names(snakemake@params)) {
-   params[["col"]] = unlist(strsplit(snakemake@params[["col"]], ","))
+   params[["col"]] = snakemake@params[["col"]]
    }
 
 if ("weights_exp" %in% base::names(snakemake@params)) {
-   params[["weights_exp"]] = as.numeric(unlist(strsplit(snakemake@params[["weights_exp"]], ",")))
+   params[["weights_exp"]] = snakemake@params[["weights_exp"]]
    }
 
 base::message("Options provided:")
 utils::str(params)
 
 
-# Infer model
+
+##################
+### Save plots ###
+##################
 pdf(out_pdf)
-curves <- Nonpareil.curve.batch(in_files,
-                                label = params[["label"]],
+curves <- Nonpareil.set(in_files,
                                 labels = params[["labels"]],
                                 col = params[["col"]],
                                 enforce.consistency = params[["enforce_consistency"]],
@@ -68,17 +75,28 @@ curves <- Nonpareil.curve.batch(in_files,
                                 plot = FALSE
                                 )
 
+lapply(curves$np.curves, plot,
+       col = params[["col"]],
+       plot.observed = params[["plot_observed"]],
+       plot.model = params[["plot_model"]],
+       plot.dispersion = params[["plot_dispersion"]],
+       plot.diversity = params[["plot_diversity"]]
+)
+dev.off()
+base::message("Nonpareil plot saved")
 
-# Get stats
+
+
+##################
+### Save stats ###
+##################
 stats <- summary(curves)
 # Fix names
 colnames(stats) <- c("Redundancy", "Avg. coverage", "Seq. effort", "Model correlation", "Required seq. effort", "Diversity")
 
-
 # If model not infered, set its values to NA
 stats[,4] <- sapply(stats[,4], function(x){if(length(x) == 0){NA} else {x}})
 stats[,5] <- sapply(stats[,5], function(x){if(length(x) == 0){NA} else {x}})
-
 
 # Convert to Gb
 stats[,3] <- stats[,3] / 1e9
@@ -89,28 +107,19 @@ stats <- round(stats, digits = 2)
 base::print(stats)
 
 
-# Save plot
-plot(curves, legend.opts = FALSE)
 
-
-# Add legend
-legend("bottomright", legend = paste0(paste(colnames(stats), t(stats), sep=": "), c("",""," Gb",""," Gb","")), cex = 0.5)
-if (length(in_files) > 1) {
-  Nonpareil.legend(curves, "topleft", cex = 0.5)
-}
-
-
-# Save model
+##################
+### Save model ###
+##################
 if ("model" %in% base::names(snakemake@output)) {
   save(curves, file=snakemake@output[["model"]])
 }
 
 
-base::message("Nonpareil plot saved")
-dev.off()
 
-
-# Save JSON
+#################
+### Save JSON ###
+#################
 if ("json" %in% base::names(snakemake@output)) {
   base::library("jsonlite")
   base::message("Exporting model as JSON")
@@ -124,8 +133,13 @@ if ("json" %in% base::names(snakemake@output)) {
     n <- names(attributes(object))[13:20]
     y <- lapply(n, function(v) attr(object,v))
     names(y) <- n
+    # Add model
+    x_min <- 1e3
+    x_max <- signif(tail(attr(curves$np.curves[[1]],"x.adj"), n=1)*10, 1)
+    x.model <- exp(seq(log(x_min), log(x_max), length.out=1e3))
+    y.model <- predict(object, lr=x.model)
 
-    append(x, y)
+    c(x, y, list(x.model=x.model), list(y.model=y.model))
   }
 
   export_set <- function(object){
