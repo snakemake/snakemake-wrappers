c43af995f0d26892cd7331b03a1d2934ae371239
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2023-12-11 13:13:59 +0100

    fix: Add json output to Nonpareil and extra plot options (#2393)
    <!-- Ensure that the PR title follows conventional commit style (<type>:
    <description>)-->
    <!-- Possible types are here:
    https://github.com/commitizen/conventional-commit-types/blob/master/index.json
    -->
    
    ### Description
    
    <!-- Add a description of your PR here-->
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] I confirm that:
    
    For all wrappers added by this PR, 
    
    * there is a test case which covers any introduced changes,
    * `input:` and `output:` file paths in the resulting rule can be changed
    arbitrarily,
    * either the wrapper can only use a single core, or the example rule
    contains a `threads: x` statement with `x` being a reasonable default,
    * rule names in the test case are in
    [snake_case](https://en.wikipedia.org/wiki/Snake_case) and somehow tell
    what the rule is about or match the tools purpose or name (e.g.,
    `map_reads` for a step that maps reads),
    * all `environment.yaml` specifications follow [the respective best
    practices](https://stackoverflow.com/a/64594513/2352071),
    * wherever possible, command line arguments are inferred and set
    automatically (e.g. based on file extensions in `input:` or `output:`),
    * all fields of the example rules in the `Snakefile`s and their entries
    are explained via comments (`input:`/`output:`/`params:` etc.),
    * `stderr` and/or `stdout` are logged correctly (`log:`), depending on
    the wrapped tool,
    * temporary files are either written to a unique hidden folder in the
    working directory, or (better) stored where the Python function
    `tempfile.gettempdir()` points to (see
    [here](https://docs.python.org/3/library/tempfile.html#tempfile.gettempdir);
    this also means that using any Python `tempfile` default behavior
    works),
    * the `meta.yaml` contains a link to the documentation of the respective
    tool or command,
    * `Snakefile`s pass the linting (`snakemake --lint`),
    * `Snakefile`s are formatted with
    [snakefmt](https://github.com/snakemake/snakefmt),
    * Python wrapper scripts are formatted with
    [black](https://black.readthedocs.io).
    * Conda environments use a minimal amount of channels, in recommended
    ordering. E.g. for bioconda, use (conda-forge, bioconda, nodefaults, as
    conda-forge should have highest priority and defaults channels are
    usually not needed because most packages are in conda-forge nowadays).

--- bio/nonpareil/plot/wrapper.R
+++ bio/nonpareil/plot/wrapper.R
@@ -27,37 +27,44 @@ base::print(out_pdf)
 
 
 # Set parameters
-params <- list("label" = ifelse("label" %in% base::names(snakemake@params), snakemake@params[["label"]], NA),
+params <- list("in_files" = in_files,
+               "out_pdf" = out_pdf,
                "labels" = NA,
                "col" = NA,
                "enforce_consistency" = ifelse("enforce_consistency" %in% base::names(snakemake@params), as.logical(snakemake@params[["enforce_consistency"]]), FALSE),
                "star" = ifelse("star" %in% base::names(snakemake@params), snakemake@params[["star"]], 95),
                "correction_factor" = ifelse("correction_factor" %in% base::names(snakemake@params), as.logical(snakemake@params[["correction_factor"]]), FALSE),
                "weights_exp" = NA,
-               "skip_model" = ifelse("skip_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["skip_model"]]), FALSE)
+               "skip_model" = ifelse("skip_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["skip_model"]]), FALSE),
+               "plot_observed" = ifelse("plot_observed" %in% base::names(snakemake@params), as.logical(snakemake@params[["plot_observed"]]), TRUE),
+               "plot_model" = ifelse("plot_model" %in% base::names(snakemake@params), as.logical(snakemake@params[["plot_model"]]), TRUE),
+               "plot_dispersion" = ifelse("plot_dispersion" %in% base::names(snakemake@params), snakemake@params[["plot_dispersion"]], FALSE),
+               "plot_diversity" = ifelse("plot_diversity" %in% base::names(snakemake@params), as.logical(snakemake@params[["plot_diversity"]]), FALSE)
               )
 
 # Not sure why, by using "ifelse" only keeps the first element of the vector
 if ("labels" %in% base::names(snakemake@params)) {
-   params[["labels"]] = unlist(strsplit(snakemake@params[["labels"]], ","))
+   params[["labels"]] = snakemake@params[["labels"]]
    }
 
 if ("col" %in% base::names(snakemake@params)) {
-   params[["col"]] = unlist(strsplit(snakemake@params[["col"]], ","))
+   params[["col"]] = snakemake@params[["col"]]
    }
 
 if ("weights_exp" %in% base::names(snakemake@params)) {
-   params[["weights_exp"]] = as.numeric(unlist(strsplit(snakemake@params[["weights_exp"]], ",")))
+   params[["weights_exp"]] = snakemake@params[["weights_exp"]]
    }
 
 base::message("Options provided:")
 utils::str(params)
 
 
-# Infer model
+
+##################
+### Save plots ###
+##################
 pdf(out_pdf)
-curves <- Nonpareil.curve.batch(in_files,
-                                label = params[["label"]],
+curves <- Nonpareil.set(in_files,
                                 labels = params[["labels"]],
                                 col = params[["col"]],
                                 enforce.consistency = params[["enforce_consistency"]],
@@ -68,17 +75,28 @@ curves <- Nonpareil.curve.batch(in_files,
                                 plot = FALSE
                                 )
 
+lapply(curves$np.curves, plot,
+       col = params[["col"]],
+       plot.observed = params[["plot_observed"]],
+       plot.model = params[["plot_model"]],
+       plot.dispersion = params[["plot_dispersion"]],
+       plot.diversity = params[["plot_diversity"]]
+)
+dev.off()
+base::message("Nonpareil plot saved")
 
-# Get stats
+
+
+##################
+### Save stats ###
+##################
 stats <- summary(curves)
 # Fix names
 colnames(stats) <- c("Redundancy", "Avg. coverage", "Seq. effort", "Model correlation", "Required seq. effort", "Diversity")
 
-
 # If model not infered, set its values to NA
 stats[,4] <- sapply(stats[,4], function(x){if(length(x) == 0){NA} else {x}})
 stats[,5] <- sapply(stats[,5], function(x){if(length(x) == 0){NA} else {x}})
-
 
 # Convert to Gb
 stats[,3] <- stats[,3] / 1e9
@@ -89,25 +107,53 @@ stats <- round(stats, digits = 2)
 base::print(stats)
 
 
-# Save plot
-plot(curves, legend.opts = FALSE)
 
-
-# Add legend
-legend("bottomright", legend = paste0(paste(colnames(stats), t(stats), sep=": "), c("",""," Gb",""," Gb","")), cex = 0.5)
-if (length(in_files) > 1) {
-  Nonpareil.legend(curves, "topleft", cex = 0.5)
-}
-
-
-# Save model
+##################
+### Save model ###
+##################
 if ("model" %in% base::names(snakemake@output)) {
   save(curves, file=snakemake@output[["model"]])
 }
 
 
-base::message("Nonpareil plot saved")
-dev.off()
+
+#################
+### Save JSON ###
+#################
+if ("json" %in% base::names(snakemake@output)) {
+  base::library("jsonlite")
+  base::message("Exporting model as JSON")
+
+  export_curve <- function(object){
+    # Extract variables
+    n <- names(attributes(object))[c(1:12,21:29)]
+    x <- sapply(n, function(v) attr(object,v))
+    names(x) <- n
+    # Extract vectors
+    n <- names(attributes(object))[13:20]
+    y <- lapply(n, function(v) attr(object,v))
+    names(y) <- n
+    # Add model
+    # https://github.com/lmrodriguezr/nonpareil/blob/162f1697ab1a21128e1857dd87fa93011e30c1ba/utils/Nonpareil/R/Nonpareil.R#L330-L332
+    x_min <- 1e3
+    x_max <- signif(tail(attr(curves$np.curves[[1]],"x.adj"), n=1)*10, 1)
+    x.model <- exp(seq(log(x_min), log(x_max), length.out=1e3))
+    y.model <- predict(object, lr=x.model)
+
+    c(x, y, list(x.model=x.model), list(y.model=y.model))
+  }
+
+  export_set <- function(object){
+    y <- lapply(object$np.curves, "export_curve")
+    names(y) <- sapply(object$np.curves, function(n) n$label)
+    jsonlite::prettify(toJSON(y, auto_unbox=TRUE))
+  }
+
+  y <- export_set(curves)
+  write(y, snakemake@output[["json"]])
+  base::message("JSON file saved")
+}
+
 
 
 # Proper syntax to close the connection for the log file
