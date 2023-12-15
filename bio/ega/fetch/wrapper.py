46ab6f08054c0b2e65aedc47510546ae5fdd7bbf
Johannes Koester <johannes.koester@uni-due.de>
2023-12-15 14:49:44 +0100

    feat: add wrapper for fetching files from EGA

---
+++ bio/ega/fetch/wrapper.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import shlex
+import shutil
+import subprocess as sp
+import sys
+import tempfile
+
+if snakemake.log:
+    sys.stderr = open(snakemake.log[0], "w")
+
+fileid = snakemake.params.fileid
+
+fmt = Path(snakemake.output).suffix[1:].upper()
+
+extra_pyega3 = shlex.split(snakemake.params.get("extra_pyega3", ""))
+extra_fetch = shlex.split(snakemake.params.get("extra_fetch", ""))
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    cmd = (
+        ["pyega3"]
+        + extra_pyega3
+        + ["fetch", "--output-dir", tmpdir, "--format", fmt, fileid]
+        + extra_fetch
+    )
+    sp.run(
+        cmd,
+        stdout=sys.stderr,
+        stderr=sp.STDOUT,
+        check=True,
+    )
+
+    # Move the file to the output
+    shutil.move(Path(tmpdir).glob(f"*.{fmt.lower()}"), snakemake.output[0])
