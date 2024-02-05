4e23870a8281473fcbdb0a7564af2746ffd173e4
Hamdiye Uzuner <hamdiye.uzuner@uni-due.de>
2024-02-05 15:52:16 +0100

    delete template lines hla wrapper.py

--- bio/orthanq/hla/candidates/wrapper.py
+++ bio/orthanq/hla/candidates/wrapper.py
@@ -6,7 +6,6 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-shell("datavzrd {snakemake.input.config} {extra} --output {snakemake.output[0]} {log}")
 shell(
     "orthanq candidates hla --allele-freq {snakemake.input.allele_freq} "
     " --alleles {snakemake.input.alleles} --genome {snakemake.input.genome} "
