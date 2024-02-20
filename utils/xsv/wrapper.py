0fbae11cb809309647738d03f14e2253c2e86299
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-02-20 14:49:52 +0100

    Make xsv input positional

--- utils/xsv/wrapper.py
+++ utils/xsv/wrapper.py
@@ -11,16 +11,16 @@ subcommand = snakemake.params["subcommand"]
 extra = snakemake.params.get("extra", "")
 
 # TSV delimiter
-if len(snakemake.input["table"]) == 1:
-    if str(snakemake.input["table"]).endswith(".tsv"):
-        extra += " --delimiter $'\t' "
-elif all(str(table).endswith(".tsv") for table in snakemake.input["table"]):
-    extra += " --delimiter $'\t' "
+if len(snakemake.input) == 1:
+    if str(snakemake.input).endswith(".tsv"):
+        extra += " --delimiter $'\t'"
+elif all(table.endswith(".tsv") for table in snakemake.input):
+    extra += " --delimiter $'\t'"
 
 
 # Automatic multithreading when possible
 if subcommand in ["frequency", "split", "stats"]:
-    extra += f" --jobs {snakemake.threads} "
+    extra += f" --jobs {snakemake.threads}"
 elif snakemake.threads > 1:
     raise Warning("Only one thread is required")
 
@@ -28,21 +28,18 @@ elif snakemake.threads > 1:
 if subcommand == "join":
     shell(
         "xsv {subcommand} {extra} "
-        "{snakemake.params.col1} {snakemake.input.table[0]} "
-        "{snakemake.params.col2} {snakemake.input.table[1]} "
+        "{snakemake.params.col1} {snakemake.input[0]} "
+        "{snakemake.params.col2} {snakemake.input[1]} "
         "> {snakemake.output} {log}"
     )
 elif subcommand == "index":
     log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-    shell("xsv {subcommand} {extra} {snakemake.input.table} {log}")
+    shell("xsv {subcommand} {extra} {snakemake.input} {log}")
 elif subcommand == "split":
     log = snakemake.log_fmt_shell(stdout=True, stderr=True)
     outdir = snakemake.output
     if len(outdir) > 1:
         outdir = os.path.dirname(outdir[0])
-    shell("xsv {subcommand} {extra} {outdir} {snakemake.input.table} {log}")
+    shell("xsv {subcommand} {extra} {outdir} {snakemake.input} {log}")
 else:
-    shell(
-        "xsv {subcommand} {extra} {snakemake.input.table} "
-        " > {snakemake.output} {log}"
-    )
+    shell("xsv {subcommand} {extra} {snakemake.input} > {snakemake.output} {log}")
