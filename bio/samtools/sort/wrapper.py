6a05aad6066b0f3aadb92e1f7f931c161222b150
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-02-22 15:13:31 +0100

    fix: account for memory overhead (#2648)
    <!-- Ensure that the PR title follows conventional commit style (<type>:
    <description>)-->
    <!-- Possible types are here:
    https://github.com/commitizen/conventional-commit-types/blob/master/index.json
    -->
    
    <!-- Add a description of your PR here-->
    According to the manual, `-m` specifies the approximated maximum
    required memory per thread.
    In some cases, `samtools sort` can use more memory than specified (e.g.
    https://github.com/samtools/samtools/issues/831) so we account for some
    overhead.
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] I confirm that:
    
    For all wrappers added by this PR, 
    
    * there is a test case which covers any introduced changes,
    * `input:` and `output:` file paths in the resulting rule can be changed
    arbitrarily,
    * either the wrapper can only use a single core, or the example rule
    contains a `threads: x` statement with `x` being a reasonable default,
    * rule names in the test case are in
    [snake_case](https://en.wikipedia.org/wiki/Snake_case) and somehow tell
    what the rule is about or match the tools purpose or name (e.g.,
    `map_reads` for a step that maps reads),
    * all `environment.yaml` specifications follow [the respective best
    practices](https://stackoverflow.com/a/64594513/2352071),
    * the `environment.yaml` pinning has been updated by running
    `snakedeploy pin-conda-envs environment.yaml` on a linux machine,
    * wherever possible, command line arguments are inferred and set
    automatically (e.g. based on file extensions in `input:` or `output:`),
    * all fields of the example rules in the `Snakefile`s and their entries
    are explained via comments (`input:`/`output:`/`params:` etc.),
    * `stderr` and/or `stdout` are logged correctly (`log:`), depending on
    the wrapped tool,
    * temporary files are either written to a unique hidden folder in the
    working directory, or (better) stored where the Python function
    `tempfile.gettempdir()` points to (see
    [here](https://docs.python.org/3/library/tempfile.html#tempfile.gettempdir);
    this also means that using any Python `tempfile` default behavior
    works),
    * the `meta.yaml` contains a link to the documentation of the respective
    tool or command,
    * `Snakefile`s pass the linting (`snakemake --lint`),
    * `Snakefile`s are formatted with
    [snakefmt](https://github.com/snakemake/snakefmt),
    * Python wrapper scripts are formatted with
    [black](https://black.readthedocs.io).
    * Conda environments use a minimal amount of channels, in recommended
    ordering. E.g. for bioconda, use (conda-forge, bioconda, nodefaults, as
    conda-forge should have highest priority and defaults channels are
    usually not needed because most packages are in conda-forge nowadays).

--- bio/samtools/sort/wrapper.py
+++ bio/samtools/sort/wrapper.py
@@ -15,7 +15,12 @@ samtools_opts = get_samtools_opts(snakemake)
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-mem_per_thread_mb = int(get_mem(snakemake) / snakemake.threads)
+# `-m`: Approximately the maximum required memory per thread [...]
+# samtools sort can use more memory than specified (e.g. https://github.com/samtools/samtools/issues/831)
+mem_overhead_factor = snakemake.params.get("mem_overhead_factor", 0.1)
+mem_per_thread_mb = int(
+    get_mem(snakemake) / snakemake.threads * (1.0 - mem_overhead_factor)
+)
 
 with tempfile.TemporaryDirectory() as tmpdir:
     tmp_prefix = Path(tmpdir) / "samtools_sort"
