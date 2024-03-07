2de4c17b78521f43d6d73ca133e32c11a7396804
tdayris <thibault.dayris@gustaveroussy.fr>
2024-03-07 11:29:33 +0100

    docstring

--- bio/rseqc/infer_experiment/wrapper.py
+++ bio/rseqc/infer_experiment/wrapper.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+"""Snakemake wrapper for RSeQC infer experiment"""
+
 __author__ = "Thibault Dayris"
 __mail__ = "thibault.dayris@gustaveroussy.fr"
 __copyright__ = "Copyright 2024, Thibault Dayris"
