5dcb85f8b869d75fe82c602b5ff50c1d4a6895ac
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-03-18 14:46:23 +0100

    Code clean-up

--- bio/taxonkit/wrapper.py
+++ bio/taxonkit/wrapper.py
@@ -2,6 +2,7 @@ __author__ = "Filipe G. Vieira"
 __copyright__ = "Copyright 2024, Filipe G. Vieira"
 __license__ = "MIT"
 
+
 from pathlib import Path
 from snakemake.shell import shell
 
@@ -11,11 +12,9 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 
 input = snakemake.input.get("input", "")
-print(f"Processing input files: {input}")
 
 in_taxdump = snakemake.input.get("taxdump", "")
 if in_taxdump:
-    print(f"Using taxdump in: {in_taxdump}")
     in_taxdump = Path(in_taxdump[0]).parent
     in_taxdump = f"--data-dir {in_taxdump}"
 
@@ -23,10 +22,8 @@ if in_taxdump:
 out_taxdump = snakemake.output.get("taxdump", "")
 if out_taxdump:
     out_taxdump = Path(out_taxdump[0]).parent
-    print(f"Dumping taxdump to: {out_taxdump}")
     extra += f" --out-dir {out_taxdump}"
 else:
-    print(f"Saving output to file: {snakemake.output}")
     if snakemake.output[0].endswith("json"):
         extra += " --json"
     extra += f" --out-file {snakemake.output}"
