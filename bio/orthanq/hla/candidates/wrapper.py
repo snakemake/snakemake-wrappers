bd395c9df1fbc0d283517c0c69e42f8cb0e963d5
Hamdiye Uzuner <hamdiye.uzuner@uni-due.de>
2024-03-21 22:24:35 +0100

    fix typo in wrapper.py

--- bio/orthanq/hla/candidates/wrapper.py
+++ bio/orthanq/hla/candidates/wrapper.py
@@ -7,7 +7,7 @@ from snakemake.shell import shell
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
-    "orthanq candidates hla --allele-freq {snakemake.input.allele_freq} "
+    "orthanq candidates hla --allele-freq {snakemake.input.allele_freqs} "
     " --alleles {snakemake.input.alleles} --genome {snakemake.input.genome} "
     " --xml {snakemake.input.xml} --output {snakemake.output[0]} {log}"
 )
