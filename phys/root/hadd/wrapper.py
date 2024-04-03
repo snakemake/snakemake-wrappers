b1f4bf373e052ad36bfde2d126dc2bc28c429412
Jamie Gooding <74370983+GoodingJamie@users.noreply.github.com>
2024-04-03 23:37:12 +0200

    Write hadd wrapper, environment and metadata

---
+++ phys/root/hadd/wrapper.py
@@ -0,0 +1,13 @@
+__author__ = "Jamie Gooding"
+__copyright__ = "Copyright 2024, Jamie Gooding"
+__email__ = "jamie.gooding@cern.ch"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell()
+
+shell(
+    "(hadd {snakemake.output} {snakemake.input}) {log}"
+)
