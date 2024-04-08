3ff00909c060041ab424f03822224075a53178f7
Curro Campuzano <69399781+currocam@users.noreply.github.com>
2024-04-08 11:53:02 +0200

    Modify params combine-outputs

--- bio/emu/combine-outputs/wrapper.py
+++ bio/emu/combine-outputs/wrapper.py
@@ -17,6 +17,7 @@ if not isinstance(input_files, list):
 
 if snakemake.output.get("abundance") and snakemake.output.get("taxonomy"):
     split = True
+    extra += " --split-tables"
     taxonomy = snakemake.output.get("taxonomy")
     abundances = snakemake.output.get("abundance")
 elif isinstance(snakemake.output[0], str):
@@ -27,10 +28,12 @@ else:
         "Please provide either one TSV file, or two named TSV files (abundances and taxonomy)"
     )
 
-if not snakemake.params.get("rank"):
-    raise ValueError("Please provide a rank parameter")
-rank = snakemake.params.get("rank")
-counts = snakemake.params.get("counts", False)
+if "--split-tables" in extra and not split:
+    raise ValueError("You cannot use --split-tables and produce a single output.")
+
+rank = snakemake.params.get("rank", "tax_id")
+counts = "--counts" in extra
+
 
 with tempfile.TemporaryDirectory() as tmpdir:
     for infile in input_files:
@@ -39,17 +42,14 @@ with tempfile.TemporaryDirectory() as tmpdir:
         if not temp.endswith("rel_abundances.tsv"):
             temp = os.path.splitext(infile)[0] + "-rel_abundances.tsv"
         os.symlink(infile, temp)
+    shell("emu combine-outputs {tmpdir} {rank} {extra} {log}")
     if split and counts:
-        shell("emu combine-outputs {tmpdir} {rank} --split-tables --counts {log}")
         shell("mv {tmpdir}/emu-combined-taxonomy-{rank}.tsv {taxonomy}")
         shell("mv {tmpdir}/emu-combined-abundance-{rank}-counts.tsv {abundances}")
     elif split and not counts:
-        shell("emu combine-outputs {tmpdir} {rank} --split-tables {log}")
         shell("mv {tmpdir}/emu-combined-taxonomy-{rank}.tsv {taxonomy}")
         shell("mv {tmpdir}/emu-combined-abundance-{rank}.tsv {abundances}")
     elif not split and counts:
-        shell("emu combine-outputs {tmpdir} {rank} --counts {log}")
         shell("mv {tmpdir}/emu-combined-{rank}-counts.tsv {table}")
     elif not split and not counts:
-        shell("emu combine-outputs {tmpdir} {rank} {extra} {log}")
         shell("mv {tmpdir}/emu-combined-{rank}.tsv {table}")
