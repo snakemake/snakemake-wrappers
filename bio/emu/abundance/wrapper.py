b803b8aa44d4902725b1ed6ed5b4a7dd6d9d4129
Curro Campuzano <69399781+currocam@users.noreply.github.com>
2024-04-08 15:27:02 +0200

    Improve abundance emu
    Co-authored-by: Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>

--- bio/emu/abundance/wrapper.py
+++ bio/emu/abundance/wrapper.py
@@ -10,29 +10,16 @@ import os
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
-threads = snakemake.threads or 1
-
-# Check input (mandatory)
-msg_error = "Please provide either one file of single-end 16S reads or two files of short paired-end 16S"
-if not snakemake.input.get("reads"):
-    raise ValueError(msg_error)
-reads = snakemake.input.get("reads")
-if isinstance(reads, list) and len(reads) > 2:
-    raise ValueError(msg_error)
-
 # Check database (optional)
-database_cmd = ""
-if database := snakemake.input.get("database_dir"):
-    if not os.path.isdir(database):
-        raise ValueError("Please provide a valid Emu database directory")
-    database_cmd = f"--db {database}"
+if db:= snakemake.input.get("db"):
+    db = f"--db {db}"
 
 with tempfile.TemporaryDirectory() as tmpdir:
     shell(
-        "emu abundance {reads} {database_cmd}"
+        "emu abundance {snakemake.input.reads} {db}"
         " --keep-files --output-dir {tmpdir}"
         " --output-basename output --output-unclassified"
-        " --threads	{threads}"
+        " --threads {snakemake.threads}"
         " {extra}"
         " {log}"
     )
