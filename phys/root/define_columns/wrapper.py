4c53edc15ff9336e3f6f4cf39a46156137f135bc
Anfeng Li <anfeng.li@cern.ch>
2024-04-22 18:36:14 +0800

    Remove support for multiple input files

--- phys/root/define_columns/wrapper.py
+++ phys/root/define_columns/wrapper.py
@@ -12,18 +12,17 @@ redefine_list = snakemake.params.get("redefine", [])
 branches = snakemake.params.get("branches", [])
 branches_to_save = snakemake.params.get("branches_to_save", None)
 
-for i in range(len(snakemake.input)):
-    df = ROOT.RDataFrame(snakemake.params.input_tree_name, snakemake.input[i])
-    for branch_name, branch_definition in branches:
-        if branch_name in redefine_list:
-            df = df.Redefine(branch_name, branch_definition)
-        else:
-            df = df.Define(branch_name, branch_definition)
-    if branches_to_save is not None:
-        df.Snapshot(
-            snakemake.params.output_tree_name,
-            snakemake.output[i],
-            branches_to_save,
-        )
+df = ROOT.RDataFrame(snakemake.params.input_tree_name, snakemake.input[0])
+for branch_name, branch_definition in branches:
+    if branch_name in redefine_list:
+        df = df.Redefine(branch_name, branch_definition)
     else:
-        df.Snapshot(snakemake.params.output_tree_name, snakemake.output[i])
+        df = df.Define(branch_name, branch_definition)
+if branches_to_save is not None:
+    df.Snapshot(
+        snakemake.params.output_tree_name,
+        snakemake.output[0],
+        branches_to_save,
+    )
+else:
+    df.Snapshot(snakemake.params.output_tree_name, snakemake.output[0])
