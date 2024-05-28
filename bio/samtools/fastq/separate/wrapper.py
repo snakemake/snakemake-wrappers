3d9d295f73d3699d22a62a1d165e0516a674fa5a
Felix Wiegand <fxwiegand@wgdnet.de>
2024-05-28 12:58:31 +0200

    Update wrapper.py

--- bio/samtools/fastq/separate/wrapper.py
+++ bio/samtools/fastq/separate/wrapper.py
@@ -10,29 +10,25 @@ from pathlib import Path
 from snakemake.shell import shell
 from snakemake_wrapper_utils.snakemake import get_mem
 
-params_sort = snakemake.params.get("sort", "")
+params_collate = snakemake.params.get("collate", "")
 params_fastq = snakemake.params.get("fastq", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 # Samtools takes additional threads through its option -@
-# One thread is used bu Samtools sort
+# One thread is used by Samtools collate
 # One thread is used by Samtools fastq
 # So snakemake.threads has to take them into account
 # before allowing additional threads through samtools sort -@
 threads = 0 if snakemake.threads <= 2 else snakemake.threads - 2
 
-mem = get_mem(snakemake, "MiB")
-mem = "-m {0:.0f}M".format(mem / threads) if mem and threads else ""
-
 with tempfile.TemporaryDirectory() as tmpdir:
-    tmp_prefix = Path(tmpdir) / "samtools_fastq.sort"
+    tmp_prefix = Path(tmpdir) / "samtools_fastq.collate"
 
     shell(
-        "(samtools sort -n"
+        "(samtools collate -u -O"
         " --threads {threads}"
-        " {mem}"
         " -T {tmp_prefix}"
-        " {params_sort}"
+        " {params_collate}"
         " {snakemake.input[0]} | "
         "samtools fastq"
         " {params_fastq}"
