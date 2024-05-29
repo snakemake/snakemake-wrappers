b203b8eeb76d74784285aaad9fc11994a457c5ac
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-05-29 13:10:54 +0200

    Use helper functions

--- utils/aria2c/wrapper.py
+++ utils/aria2c/wrapper.py
@@ -2,32 +2,15 @@ __author__ = "Filipe G. Vieira"
 __copyright__ = "Copyright 2023, Filipe G. Vieira"
 __license__ = "MIT"
 
-import pandas as pd
-from pathlib import Path
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 
 
-if len(snakemake.input) > 0:
-    # Get hash function
-    hash_function = snakemake.params.get(
-        "hash_function",
-        Path(snakemake.input[0].removesuffix(".gz")).suffix.lstrip(".").lower(),
-    )
-    # Get hash
-    df = pd.read_csv(
-        snakemake.input[0],
-        sep="  ",
-        index_col="file_name",
-        header=None,
-        names=["checksum", "file_name"],
-        engine="python",
-    )
-    hash = df[df.index.str.endswith(Path(snakemake.params.url).name, na=False)][
-        "checksum"
-    ].item()
-    extra += f" --checksum {hash_function}={hash}"
+hash_function = snakemake.params.get("hash_function")
+checksum = snakemake.params.get("checksum")
+if hash_function and checksum:
+    extra += f" --checksum {hash_function}={checksum}"
 
 
 shell(
