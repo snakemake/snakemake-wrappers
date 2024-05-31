ec814120ff9fa81434bb7cad3066d4468ea2f78c
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-05-31 08:46:17 +0200

    Fix new command syntax

--- bio/rasusa/wrapper.py
+++ bio/rasusa/wrapper.py
@@ -6,12 +6,12 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
-
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
 
 bases = snakemake.params.get("bases")
 if bases:
-    extra += f" -b {bases}"
+    extra += f" --bases {bases}"
 else:
     covg = snakemake.params.get("coverage")
     gsize = snakemake.params.get("genome_size")
@@ -19,7 +19,7 @@ else:
         raise ValueError(
             "If `bases` is not given, then `coverage` and `genome_size` must be"
         )
-    extra += f" -g {gsize} -c {covg}"
+    extra += f" --genome-size {gsize} --coverage {covg}"
 
-shell("rasusa reads -i {snakemake.input} {extra} -o {snakemake.output} 2> {snakemake.log}")
+shell("rasusa reads {extra} --output {snakemake.output[0]} --output {snakemake.output[1]} {snakemake.input} {log}")
 
