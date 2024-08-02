86b488da6d11c1efea2a8cc38d592838de65785a
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-08-02 09:59:48 +0200

    Fix typo and make output more general

--- bio/nonpareil/plot/wrapper.py
+++ bio/nonpareil/plot/wrapper.py
@@ -3,23 +3,17 @@ __copyright__ = "Copyright 2024, Filipe G. Vieira"
 __license__ = "MIT"
 
 
+from pathlib import Path
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 
-json = snakemake.output.get("json", "")
-if json:
-    json = f"--json {json}"
-
-tsv = snakemake.output.get("tsv", "")
-if tsv:
-    tsv = f"--tsv {tsv}"
-
-pdf = snakemake.output.get("pdf", "")
-if pdf:
-    pdf = f"--pdf {pdf}"
+for output in snakemake.output:
+    ext = Path(output).suffix
+    if ext in ["json", "tsv", "pdf"]:
+        extra += f" --{ext} {output}"
 
 
-shell("NonpareilCurves.R {extra} {json} {tsv} {pdf} {snakemake.input} {log}")
+shell("NonpareilCurves.R {extra} {snakemake.input} {log}")
