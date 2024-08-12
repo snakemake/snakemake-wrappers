815d1a20a230fbd9ee4014f5cf3de80d9b8697c7
David Laehnemann <david.laehnemann@hhu.de>
2024-08-12 12:05:19 +0200

    feat: improve test for correct build (start with meta table entry checking, do previously created options as fallback), structure error messages better

--- bio/reference/ensembl-mysql-table/wrapper.R
+++ bio/reference/ensembl-mysql-table/wrapper.R
@@ -50,16 +50,47 @@ ensembl_connection <- dbConnect(
 )
 
 get_and_check_db_name <- function(connection, species, database, release, wanted_build) {
+  species_dbs <- dbGetQuery(connection, "SHOW DATABASES") |> filter(str_starts(Database, species))
+
   dbname_prefix <- str_c(
     species,
     database,
     release,
     sep ="_"
   )
-  dbname <- dbGetQuery(connection, "SHOW DATABASES") |> filter(str_starts(Database, dbname_prefix))
+  dbname <- species_dbs |> filter(str_starts(Database, dbname_prefix)) |> pull(Database)
+
+  core_dbname_prefix <- str_c(
+    species,
+    "core",
+    release,
+    sep ="_"
+  )
+  core_dbname <- species_dbs |> filter(str_starts(Database, core_dbname_prefix)) |> pull(Database)
 
   if ( length(dbname) == 1 ) {
+    # CHECK THAT WE ARE GETTING THE CORRECT BUILD
+    # 1st option: the meta table of the core database contains build / assembly
+    #             identifiers that match what is requested
+    core_connection <- dbConnect(
+      MariaDB(),
+      dbname = core_dbname,
+      host = "ensembldb.ensembl.org", 
+      user = "anonymous",
+      port = port,
+      password = "",
+      timeout=45
+    )
+    assembly_default <- tbl(core_connection, "meta") |> filter(meta_key == "assembly.default") |> pull("meta_value")
+    assembly_name <- tbl(core_connection, "meta") |> filter(meta_key == "assembly.name") |> pull("meta_value")
+
+    if (!wanted_build %in% c(assembly_default, assembly_name)) {
+      # 2nd option: the build number from the retrieved database name matches
+      #             the systematically parsed version number at the end of
+      #             the build requested via params:
+
     retrieved_build_num <- dbname |> first() |> str_split("_") |> first() |> last()
+
     # Canonicalize the build number to a single number as used in the mysql
     # database names. At the time of writing, this code fails for 20 species,
     # but build/assembly name numbers in those have no systematic relationship
@@ -82,8 +113,8 @@ get_and_check_db_name <- function(connection, species, database, release, wanted
       str_replace("0$", "")
 
     if (retrieved_build_num != wanted_build_num) {
-      # Systematic extraction didn't match up. We'll try matching up by downloading
-      # the releases species summary file from the FTP servers. ¯\_(ツ)_/¯
+        # 3rd option: try matching up by downloading the releases species
+        #             summary file from the FTP servers... ¯\_(ツ)_/¯
       species_file_address <- str_c(
         "https://ftp.ensembl.org/pub/release-",
         release,
@@ -97,30 +128,54 @@ get_and_check_db_name <- function(connection, species, database, release, wanted
       if ( (! str_starts(wanted_build, assembly)) & (summary_build_num != wanted_build_num) ) {
         cli_abort(c(
                 "The build we could retrieve for the specified combination of species, database, and release does",
-                " not match the build you specified.",
-          "x" = "Genome build '{wanted_build}' requested for species '{species}' at Ensembl release '{release}', but",
-          " " = "only found build with number '{retrieved_build_num}'. This does not match the build number we",
-          " " = "systematically extracted from '{wanted_build}', which is '{wanted_build_num}'. We also checked",
-          " " = "against the Ensembl species summary file at:",
+                  "not match the build you specified.",
+            "x" = "You specified:",
+            "*" = "genome build: {wanted_build}",
+            "*" = "species: {species}",
+            "*" = "Ensembl release {release}",
+            "x" = "But the 'meta' table of the respective 'core' database ('{core_dbname}') gave:",
+            "*" = "assembly.default: {assembly_default}",
+            "*" = "assembly.name: {assembly_name}",
+            " " = "Neither matches the requested genome build.",
+            " " = "",
+            "x" = "Also, build number '{retrieved_build_num}' at the end of the determined database name",
+            " " = "does not match the build number we systematically extracted from '{wanted_build}',",
+            " " = "which is '{wanted_build_num}'.",
+            " " = "",
+            "x" = "Finally, we also checked against the Ensembl species summary file at:",
           " " = "{species_file_address}",
           " " = "It listed the following info, which doesn't match the wanted build specification:",
           "*" = "build / assembly : '{assembly}'",
           "*" = "build number for core_db: '{summary_build_num}'",
+            " " = "We give up.",
+            " " = "",
           "i" = "Please ensure that you specify an existing combination of species, build and release.",
-          " " = "The above listed species summary file is a good starting point."
+            " " = "The Ensembl species summary file for the release you want, is a good starting point.",
+            " " = " It includes correct 'species' names and valid 'assembly' (build) names:",
+            " " = "{species_file_address}",
+            " " = ""
         ))
+        }
       }
     }
-    dbname |> pull(Database) |> first()
+    dbname
   } else {
     cli_abort(c(
             "Could not retrieve a (unique?) database name for the specified combination of species, database, release, and build.",
-      "x" = "You requested species '{species}', database '{database}', Ensembl release '{release}', and",
-      " " = "genome build '{wanted_build}'. As builds are simple numbers in the mysql database names, we",
-      " " = "looked for a database with prefix '{dbname_prefix}'. Here's what we found (can be empty):",
+      "x" = "You requested",
+      "*" = "species: {species}",
+      "*" = "database: {database}",
+      "*" = "Ensembl release: {release}",
+      "*" = "genome build: {wanted_build}",
+      " " = "",
+      " " = "As builds are simple numbers in the mysql database names, we looked for a database with prefix:",
+      " " = "{dbname_prefix}",
+      " " = "",
+      "x" = "Here's what we found (can be empty):",
       " " = "{dbname}",
+      " " = "",
       "i" = "Please ensure that you specify an existing and unique combination of species, database, release, and build.",
-      " " = "The following species summary file can be a good starting point:",
+      " " = "The following species summary{dbname} file can be a good starting point:",
       " " = "https://ftp.ensembl.org/pub/release-{release}/species_EnsemblVertebrates.txt"
     ))
   }
