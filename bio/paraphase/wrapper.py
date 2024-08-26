6745c6bb860da3ef26c8aec2a7497b208f0e88da
magdalenazz <magdalenazz477@gmail.com>
2024-08-26 10:21:38 +0200

    bug: linting

--- bio/paraphase/wrapper.py
+++ bio/paraphase/wrapper.py
@@ -9,79 +9,70 @@ extra = snakemake.params.get("extra", "")
 
 with TemporaryDirectory() as tmpdirname:
 
-	### Run paraphase
-	shell(
-	    f"""
-	    (paraphase --bam {snakemake.input.bam} \
-	    --reference {snakemake.input.fasta} \
-	    --out {tmpdirname} \
-	    {snakemake.params.genome} \
-	    {extra}) {log}
-	    """,
-	    tmpdirname=tmpdirname,  # Pass tmpdirname to the shell environment
-	)
-	shell(
-	    """
-	    touch {snakemake.output}
-	    """
-	)
+    ### Run paraphase
+    shell(
+        f"""
+        (paraphase --bam {snakemake.input.bam} \
+        --reference {snakemake.input.fasta} \
+        --out {tmpdirname} \
+        {snakemake.params.genome} \
+        {extra}) {log}
+        """,
+        tmpdirname=tmpdirname,  # Pass tmpdirname to the shell environment
+    )
+    shell(
+        """
+        touch {snakemake.output}
+        """
+    )
 
-	### Create a new VCF header
+    ### Create a new VCF header
 
-	# Get the paths from Snakemake input and output objects
-	input_faidx = snakemake.input.faidx
-	output_vcf_header = snakemake.output.vcf_header
+    # Get the paths from Snakemake input and output objects
+    input_faidx = snakemake.input.faidx
+    output_vcf_header = snakemake.output.vcf_header
 
-	# Open the .fai index file and read lines
-	with open(input_faidx, "r") as fai_file:
-	    lines = fai_file.readlines()
+    # Open the .fai index file and read lines
+    with open(input_faidx, "r") as fai_file:
+        lines = fai_file.readlines()
 
-	# Open the output file and write formatted header lines
-	with open(output_vcf_header, "w") as output:
-	    for line in lines:
-		contig_id, length = line.split()[
-		    :2
-		]  # Assuming the first two elements are ID and length
-		output.write(f"##contig=<ID={contig_id},length={length}>\n")
+    # Open the output file and write formatted header lines
+    with open(output_vcf_header, "w") as output:
+        for line in lines:
+            contig_id, length = line.split()[:2]  # Assuming the first two elements are ID and length
+            output.write(f"##contig=<ID={contig_id},length={length}>\n")
 
-	### Concatenating, reheadering, and sorting the zipped and indexed VCF files, and copy the remapped reads
-	vcf_res = glob.glob(f"{tmpdirname}/*_vcfs/*vcf")
-	if vcf_res:
-	    for vcf in vcf_res:
-		bgzip_cmd = f"bgzip -c {vcf} > {vcf}.gz"
-		shell(bgzip_cmd)
-		index_cmd = f"bcftools index {vcf}.gz"
-		shell(index_cmd)
-		print(f"Compressed and indexed: {vcf}.gz")
+    ### Concatenating, reheadering, and sorting the zipped and indexed VCF files, and copy the remapped reads
+    vcf_res = glob.glob(f"{tmpdirname}/*_vcfs/*vcf")
+    if vcf_res:
+        for vcf in vcf_res:
+            bgzip_cmd = f"bgzip -c {vcf} > {vcf}.gz"
+            shell(bgzip_cmd)
+            index_cmd = f"bcftools index {vcf}.gz"
+            shell(index_cmd)
+            print(f"Compressed and indexed: {vcf}.gz")
 
-	    params_variant_files = " ".join([f"{vcf}.gz" for vcf in vcf_res])
-	    shell(
-		f"bcftools concat -a -Oz {params_variant_files} | "
-		f"bcftools annotate --header-lines {snakemake.output.vcf_header} | "
-		f"bcftools sort -Oz -o {snakemake.output.merged_vcf}"
-	    )
-	    print(
-		f"Merged, reheadered, and sorted VCF file created: {snakemake.output.merged_vcf}"
-	    )
+        params_variant_files = " ".join([f"{vcf}.gz" for vcf in vcf_res])
+        shell(
+            f"bcftools concat -a -Oz {params_variant_files} | "
+            f"bcftools annotate --header-lines {snakemake.output.vcf_header} | "
+            f"bcftools sort -Oz -o {snakemake.output.merged_vcf}"
+        )
+        print(f"Merged, reheadered, and sorted VCF file created: {snakemake.output.merged_vcf}")
 
-	    # Copy out bam and bai files
-	    bam_res = glob.glob(f"{tmpdirname}/*.bam")
-	    bai_res = glob.glob(f"{tmpdirname}/*.bai")
-	    # print("BAM RES: ", bam_res, bai_res)
-	    shell(
-		f"""
-		cp -pr {' '.join(bam_res)} {snakemake.output.bam};
-		cp -pr {' '.join(bai_res)} {snakemake.output.bai}
-		"""
-	    )
-	else:
-	    print(
-		"No output VCF or BAM files were produced by paraphase, I hope this is what you were expecting, human?"
-	    )
-	    shell(f"touch {snakemake.output.merged_vcf}")
-
-
-
+        # Copy out bam and bai files
+        bam_res = glob.glob(f"{tmpdirname}/*.bam")
+        bai_res = glob.glob(f"{tmpdirname}/*.bai")
+        # print("BAM RES: ", bam_res, bai_res)
+        shell(
+            f"""
+        cp -pr {' '.join(bam_res)} {snakemake.output.bam};
+        cp -pr {' '.join(bai_res)} {snakemake.output.bai}
+        """
+        )
+    else:
+        print("No output VCF or BAM files were produced by paraphase, I hope this is what you were expecting, human?")
+        shell(f"touch {snakemake.output.merged_vcf}")
 
 
 with TemporaryDirectory() as tmpdirname:
@@ -116,9 +107,7 @@ with TemporaryDirectory() as tmpdirname:
     # Open the output file and write formatted header lines
     with open(output_vcf_header, "w") as output:
         for line in lines:
-            contig_id, length = line.split()[
-                :2
-            ]  # Assuming the first two elements are ID and length
+            contig_id, length = line.split()[:2]  # Assuming the first two elements are ID and length
             output.write(f"##contig=<ID={contig_id},length={length}>\n")
     output.close()
 
@@ -138,9 +127,7 @@ with TemporaryDirectory() as tmpdirname:
             f"bcftools annotate --header-lines {snakemake.output.vcf_header} | "
             f"bcftools sort -Oz -o {snakemake.output.merged_vcf}"
         )
-        print(
-            f"Merged, reheadered, and sorted VCF file created: {snakemake.output.merged_vcf}"
-        )
+        print(f"Merged, reheadered, and sorted VCF file created: {snakemake.output.merged_vcf}")
 
         # Copy out bam and bai files
         bam_res = glob.glob(f"{tmpdirname}/*.bam")
@@ -153,9 +140,5 @@ with TemporaryDirectory() as tmpdirname:
             """
         )
     else:
-        print(
-            "No output VCF or BAM files were produced by paraphase, I hope this is what you were expecting, human?"
-        )
+        print("No output VCF or BAM files were produced by paraphase, I hope this is what you were expecting, human?")
         shell(f"touch {snakemake.output.merged_vcf}")
-
-
