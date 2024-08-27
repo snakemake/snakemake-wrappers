d34ae24990d15275b71b2967ba71981a1cca2a2a
tdayris <thibault.dayris@gustaveroussy.fr>
2024-08-27 09:33:41 +0200

    feat: deeptools plot pca

---
+++ bio/deeptools/plotpca/wrapper.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Copyright 2024, Thibault Dayris"
+__email__ = "thibault.dayris@gustaveroussy.fr"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+# Optional parameters
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+extra = snakemake.params.get("extra", "")
+
+
+# Get plot file format
+fmt = str(snakemake.output["plot"]).split(".")[-1].lower()
+
+# Get optional output matrix
+pca_tab = snakemake.output.get("matrix")
+if pca_tab:
+    extra += f" --outFileNameData {pca_tab} "
+
+shell(
+    "plotPCA "
+    "--corData {snakemake.input[0]} "
+    "--plotFile {snakemake.output.plot} "
+    "--plotFileFormat {fmt} "
+    "{extra} {log}"
+)
+
