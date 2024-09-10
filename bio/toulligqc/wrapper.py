f2fe2ff8768b72fc16f42154556c5db46aa91ab7
Salome Brunon <salome.brunon@gmail.com>
2024-09-10 11:50:36 +0200

    UPDATE: use pathlib.Path for handling file paths and extensions

--- bio/toulligqc/wrapper.py
+++ bio/toulligqc/wrapper.py
@@ -4,16 +4,22 @@ __email__ = "salome.brunon@gmail.com"
 __license__ = "CECILL-2.1"
 
 from snakemake.shell import shell
+from pathlib import Path
 
-input_flag = "--sequencing-summary-source "
-if snakemake.input[0].endswith(".fastq") or snakemake.input[0].endswith(".fastq.gz") or snakemake.input[0].endswith(".fasta") or snakemake.input[0].endswith(".fastq.gz"):
+input_path = Path(snakemake.input[0])
+if input_path.suffix in [".fastq", ".fastq.gz", ".fasta"]:
     input_flag = "--fastq "
-if snakemake.input[0].endswith(".bam"):
+elif input_path.suffix == ".bam":
     input_flag = "--bam "
+else:
+    input_flag = "--sequencing-summary-source "
+
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell("toulligqc {extra} {input_flag} {snakemake.input} {log} -n toulligqc_demo --force")
+
+
 
 
 
