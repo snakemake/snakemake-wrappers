06fbd498333883347c9dc5d816fe9298536b1e84
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-09-19 14:45:14 +0200

    Fix code

--- bio/toulligqc/wrapper.py
+++ bio/toulligqc/wrapper.py
@@ -11,22 +11,13 @@ if snakemake.input[0].endswith(".gz"):
 else:
     input_path = Path(snakemake.input[0])
 
-if input_path.suffix in [".fastq", ".fastq", ".fas", ".fq"]:
+if input_path.suffix in [".fastq", ".fq"]:
     input_flag = "--fastq "
-    outdir_flag = "-n toulligqc_fastq"
 elif input_path.suffix == ".bam":
     input_flag = "--bam "
-    outdir_flag = "-n toulligqc_bam"
 else:
     input_flag = "--sequencing-summary-source "
-    outdir_flag = "-n toulligqc_sequencing_summary"
 
-extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+outdir = Path(snakemake.output[0]).parent
 
-shell("toulligqc {extra} {input_flag} {snakemake.input} {log} {outdir_flag} --force")
-
-
-
-
-
+shell("toulligqc {extra} {input_flag} {snakemake.input} -n {outdir} {log}")
