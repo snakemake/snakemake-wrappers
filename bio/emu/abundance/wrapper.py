99bb674f9fb6602f00105fd602b833e18674f748
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-09-20 09:08:03 +0200

    Infer output format

--- bio/emu/abundance/wrapper.py
+++ bio/emu/abundance/wrapper.py
@@ -10,8 +10,16 @@ import os
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 extra = snakemake.params.get("extra", "")
-db = snakemake.input.get("db", "")
-if db:
+
+# Infer format of input file
+if len(snakemake.input.reads) == 1:
+    in_fmt = "fasta"
+elif len(snakemake.input.reads) == 2:
+    in_fmt = "fastq"
+else:
+    raise ValueError("invalid number of input files")
+
+if db := snakemake.input.get("db", "")
     db = f"--db {db}"
 
 with tempfile.TemporaryDirectory() as tmpdir:
@@ -28,6 +36,6 @@ with tempfile.TemporaryDirectory() as tmpdir:
     if out_sam := snakemake.output.get("alignments"):
         shell("mv {tmpdir}/output_emu_alignments.sam {out_sam}")
     if out_unclassified_fq := snakemake.output.get("unclassified"):
-        shell("mv {tmpdir}/output_unclassified_mapped.fastq {out_unclassified_fq}")
+        shell("mv {tmpdir}/output_unclassified_mapped.{in_fmt} {out_unclassified_fq}")
     if out_unmapped_fq := snakemake.output.get("unmapped"):
-        shell("mv {tmpdir}/output_unmapped.fastq {out_unmapped_fq}")
+        shell("mv {tmpdir}/output_unmapped.{in_fmt} {out_unmapped_fq}")
