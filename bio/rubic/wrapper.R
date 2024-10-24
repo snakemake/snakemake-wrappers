a72ab474107db3ef789362cd02aaead5b0d7a808
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-10-24 23:05:57 +0200

    Explicit options

--- bio/rubic/wrapper.R
+++ bio/rubic/wrapper.R
@@ -8,7 +8,7 @@ library(RUBIC)
 all_genes <- if ("genefile" %in% snakemake@input) snakemake@input[["genefile"]] else system.file("extdata", "genes.tsv", package="RUBIC")
 fdr <- if ("fdr" %in% snakemake@params) snakemake@params[["fdr"]] else 0.25
 
-rbc <- rubic(fdr, snakemake@input[["seg"]], snakemake@input[["markers"]], genes=all_genes)
+rbc <- rubic(fdr=fdr, seg.cna=snakemake@input[["seg"]], markers=snakemake@input[["markers"]], genes=all_genes)
 rbc$save.focal.gains(snakemake@output[["out_gains"]])
 rbc$save.focal.losses(snakemake@output[["out_losses"]])
 rbc$save.plots(snakemake@output[["out_plots"]])
