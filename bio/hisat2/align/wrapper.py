01f0664f605a4a204058a7d8fc75fc36529e9ffc
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-10-29 19:05:04 +0100

    Add support for ht2l indexes

--- bio/hisat2/align/wrapper.py
+++ bio/hisat2/align/wrapper.py
@@ -11,29 +11,31 @@ from snakemake.shell import shell
 # Placeholder for optional parameters
 extra = snakemake.params.get("extra", "")
 # Run log
-log = snakemake.log_fmt_shell()
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 # Input file wrangling
 reads = snakemake.input.get("reads")
 if isinstance(reads, str):
-    input_flags = "-U {0}".format(reads)
+    input_flags = f"-U {reads}"
 elif len(reads) == 1:
-    input_flags = "-U {0}".format(reads[0])
+    input_flags = f"-U {reads[0]}"
 elif len(reads) == 2:
-    input_flags = "-1 {0} -2 {1}".format(*reads)
+    input_flags = f"-1 {reads[0]} -2 {reads[1]}"
 else:
     raise RuntimeError(
-        "Reads parameter must contain at least 1 and at most 2" " input files."
+        "Reads parameter must contain at least 1 and at most 2 input files."
     )
 
-ht2_files = Path(snakemake.input.idx).glob("*.ht2")
-idx_prefix = os.path.commonprefix(list(ht2_files)).rstrip(".")
+# Index path
+idx_prefix = Path(os.path.commonprefix(snakemake.input.idx)).parent
 
 # Executed shell command
 shell(
-    "(hisat2 {extra} "
-    "--threads {snakemake.threads} "
-    " -x {idx_prefix} {input_flags} "
-    " | samtools view -Sbh -o {snakemake.output[0]} -) "
-    " {log}"
+    "(hisat2"
+    " --threads {snakemake.threads}"
+    " -x {idx_prefix}"
+    " {extra}"
+    " {input_flags}"
+    " | samtools view -Sbh -o {snakemake.output[0]} -"
+    ") {log}"
 )
