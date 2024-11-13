c5efb17d71eb235ec200c9a3501d7bcaba34d8af
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-11-13 21:58:46 +0100

    Simplify code

--- bio/prinseq-plus-plus/wrapper.py
+++ bio/prinseq-plus-plus/wrapper.py
@@ -30,46 +30,26 @@ else:
     raise ValueError("Invalid input file format")
 
 
+# Output files
+for key, value in snakemake.output.items():
+    extras += f" -out_{key} {value}"
+
+
 # Output format
 out_fmt = Path(snakemake.output[0]).suffix
-out_suffix = ""
 if out_fmt == ".gz":
     extra += " -out_gz"
-    out_suffix = out_fmt
     out_fmt = Path(snakemake.output[0].removesuffix(".gz")).suffix
 if out_fmt in ext_fq:
     extra += " -out_format 0"
-    out_suffix = f".fastq{out_suffix}"
 elif out_fmt in ext_fas:
     extra += " -out_format 1"
-    out_suffix = f".fasta{out_suffix}"
 else:
     raise ValueError("Invalid output file format")
 
 
 # Run Prinseq++
 with tempfile.TemporaryDirectory() as tmpdir:
-    out_prefix = "out"
-
     shell(
-        "prinseq++ -threads {snakemake.threads} -fastq {snakemake.input[0]} {file2} {extra} -out_name {tmpdir}/{out_prefix} {log}"
+        "prinseq++ -threads {snakemake.threads} -fastq {snakemake.input[0]} {file2} {extra} -out_name {tmpdir}/tmp {log}"
     )
-
-    if out := snakemake.output.get("r1"):
-        out_pe = "_R1" if input_pe else ""
-        shell("mv {tmpdir}/{out_prefix}_good_out{out_pe}{out_suffix} {out}")
-    if out := snakemake.output.get("r2"):
-        out_pe = "_R2" if input_pe else ""
-        shell("mv {tmpdir}/{out_prefix}_good_out{out_pe}{out_suffix} {out}")
-    if out := snakemake.output.get("r1_single"):
-        out_pe = "_R1" if input_pe else ""
-        shell("mv {tmpdir}/{out_prefix}_single_out{out_pe}{out_suffix} {out}")
-    if out := snakemake.output.get("r2_single"):
-        out_pe = "_R2" if input_pe else ""
-        shell("mv {tmpdir}/{out_prefix}_single_out{out_pe}{out_suffix} {out}")
-    if out := snakemake.output.get("r1_bad"):
-        out_pe = "_R1" if input_pe else ""
-        shell("mv {tmpdir}/{out_prefix}_bad_out{out_pe}{out_suffix} {out}")
-    if out := snakemake.output.get("r2_bad"):
-        out_pe = "_R2" if input_pe else ""
-        shell("mv {tmpdir}/{out_prefix}_bad_out{out_pe}{out_suffix} {out}")
