409495c97427cdd528bf3cfc40a90a54d93cfabf
Felix Mölder <felix.moelder@uk-essen.de>
2024-11-13 13:54:27 +0100

    import path

--- bio/vg/autoindex/giraffe/wrapper.py
+++ bio/vg/autoindex/giraffe/wrapper.py
@@ -4,12 +4,13 @@ __email__ = "felix.moelder@uk-essen.de"
 __license__ = "MIT"
 
 from snakemake.shell import shell
+from os import path
 
 # Extract arguments.
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
-prefix_path = snakemake.output[0].splitext()[0]
+prefix_path = path.splitext(snakemake.output[0])[0]
 vcf_cmd = "-v {snakemake.input['vcf']}" if snakemake.input.get("vcf", None) else ""
 shell(
     "(vg autoindex -w giraffe"
