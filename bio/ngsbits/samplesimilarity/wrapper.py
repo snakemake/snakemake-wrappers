b150b26bf94e1abdda8e4cd88caaa690f2e4f88c
tdayris <thibault.dayris@gustaveroussy.fr>
2024-11-25 17:48:17 +0100

    Dev

--- bio/ngsbits/samplesimilarity/wrapper.py
+++ bio/ngsbits/samplesimilarity/wrapper.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-"""Snakemake wrapper for Sex.DetERRmine"""
+"""Snakemake wrapper for NGS-bits SampleSimilarity"""
 
 __author__ = "Thibault Dayris"
 __copyright__ = "Copyright 2024, Thibault Dayris"
@@ -8,7 +8,6 @@ __email__ = "thibault.dayris@gustaveroussy.fr"
 __license__ = "MIT"
 
 from snakemake.shell import shell
-from tempfile import TemporaryDirectory
 
 log = snakemake.log_fmt_shell(
     stdout=True,
