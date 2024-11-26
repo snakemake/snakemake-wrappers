d03571c9e085209ceda530a47a416b4b250c4cc5
tdayris <thibault.dayris@gustaveroussy.fr>
2024-11-26 14:34:55 +0100

    Initial commit

---
+++ ngsbits/sampleancestry/wrapper.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+"""Snakemake wrapper for NGS-bits SampleAncestry"""
+
+__author__ = "Thibault Dayris"
+__copyright__ = "Copyright 2024, Thibault Dayris"
+__email__ = "thibault.dayris@gustaveroussy.fr"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+log = snakemake.log_fmt_shell(
+    stdout=False,
+    stderr=True,
+)
+extra = snakemake.params.get("extra", "")
+
+shell(
+    "SampleAncestry {extra} "
+    "-in {snakemake.input} "
+    "-out '{snakemake.output}' "
+    "{log}"
+)
