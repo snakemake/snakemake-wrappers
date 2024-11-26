50ac2db8d79e2330a3c42983421d0a3924550f7e
tdayris <thibault.dayris@gustaveroussy.fr>
2024-11-26 10:19:49 +0100

    Protect paths

--- bio/ngsbits/samplesimilarity/wrapper.py
+++ bio/ngsbits/samplesimilarity/wrapper.py
@@ -16,11 +16,11 @@ log = snakemake.log_fmt_shell(
 extra = snakemake.params.get("extra", "")
 ref = snakemake.input.get("ref")
 if ref:
-    extra += f" -ref {ref} "
+    extra += f" -ref '{ref}' "
 
 roi = snakemake.input.get("roi")
 if roi:
-    extra += f" -roi {roi} "
+    extra += f" -roi '{roi}' "
 
 input_files = snakemake.input.get("samples")
 if all(str(i).endswith(("vcf", "vcf.gz")) for i in input_files):
