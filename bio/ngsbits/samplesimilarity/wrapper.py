53f38afe91aec9f7c800a6e1de3cb06ce717a352
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-12-02 14:24:38 +0100

    Code format

--- bio/ngsbits/samplesimilarity/wrapper.py
+++ bio/ngsbits/samplesimilarity/wrapper.py
@@ -16,23 +16,24 @@ log = snakemake.log_fmt_shell(
 extra = snakemake.params.get("extra", "")
 ref = snakemake.input.get("ref")
 if ref:
-    extra += f" -ref {ref:q} "
+    extra += f" -ref {ref:q}"
 
 roi = snakemake.input.get("regions")
 if roi:
-    extra += f" -roi {roi:q} "
+    extra += f" -roi {roi:q}"
 
 input_files = snakemake.input.get("samples")
-if all(str(i).endswith(("vcf", "vcf.gz")) for i in input_files):
-    extra += " -mode vcf "
-elif all(str(i).endswith(("sam", "bam", "cram")) for i in input_files):
-    extra += " -mode bam "
+if all(str(i).endswith((".vcf", ".vcf.gz")) for i in input_files):
+    extra += " -mode vcf"
+elif all(str(i).endswith((".sam", ".bam", ".cram")) for i in input_files):
+    extra += " -mode bam"
 else:
-    extra += " -mode gsvar "
+    extra += " -mode gsvar"
 
 shell(
-    "SampleSimilarity {extra} "
-    "-in {input_files} "
-    "-out '{snakemake.output}' "
-    "{log}"
+    "SampleSimilarity"
+    " -in {input_files}"
+    " {extra}"
+    " -out {snakemake.output:q}"
+    " {log}"
 )
