3baaaaab1c2f8949b86a4447b1c0bb88f49b55b8
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2025-03-06 16:01:50 +0100

    PR fixes: simplify shell call

--- geo/overturemaps/download/wrapper.py
+++ geo/overturemaps/download/wrapper.py
@@ -7,19 +7,19 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+extra = snakemake.params.get("extra", "")
 bbox = snakemake.params.get("bbox", "")
 if bbox:
     if isinstance(bbox, list):
         bbox = ",".join(map(str, bbox))
     bbox = f"--bbox {bbox}"
 
-command = (
-    "overturemaps download "
-    f"-f {snakemake.params.format} "
-    f"--type={snakemake.params.type} "
-    f"-o {snakemake.output.path} "
-    f"{bbox} "
-    f"{snakemake.params.get("extra", "")} "
+shell(
+    "overturemaps download"
+    f" -f {snakemake.params.format}"
+    f" --type {snakemake.params.type}"
+    f" {bbox}"
+    f" {extra}"
+    f" --output {snakemake.output.path}"
+    f" {log}"
 )
-
-shell(f"({command}) {log}")
