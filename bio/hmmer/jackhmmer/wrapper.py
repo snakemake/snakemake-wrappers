e9cc437b7f3f026cb59432d0468907fc9ed6f1a3
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-03-12 22:41:25 +0100

    Fix names

--- bio/hmmer/jackhmmer/wrapper.py
+++ bio/hmmer/jackhmmer/wrapper.py
@@ -9,23 +9,26 @@ from os import path
 from snakemake.shell import shell
 
 
-# Output must be set even if its not desired.  Default to '/dev/null'
-out_cmd = (f" -o {snakemake.output.get('outfile', '/dev/null')}",)
-
-if snakemake.output.get("tblout") is not None:
-    out_cmd += (f" --tblout {snakemake.output.get('tblout')}",)
-
-if snakemake.output.get("domtblout") is not None:
-    out_cmd += (f" --domtblout {snakemake.output.get('domtblout')}",)
-
-if snakemake.output.get("alignment_hits") is not None:
-    out_cmd += (f" -A {snakemake.output.get('alignment_hits')}",)
-
-extra_flags = snakemake.params.get("extra", "")
+extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
+# Output must be set even if its not desired.  Default to '/dev/null'
+out_cmd = f" -o {snakemake.output.get('summary', '/dev/null')}"
+
+if snakemake.output.get("tblout") is not None:
+    out_cmd += f" --tblout {snakemake.output.get('hits_tbl')}"
+
+if snakemake.output.get("domtblout") is not None:
+    out_cmd += f" --domtblout {snakemake.output.get('dom_tbl')}"
+
+if snakemake.output.get("alignment_hits") is not None:
+    out_cmd += f" -A {snakemake.output.get('hits_aln')}"
+
 shell(
-    " jackhmmer --cpu {snakemake.threads} "
-    " {out_cmd} {extra_flags} "
-    " {snakemake.input.query} {snakemake.input.db} {log}"
+    "jackhmmer --cpu {snakemake.threads}"
+    " {extra}"
+    " {out_cmd}"
+    " {snakemake.input.query}"
+    " {snakemake.input.db}"
+    " {log}"
 )
