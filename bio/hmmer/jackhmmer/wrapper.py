740de371fd3de0fec9a291d21db0a8d88bb2688a
Thomas <mulvaney@mailbox.org>
2025-03-12 10:10:32 +0100

    move all extra options into extra.

--- bio/hmmer/jackhmmer/wrapper.py
+++ bio/hmmer/jackhmmer/wrapper.py
@@ -9,56 +9,23 @@ from os import path
 from snakemake.shell import shell
 
 
-# Some parameters are mutually exclusive
-def exclusive_parameters(params):
-    num_set = 0
-    for p in params:
-        if snakemake.params.get(p) is not None:
-            num_set += 1
-        if num_set > 1:
-            raise Exception(
-                f"Only one of the parameters {', '.join(params)} can be set at a time"
-            )
+# Output must be set even if its not desired.  Default to '/dev/null'
+out_cmd = (f" -o {snakemake.output.get('outfile', '/dev/null')}",)
 
+if snakemake.output.get("tblout") is not None:
+    out_cmd += (f" --tblout {snakemake.output.get('tblout')}",)
 
-# Map outputs to flags
-output_parameters = [
-    ("-o", "outfile", ""),
-    ("--tblout", "tblout", ""),
-    ("--domtblout", "domtblout", ""),
-    ("-A", "alignment_hits", ""),
-]
+if snakemake.output.get("domtblout") is not None:
+    out_cmd += (f" --domtblout {snakemake.output.get('domtblout')}",)
 
-# Map parameters to flags
-parameters = [
-    ("--F1", "filter1"),
-    ("--F2", "filter2"),
-    ("--F3", "filter3"),
-    ("-E", "evalue_threshold"),
-    ("-T", "tvalue_threshold"),
-    ("--incE", "inclusion_evalue"),
-    ("--incT", "inclusion_tvalue"),
-]
+if snakemake.output.get("alignment_hits") is not None:
+    out_cmd += (f" -A {snakemake.output.get('alignment_hits')}",)
 
-# Raise an error if any of these are set at the same time
-exclusive_parameters(["evalue_threshold", "tvalue_threshold"])
-exclusive_parameters(["inclusion_evalue", "inclusion_tvalue"])
-
-out_cmd = ""
-for flag, snake_param_name, default in output_parameters:
-    out_cmd += f" {flag} {snakemake.output.get(snake_param_name, default)}"
-
-
-param_cmd = ""
-for flag, snake_param_name in parameters:
-    value = snakemake.params.get(snake_param_name)
-    if value is not None:
-        param_cmd += f" {flag} {value}"
-
+extra_flags = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 shell(
     " jackhmmer --cpu {snakemake.threads} "
-    " {out_cmd} {param_cmd} "
-    " {snakemake.input.query_file} {snakemake.input.database_file} {log}"
+    " {out_cmd} {extra_flags} "
+    " {snakemake.input.query} {snakemake.input.db} {log}"
 )
