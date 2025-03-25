f868cbf3bf48953fbe48ad305321bcd6d9f693a7
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-03-25 13:30:53 +0100

    Update with new input function

--- utils/aria2c/wrapper.py
+++ utils/aria2c/wrapper.py
@@ -2,16 +2,17 @@ __author__ = "Filipe G. Vieira"
 __copyright__ = "Copyright 2023, Filipe G. Vieira"
 __license__ = "MIT"
 
+snakemake.utils.min_version("9.0.0")
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
 
 
-hash_function = snakemake.params.get("hash_function")
-checksum = snakemake.params.get("checksum")
-if hash_function and checksum:
-    extra += f" --checksum {hash_function}={checksum}"
-
+for hash_function, digest in snakemake.params.items():
+    if hash_function in ["sha-1", "sha-224", "sha-256", "sha-384", "sha-512", "md5", "adler32"]:
+        extra += f" --checksum {hash_function}={digest}"
+        break
+    
 
 shell(
     "aria2c --max-concurrent-downloads {snakemake.threads} {extra} --log {snakemake.log} --out {snakemake.output[0]} {snakemake.params.url} > /dev/null"
