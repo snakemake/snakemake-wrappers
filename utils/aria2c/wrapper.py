a7962d741e73299dadc11e150691dba85d1f8cba
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-03-25 14:24:15 +0100

    Fix typos

--- utils/aria2c/wrapper.py
+++ utils/aria2c/wrapper.py
@@ -9,10 +9,18 @@ extra = snakemake.params.get("extra", "")
 
 
 for hash_function, digest in snakemake.params.items():
-    if hash_function in ["sha-1", "sha-224", "sha-256", "sha-384", "sha-512", "md5", "adler32"]:
+    if hash_function in [
+        "sha-1",
+        "sha-224",
+        "sha-256",
+        "sha-384",
+        "sha-512",
+        "md5",
+        "adler32",
+    ]:
         extra += f" --checksum {hash_function}={digest}"
         break
-    
+
 
 shell(
     "aria2c --max-concurrent-downloads {snakemake.threads} {extra} --log {snakemake.log} --out {snakemake.output[0]} {snakemake.params.url} > /dev/null"
