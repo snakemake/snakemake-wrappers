c8a1565b1e84915fc49a5b353b01aa5e19af7b99
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-03-27 09:41:50 +0100

    Code refactor, with wrapper utils function

--- utils/csvtk/wrapper.py
+++ utils/csvtk/wrapper.py
@@ -4,24 +4,22 @@ __license__ = "MIT"
 
 from pathlib import Path
 from snakemake.shell import shell
+from snakemake_wrapper_utils.snakemake import get_format
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 subcommand = snakemake.params["subcommand"]
 extra = snakemake.params.get("extra", "")
 
-# Input TSV delimiter
-if len(snakemake.input) == 1:
-    if str(snakemake.input).removesuffix(".gz").endswith(".tsv"):
-        extra += " --tabs"
-elif all(input.removesuffix(".gz").endswith(".tsv") for input in snakemake.input):
+
+# Input file delimiter
+in_files = [snakemake.input[0]] if len(snakemake.input) == 1 else snakemake.input
+if all(get_format(in_file) == "tsv" for in_file in in_files):
     extra += " --tabs"
 
 
-# Output TSV delimiter
-if len(snakemake.output) == 1:
-    if str(snakemake.output).removesuffix(".gz").endswith(".tsv"):
-        extra += " --out-tabs"
-elif all(output.removesuffix(".gz").endswith(".tsv") for output in snakemake.output):
+# Output file delimiter
+out_files = [snakemake.output[0]] if len(snakemake.output) == 1 else snakemake.output
+if all(get_format(out_file) == "tsv" for out_file in out_files):
     extra += " --out-tabs"
 
 
