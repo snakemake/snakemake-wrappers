267f418c6fe88ee3a4a9c6d46847054ff3405c95
David Laehnemann <david.laehnemann@hhu.de>
2025-03-31 21:19:16 +0200

    fix: test path typo

--- test_wrappers.py
+++ test_wrappers.py
@@ -5731,7 +5731,7 @@ def test_bismark_genome_preparation(run):
             "-F",
         ],
         compare_results_with_expected={
-            "resources/genome/bismark/Bilsufite_Genome/GA_conversion/genome_mfa.GA_conversion.fa": "expected/genome/bismark/Bilsufite_Genome/genome_mfa.GA_conversion.fa",
+            "resources/genome/bismark/Bisulfite_Genome/GA_conversion/genome_mfa.GA_conversion.fa": "expected/genome/bismark/Bisulfite_Genome/genome_mfa.GA_conversion.fa",
             "resources/genome_gz/bismark/Bisulfite_Genome/CT_conversion/genome_mfa.CT_conversion.fa": "expected/genome_gz/bismark/Bisulfite_Genome/genome_mfa.CT_conversion.fa",
         },
     )
