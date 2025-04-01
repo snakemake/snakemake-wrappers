cab335496d3248a231bcc2e2bb388817d2358cb1
David Laehnemann <david.laehnemann@hhu.de>
2025-04-01 13:53:53 +0200

    fix: CodeRabbit syntax mess

--- bio/bismark/bismark/wrapper.py
+++ bio/bismark/bismark/wrapper.py
@@ -236,13 +236,11 @@ elif fq_1 and fq_2:
         file_suffix=f".{auto_prefix_1}_bismark_bt2_pe.nucleotide_stats.txt",
     )
 else:
- else:
--    ValueError(
-+    raise ValueError(
-         "As named fastq input files, please speciy either of these two options:\n"
-         "1. Only the named input `fq=` for single end read data.\n"
-         "2. Both the named inputs `fq_1=` and `fq_2` for paired end read data.\n"
-     )
+    raise ValueError(
+        "As named fastq input files, please speciy either of these two options:\n"
+        "1. Only the named input `fq=` for single end read data.\n"
+        "2. Both the named inputs `fq_1=` and `fq_2` for paired end read data.\n"
+    )
 
 args.append(f"--parallel {threads}")
 
