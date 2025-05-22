b893f9acc9ab17cd7a1be2044066c69a7c372776
Jie Zhu <alienchuj@gmail.com>
2025-05-22 17:06:57 +0800

    Use custom tempdir and update output

--- bio/megahit/wrapper.py
+++ bio/megahit/wrapper.py
@@ -1,24 +1,17 @@
 """Snakemake wrapper for megahit."""
 
-__author__ = "Jie Zhu @alienzj"
-__copyright__ = "Copyright 2025, Jie Zhu"
+__author__ = "Jie Zhu @alienzj, Filipe G. Vieira @fgvieira"
+__copyright__ = "Copyright 2025, Jie Zhu, Filipe G. Vieira"
 __email__ = "alienchuj@gmail.com"
 __license__ = "MIT"
 
-import os, tempfile, shutil
+import os, tempfile
 from snakemake.shell import shell
 from snakemake_wrapper_utils.snakemake import get_mem
 
-# get output_dir and files from output
-output_dir = os.path.split(snakemake.output[0])[0]
-contigs_file = snakemake.output.get("contigs", os.path.join(output_dir, "contigs.fa"))
-contigs_file_original = os.path.join(output_dir, "final.contigs.fa")
-options_file = snakemake.output.get("options", os.path.join(output_dir, "options.json"))
-log_file = snakemake.output.get("log", os.path.join(output_dir, "log"))
-
 # parse params
 extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
 memory_requirements = get_mem(snakemake, out_unit="KiB") * 1024
 
 # parse short reads
@@ -47,8 +40,9 @@ if hasattr(snakemake.input, "unpaired"):
 elif len(reads) >= 4 and not hasattr(snakemake.input, "r1"):
     input_arg += " --read {} ".format(reads[3])
 
-
-with tempfile.TemporaryDirectory() as temp_dir:
+# run megahit
+output_dir = os.path.dirname(snakemake.output.get("contigs"))
+with tempfile.TemporaryDirectory(dir=os.path.dirname(output_dir)) as temp_dir:
     output_temp_dir = os.path.join(temp_dir, "temp")
 
     shell(
@@ -58,16 +52,16 @@ with tempfile.TemporaryDirectory() as temp_dir:
         " -o {output_temp_dir} "
         " {input_arg} "
         " {extra} "
-        " {log}"
+        " {log} "
     )
 
-    if os.path.exists(os.path.join(output_temp_dir, "done")):
-        shell("rm -rf {output_dir}")
-        shutil.move(output_temp_dir, output_dir)
-
-        if (
-            os.path.exists(contigs_file_original)
-            and os.path.exists(options_file)
-            and os.path.exists(log_file)
-        ):
-            shutil.move(contigs_file_original, contigs_file)
+    # Ensure user can name each file according to their need
+    output_mapping = {
+        "contigs": f"{output_temp_dir}/final.contigs.fa",
+        "json": f"{output_temp_dir}/options.json",
+        "log": f"{output_temp_dir}/log",
+    }
+    for output_key, temp_file in output_mapping.items():
+        output_path = snakemake.output.get(output_key)
+        if output_path:
+            shell("mv --verbose {temp_file:q} {output_path:q} {log}")
