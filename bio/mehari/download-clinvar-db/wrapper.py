27d1feaa0ee921de99959e2147cd8e4decd4583b
Till Hartmann <till.hartmann@tu-dortmund.de>
2025-06-15 09:37:03 +0200

    Apply suggestions from code review
    Co-authored-by: Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>

--- bio/mehari/download-clinvar-db/wrapper.py
+++ bio/mehari/download-clinvar-db/wrapper.py
@@ -11,16 +11,16 @@ version_re = re.compile(r"(?P<date>\d{8})\+(?P<annonars>\d+\.\d+\.\d+)")
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-version = snakemake.params["version"]
+version = snakemake.params.get("version")
 if not version_re.fullmatch(version):
     raise ValueError("Version must have format YYYYMMDD+MAJOR.MINOR.PATCH")
 date, annonars_version = version_re.search(version).groups()
 
-build = snakemake.params["build"].lower()
+build = snakemake.params.get("build", "").lower()
 if build not in {"grch37", "grch38"}:
     raise ValueError("build must be 'grch37' or 'grch38'")
 
-flavour = snakemake.params["flavour"].lower()
+flavour = snakemake.params.get("flavour", "").lower()
 if flavour not in {"genes", "minimal", "sv"}:
     raise ValueError("flavour must be 'genes', 'minimal' or 'sv'")
 
