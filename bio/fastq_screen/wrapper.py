d7c30b2cf83cc65b816d0c2eef3dcd36ff7c478f
tdayris <thibault.dayris@gustaveroussy.fr>
2025-06-18 17:21:15 +0200

    fix PNG error

--- bio/fastq_screen/wrapper.py
+++ bio/fastq_screen/wrapper.py
@@ -10,56 +10,55 @@ __license__ = "MIT"
 
 _config = snakemake.params["fastq_screen_config"]
 
-subset = snakemake.params.get("subset", 100_000)
+subset = snakemake.params.get("subset", 100000)
 aligner = snakemake.params.get("aligner", "bowtie2")
 extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell()
-
-# snakemake.params.fastq_screen_config can be either a dict or a string. If
-# string, interpret as a filename pointing to the fastq_screen config file.
-# Otherwise, create a new tempfile out of the contents of the dict:
-if isinstance(_config, dict):
-    tmp = tempfile.NamedTemporaryFile(delete=False).name
-    with open(tmp, "w") as fout:
-        for label, indexes in _config["database"].items():
-            for aligner, index in indexes.items():
-                fout.write(
-                    "\t".join(["DATABASE", label, index, aligner.upper()]) + "\n"
-                )
-        for aligner, path in _config["aligner_paths"].items():
-            fout.write("\t".join([aligner.upper(), path]) + "\n")
-    config_file = tmp
-else:
-    config_file = _config
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
 
 # fastq_screen hard-codes filenames according to this prefix. We will send
 # hard-coded output to a temp dir, and then move them later.
 prefix = re.split(".fastq|.fq|.txt|.seq", os.path.basename(snakemake.input[0]))[0]
 
-tempdir = tempfile.mkdtemp()
 
-shell(
-    "fastq_screen --outdir {tempdir} "
-    "--force "
-    "--aligner {aligner} "
-    "--conf {config_file} "
-    "--subset {subset} "
-    "--threads {snakemake.threads} "
-    "{extra} "
-    "{snakemake.input[0]} "
-    "{log}"
-)
+with tempfile.TemporaryDirectory() as tempdir:
+    # snakemake.params.fastq_screen_config can be either a dict or a string. If
+    # string, interpret as a filename pointing to the fastq_screen config file.
+    # Otherwise, create a new tempfile out of the contents of the dict:
+    if isinstance(_config, dict):
+        config_file = f"{tempdir}/fastq_screen_config.txt"
 
-# Move output to the filenames specified by the rule
-txt = snakemake.output.get("txt")
-if txt:
-    shell("mv {tempdir}/{prefix}_screen.txt {txt}")
+        with open(config_file, "w") as fout:
+            for label, indexes in _config["database"].items():
+                for aligner, index in indexes.items():
+                    fout.write(
+                        "\t".join(["DATABASE", label, index, aligner.upper()]) + "\n"
+                    )
+                for aligner, path in _config["aligner_paths"].items():
+                    fout.write("\t".join([aligner.upper(), path]) + "\n")
+    else:
+        config_file = _config
 
-png = snakemake.output.get("png")
-if png:
-    shell("mv {tempdir}/{prefix}_screen.png {png}")
+    shell(
+        "fastq_screen --outdir {tempdir} "
+        "--force "
+        "--aligner {aligner} "
+        "--conf {config_file} "
+        "--subset {subset} "
+        "--threads {snakemake.threads} "
+        "{extra} "
+        "{snakemake.input[0]} "
+        "{log}"
+    )
 
-# Clean up temp
-shell("rm -r {tempdir}")
-if isinstance(_config, dict):
-    shell("rm {tmp}")
+    # Move output to the filenames specified by the rule if user expects them
+    txt = snakemake.output.get("txt")
+    if txt:
+        shell("mv --verbose {tempdir}/{prefix}_screen.txt {txt} {log}")
+
+    png = snakemake.output.get("png")
+    if png:
+        shell("mv --verbose {tempdir}/{prefix}_screen.png {png} {log}")
+
+    conf = snakemake.output.get("conf")
+    if conf:
+        shell("mv --verbose {config_file} {conf} {log}")
