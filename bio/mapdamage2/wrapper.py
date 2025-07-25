991f553712f081f92d5061b3d74eac9cfdf73318
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-07-25 18:54:36 +0200

    fix: bug when rescaling BAM (#3687)
    <!-- Ensure that the PR title follows conventional commit style (<type>:
    <description>)-->
    <!-- Possible types are here:
    https://github.com/commitizen/conventional-commit-types/blob/master/index.json
    -->
    
    <!-- Add a description of your PR here-->
    Fix #3686 
    
    ### QC
    <!-- Make sure that you can tick the boxes below. -->
    
    * [x] I confirm that I have followed the [documentation for contributing
    to
    `snakemake-wrappers`](https://snakemake-wrappers.readthedocs.io/en/stable/contributing.html).
    
    While the contributions guidelines are more extensive, please
    particularly ensure that:
    * [x] `test.py` was updated to call any added or updated example rules
    in a `Snakefile`
    * [x] `input:` and `output:` file paths in the rules can be chosen
    arbitrarily
    * [x] wherever possible, command line arguments are inferred and set
    automatically (e.g. based on file extensions in `input:` or `output:`)
    * [x] temporary files are either written to a unique hidden folder in
    the working directory, or (better) stored where the Python function
    `tempfile.gettempdir()` points to
    * [x] the `meta.yaml` contains a link to the documentation of the
    respective tool or command under `url:`
    * [x] conda environments use a minimal amount of channels and packages,
    in recommended ordering
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Updated the rescaling configuration so that when rescaling is enabled
    for BAM file processing, both required parameters are applied. This
    improvement ensures that command-line operations align with current
    expectations for proper rescaling behavior.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- bio/mapdamage2/wrapper.py
+++ bio/mapdamage2/wrapper.py
@@ -2,31 +2,74 @@ __author__ = "Filipe G. Vieira"
 __copyright__ = "Copyright 2020, Filipe G. Vieira"
 __license__ = "MIT"
 
-import os.path
+import tempfile
+from pathlib import Path
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
 
+# Input BAM fle
 in_bam = snakemake.input.get("bam", "")
 if in_bam:
-    in_bam = "--input " + in_bam
+    in_bam = f"--input {in_bam}"
 
-output_folder = os.path.dirname(snakemake.output.get("log", ""))
-if not output_folder:
-    raise ValueError("mapDamage2 rule needs output 'log'.")
+# Output
+out_plot = any([in_tag.startswith("plot_") for in_tag, _ in snakemake.output.items()])
+out_stats = any([in_tag.startswith("stats_") for in_tag, _ in snakemake.output.items()])
+out_bam = snakemake.output.get("bam", "")
+if out_bam:
+    out_bam = f"--rescale --rescale-out {out_bam}"
 
-rescaled_bam = snakemake.output.get("rescaled_bam", "")
-if rescaled_bam:
-    rescaled_bam = "--rescale-out " + rescaled_bam
+# Check if only tpye of output is specified
+# https://stackoverflow.com/questions/16801322/how-can-i-check-that-a-list-has-one-and-only-one-truthy-value
+i = iter([out_plot, out_stats, out_bam])
+if any(i) and not any(i):
+    if out_plot:
+        extra += " --no-stats"
+    elif out_stats:
+        extra += " --stats-only"
+    elif out_bam:
+        extra += " --rescale-only"
+    else:
+        raise ValueError("unexpected error.")
 
 
-shell(
-    "mapDamage "
-    "{in_bam} "
-    "--reference {snakemake.input.ref} "
-    "--folder {output_folder} "
-    "{rescaled_bam} "
-    "{extra} "
-    "{log}"
-)
+with tempfile.TemporaryDirectory() as tmpdir:
+    fnames_tmp = {
+        "GtoA3p": "3pGtoA_freq.txt",
+        "CtoT5p": "5pCtoT_freq.txt",
+        "dnacomp": "dnacomp.txt",
+        "lg_dist": "lgdistribution.txt",
+        "misinc": "misincorporation.txt",
+        "plot_misinc": "Fragmisincorporation_plot.pdf",
+        "plot_len": "Length_plot.pdf",
+        "log": "Runtime_log.txt",
+        "stats_ref": "dnacomp_genome.csv",
+        "stats_prob": "Stats_out_MCMC_correct_prob.csv",
+        "stats_hist": "Stats_out_MCMC_hist.pdf",
+        "stats_iter": "Stats_out_MCMC_iter.csv",
+        "stats_summ": "Stats_out_MCMC_iter_summ_stat.csv",
+        "stats_plot_freq": "Stats_out_MCMC_post_pred.pdf",
+        "stats_plot_trace": "Stats_out_MCMC_trace.pdf",
+    }
+
+    # Symlink input files to temp (to use as results) folder
+    for in_tag, fname_tmp in snakemake.input.items():
+        if in_tag not in ["bam", "ref"]:
+            (Path(tmpdir) / fnames_tmp[in_tag]).symlink_to(Path(fname_tmp).resolve())
+
+    shell(
+        "mapDamage"
+        " {in_bam}"
+        " --reference {snakemake.input.ref}"
+        " --folder {tmpdir}"
+        " {out_bam}"
+        " {extra}"
+        " {log}"
+    )
+
+    for in_tag, fname_tmp in fnames_tmp.items():
+        fname_dest = snakemake.output.get(in_tag)
+        if fname_dest:
+            shell("cp --verbose {tmpdir}/{fname_tmp} {fname_dest} {log}")
