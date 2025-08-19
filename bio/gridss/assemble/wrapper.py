77018223859171c255962ef4e546d861a6871b9e
Christoph Ritzel <christoph.ritzel@tron-mainz.de>
2025-08-19 18:13:20 +0200

    Support both reference.dict and reference.fasta.dict in GRIDSS wrappers Reference setup has changed to produce reference.fasta.dict instead of reference.dict.
    Here I have changed it to just check the existance of the given dictionary without asuming a fixed ending like .dict.

--- bio/gridss/assemble/wrapper.py
+++ bio/gridss/assemble/wrapper.py
@@ -31,7 +31,7 @@ for ending in (".amb", ".ann", ".bwt", ".pac", ".sa"):
             )
         )
 
-dictionary = path.splitext(reference)[0] + ".dict"
+dictionary = reference + ".dict"
 if not path.exists(dictionary):
     raise ValueError(
         "{dictionary}.dict missing. Please make sure the reference dictionary was properly created. This can be accomplished for example by CreateSequenceDictionary.jar from Picard".format(
@@ -45,6 +45,6 @@ shell(
     "--threads {snakemake.threads} "  # Threads
     "--workingdir {snakemake.params.workingdir} "  # Working directory
     "--assembly {snakemake.output.assembly} "  # Assembly output
-    "{snakemake.input.bams} "
+    "{snakemake.input.bam} "  # changed to bam for consistancy
     "{extra}) {log}"
 )
