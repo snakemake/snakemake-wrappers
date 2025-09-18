3e796591da167493bb64ef70676c0e3745d36a76
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-09-18 11:01:38 +0200

    Code format

--- bio/tmb/pyeffgenomesize/wrapper.py
+++ bio/tmb/pyeffgenomesize/wrapper.py
@@ -17,7 +17,7 @@ extra = snakemake.params.get("extra", "")
 # Optional IO files
 bam = snakemake.input.get("bam")
 if bam:
-    extra += f" --bam '{bam}' --mosdepth "
+    extra += f" --bam '{bam}' --mosdepth"
 
 
 # pyEffGenomeSize does not erase temporary files
@@ -27,12 +27,12 @@ with TemporaryDirectory() as tempdir:
     thresholds = snakemake.output.get("thresholds")
     if thresholds:
         optional_output["thresholds"] = f"{tempdir}/snake_result.thresholds.bed"
-        extra += " --saveIntermediates "
+        extra += " --saveIntermediates"
 
     regions = snakemake.output.get("regions")
     if regions:
         if not is_arg("--saveIntermediates", extra):
-            extra += " --saveIntermediates "
+            extra += " --saveIntermediates"
         optional_output["regions"] = f"{tempdir}/snake_result.regions.bed.gz"
 
     intersect = snakemake.output.get("intersect")
@@ -40,13 +40,14 @@ with TemporaryDirectory() as tempdir:
         optional_output["intersect"] = f"{tempdir}/snake_result.intersect.bed"
 
     shell(
-        "pyEffGenomeSize.py {extra} "
-        "--bed {snakemake.input.bed:q} "
-        "--gtf {snakemake.input.gtf:q} "
-        "--thread {snakemake.threads} "
-        "--oprefix '{tempdir}/snake_result' "
-        " > {snakemake.output.txt:q} "
-        "{log} "
+        "pyEffGenomeSize.py"
+        " --thread {snakemake.threads}"
+        " --bed {snakemake.input.bed:q}"
+        " --gtf {snakemake.input.gtf:q}"
+        " {extra}"
+        " --oprefix {tempdir}/snake_result"
+        " > {snakemake.output.txt:q}"
+        " {log}"
     )
     log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
     for move_cmd in move_files(snakemake, optional_output):
