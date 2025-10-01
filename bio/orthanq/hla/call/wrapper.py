27076add73f23659a086261648b8f9139241e70e
Hamdiye Uzuner <hamdiye.uzuner@uni-due.de>
2025-10-01 21:58:22 +0200

    add sample-name parameter

--- bio/orthanq/hla/call/wrapper.py
+++ bio/orthanq/hla/call/wrapper.py
@@ -6,8 +6,13 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 prior_opt = snakemake.params.get("prior", "")
+sample_name = snakemake.params.get("sample_name", "")
+
+# only add --sample-name if a sample name is given
+sample_name_opt = f"--sample-name {sample_name}" if sample_name else ""
+
 shell(
     "orthanq call hla --haplotype-calls {snakemake.input.haplotype_calls} "
     " --haplotype-variants {snakemake.input.haplotype_variants} --prior {prior_opt} "
-    " --xml {snakemake.input.xml} --output {snakemake.output[0]} {log}"
+    " --xml {snakemake.input.xml} --output {snakemake.output[0]} {sample_name_opt} {log}"
 )
