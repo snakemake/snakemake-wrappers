e968312c1cba0e46e953a4d641dd7c3a94d1bb41
Hamdiye Uzuner <hamdiye.uzuner@uni-due.de>
2025-10-01 22:05:18 +0200

    update candidate generation with the new flag

--- bio/orthanq/hla/candidates/wrapper.py
+++ bio/orthanq/hla/candidates/wrapper.py
@@ -6,8 +6,12 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+# Optional flag for outputting bcf instead of vcf.
+output_bcf = snakemake.params.get("output_bcf", False)
+output_bcf_flag = "--output-bcf" if output_bcf else ""
+
 shell(
     "orthanq candidates hla --allele-freq {snakemake.input.allele_freqs} "
     " --alleles {snakemake.input.alleles} --genome {snakemake.input.genome} "
-    " --xml {snakemake.input.xml} --output {snakemake.output[0]} {log}"
+    " --xml {snakemake.input.xml} {output_bcf_flag} --output {snakemake.output[0]} {log}"
 )
