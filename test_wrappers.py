a2700f74df7a0ee54584c1745553cb608fd702c3
fxwiegand <fxwiegand@wgdnet.de>
2025-10-27 13:46:51 +0100

    Update alignoth output dir and add test for output-dir
    Add test_alignoth to verify output-dir target and update Snakefile to
    use output-dir instead of out/ for alignoth_output_dir rule.

--- test_wrappers.py
+++ test_wrappers.py
@@ -250,6 +250,21 @@ def test_agat(run):
     )
 
 
+def test_alignoth(run):
+    run(
+        "bio/alignoth",
+        ["snakemake", "--cores", "1", "--use-conda", "-F", "out/json_plot.vl.json"],
+    )
+    run(
+        "bio/alignoth",
+        ["snakemake", "--cores", "1", "--use-conda", "-F", "out/plot.html"],
+    )
+    run(
+        "bio/alignoth",
+        ["snakemake", "--cores", "1", "--use-conda", "-F", "output-dir/"],
+    )
+
+
 def test_miller(run):
     run(
         "utils/miller",
