e9ced1baa33bc5f6fab726b5cee1774ecbb9edfb
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-11-21 10:16:13 +0100

    Fix NPO files

--- bio/nonpareil/infer/wrapper.py
+++ bio/nonpareil/infer/wrapper.py
@@ -77,3 +77,7 @@ with tempfile.NamedTemporaryFile() as tmp:
         " {extra}"
         " {log}"
     )
+
+    if empty and redund_sum:
+        with open(redund_sum, "at") as f:
+            f.write("\t".join(["0.000000"]+["0.00000"]*5) + "\n")
