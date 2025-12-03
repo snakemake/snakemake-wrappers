3a7f374414ca52e9e498c51e67a7b996cfcad654
Yoann Pradat <yoann.pradat@gustaveroussy.fr>
2025-12-03 13:41:48 +0100

    remove output_dir optional param

--- bio/falco/wrapper.py
+++ bio/falco/wrapper.py
@@ -17,17 +17,7 @@ if len(snakemake.input) > 1:
     raise IOError("Got multiple input files, I don't know how to process them!")
 
 # Output directory
-output_dir_out = snakemake.output.get("dir", "")
-output_dir_param = snakemake.params.get("output_dir", "")
-if output_dir_out and output_dir_param:
-    raise ValueError(
-        "You provided values for output_dir via snakemake output and parameters. Choose one."
-    )
-else:
-    if output_dir_out:
-        output_dir_user = output_dir_out
-    else:
-        output_dir_user = output_dir_param
+output_dir_user = snakemake.output.get("dir", "")
 
 # Run falco.
 # We use a temp dir to clean up intermediate files.
@@ -49,7 +39,7 @@ with TemporaryDirectory() as tempdir:
 
     # If the user did not specify an output directory in the snakemake outputs, move individual files
     # to the locations specified by the user
-    if output_dir_out == "":
+    if not output_dir_user:
         html_path = os.path.join(output_dir_algo, "fastqc_report.html")
         data_path = os.path.join(output_dir_algo, "fastqc_data.txt")
         summ_path = os.path.join(output_dir_algo, "summary.txt")
