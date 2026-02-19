be5f9c650219ae9187fcb4758c500c933ea952f3
fgvieira <1151762+fgvieira@users.noreply.github.com>
2026-02-19 19:02:52 +0100

    Fix typo

--- utils/jq/wrapper.py
+++ utils/jq/wrapper.py
@@ -4,9 +4,9 @@
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
 # jq expression should be quoted
 expression = snakemake.params.get("expression", ".")
 
-shell("jq {extra} {expression:q} {snakemake.input[0]} {snakemake.output[0]} {log}")
+shell("jq {extra} {expression:q} {snakemake.input[0]} > {snakemake.output[0]} {log}")
