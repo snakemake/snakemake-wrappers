9533b3a849a864a6449f276336a9611c4af22b5f
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-03-25 14:56:24 +0100

    Code refactoring

--- bio/kraken2/build/wrapper.py
+++ bio/kraken2/build/wrapper.py
@@ -5,20 +5,14 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
-db = snakemake.input.get("db")
-assert db is not None, "Input -> db is a required input parameter"
-
 extra = snakemake.params.get("extra", "")
 
-hash_file = snakemake.output.get("hash")
-opts_file = snakemake.output.get("opts")
-taxo_file = snakemake.output.get("taxo")
+assert snakemake.output.get("hash") is not None, "Output -> hash is required"
+assert snakemake.output.get("opts") is not None, "Output -> opts is required"
+assert snakemake.output.get("taxo") is not None, "Output -> taxo is required"
 
-assert hash_file is not None, "Output -> hash is required"
-assert opts_file is not None, "Output -> opts is required"
-assert taxo_file is not None, "Output -> taxo is required"
+log = snakemake.log[0] if snakemake.log else ""
+if log:
+    log = f"--log {log}"
 
-log_file = snakemake.log[0] if snakemake.log else None
-log_flag = f"--log {log_file}" if log_file else ""
-
-shell("k2 build " "--db {db} " "--threads {snakemake.threads} " "{log_flag} " "{extra}")
+shell("k2 build --threads {snakemake.threads} --db {snakemake.input.db} {extra} {log}")
