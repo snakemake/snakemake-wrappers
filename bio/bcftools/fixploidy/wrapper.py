26bc07b26e0649913eda24695ab17c5fdf728866
tdayris <thibault.dayris@outlook.fr>
2026-03-27 13:37:32 +0100

    Update bio/bcftools/fixploidy/wrapper.py
    Co-authored-by: Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>

--- bio/bcftools/fixploidy/wrapper.py
+++ bio/bcftools/fixploidy/wrapper.py
@@ -11,29 +11,5 @@ from snakemake_wrapper_utils.bcftools import get_bcftools_opts
 bcftools_opts = get_bcftools_opts(snakemake, parse_ref=False, parse_memory=False)
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-min_threads = 1
 
-
-# Uncompression shall be done according to user-defined input
-incall = snakemake.input[0]
-if snakemake.input[0].endswith(("gz", "bcf")):
-    min_threads += 1
-    incall = "< <(bcftools view {})".format(incall)
-
-# Compression shall be done according to user-defined output
-outcall = snakemake.output[0]
-if snakemake.output[0].endswith(("gz", "bcf")):
-    min_threads += 1
-    outcall = "| bcftools view {} > {}".format(bcftools_opts, outcall)
-else:
-    outcall = "> {}".format(outcall)
-
-# Each (un)compression step raises the threads requirements
-if snakemake.threads < min_threads:
-    raise ValueError(
-        "At least {} threads required, {} provided".format(
-            min_threads, snakemake.threads
-        )
-    )
-
-shell("(bcftools plugin fixploidy {extra} {incall} {outcall}) {log}")
+shell("bcftools plugin fixploidy {bcftools_opts} {extra} {snakemake.input[0]} {log}")
