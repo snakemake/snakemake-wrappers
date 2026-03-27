b2df06c33b1c814e23d6e4eaa6bdcf8b614c18c1
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-03-27 09:55:04 +0100

    Code format

--- bio/refgenie/wrapper.py
+++ bio/refgenie/wrapper.py
@@ -8,11 +8,19 @@ import refgenconf
 
 conf_path = os.environ["REFGENIE"]
 
-rgc = refgenconf.RefGenConf(conf_path)
+rgc = refgenconf.RefGenConf(filepath=conf_path, writable=True)
 
 # pull asset if necessary
-gat, archive_data, server_url = rgc.pull(snakemake.params.genome, snakemake.params.asset, snakemake.params.tag, force=False)
+gat, archive_data, server_url = rgc.pull(
+    snakemake.params.genome, snakemake.params.asset, snakemake.params.tag, force=False
+)
 
 for seek_key, out in snakemake.output.items():
-    path = rgc.seek(snakemake.params.genome, snakemake.params.asset, tag_name=snakemake.params.tag, seek_key=seek_key, strict_exists=True)
+    path = rgc.seek(
+        snakemake.params.genome,
+        snakemake.params.asset,
+        tag_name=snakemake.params.tag,
+        seek_key=seek_key,
+        strict_exists=True,
+    )
     os.symlink(path, out)
