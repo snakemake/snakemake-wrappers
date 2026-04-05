6e8ee5ad1e5ea1ad7fc7b24178f647d672170309
fgvieira <1151762+fgvieira@users.noreply.github.com>
2026-04-05 21:41:17 +0200

    Code refactor

--- bio/transdecoder/longorfs/wrapper.py
+++ bio/transdecoder/longorfs/wrapper.py
@@ -5,28 +5,25 @@ __copyright__ = "Copyright 2019, N. Tessa Pierce"
 __email__ = "ntpierce@gmail.com"
 __license__ = "MIT"
 
-from os import path
+from pathlib import Path
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
-
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-gtm_cmd = ""
 gtm = snakemake.input.get("gene_trans_map", "")
 if gtm:
-    gtm_cmd = " --gene_trans_map " + gtm
-
-output_dir = path.dirname(str(snakemake.output))
+    gtm = f"--gene_trans_map {gtm}"
 
 # transdecoder fails if output already exists. No force option available
+output_dir = Path(snakemake.output).parent
 shell("rm -rf {output_dir}")
 
 input_fasta = str(snakemake.input.fasta)
-if input_fasta.endswith("gz"):
-    input_fa = input_fasta.rsplit(".gz")[0]
+if input_fasta.endswith(".gz"):
+    input_fa = Path(input_fasta).with_suffix("")
     shell("gunzip -c {input_fasta} > {input_fa}")
 else:
     input_fa = input_fasta
 
-shell("TransDecoder.LongOrfs -t {input_fa} {gtm_cmd} {log}")
+shell("TransDecoder.LongOrfs -t {input_fa} {gtm} --output_dir {output_dir} {log}")
