509f1a4bf1dae98f6a2b20f09ec0f8aa69fdd444
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-04-05 21:14:02 +0200

    Update wrapper.py

--- bio/fgbio/annotatebamwithumis/wrapper.py
+++ bio/fgbio/annotatebamwithumis/wrapper.py
@@ -12,39 +12,11 @@ log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 extra_params = snakemake.params.get("extra", "")
 java_opts = get_java_opts(snakemake)
 
-bam_input = snakemake.input.bam
-
-if bam_input is None:
-    raise ValueError("Missing bam input file!")
-elif not isinstance(bam_input, str):
-    raise ValueError("Input bam should be a string: " + str(bam_input) + "!")
-
-umi_input = snakemake.input.umi
-
-if umi_input is None:
-    raise ValueError("Missing input file with UMIs")
-elif not (isinstance(umi_input, str) or isinstance(umi_input, Namedlist)):
-    raise ValueError(
-        "Input UMIs-file should be a string or a snakemake io list: "
-        + str(umi_input)
-        + "!"
-    )
-
-if not len(snakemake.output) == 1:
-    raise ValueError("Only one output value expected: " + str(snakemake.output) + "!")
-output_file = snakemake.output[0]
-
-
-if output_file is None:
-    raise ValueError("Missing output file!")
-elif not isinstance(output_file, str):
-    raise ValueError("Output bam-file should be a string: " + str(output_file) + "!")
-
 shell(
     "fgbio {java_opts} AnnotateBamWithUmis"
-    " -i {bam_input}"
-    " -f {umi_input}"
-    " -o {output_file}"
+    " -i {snakemake.input.bam}"
+    " -f {snakemake.input.umi}"
     " {extra_params}"
+    " -o {snakemake.output[0]}"
     " {log}"
 )
