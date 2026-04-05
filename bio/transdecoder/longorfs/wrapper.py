d36ebec71bea3d7c75f5ea625e4816c2b0f163b8
fgvieira <1151762+fgvieira@users.noreply.github.com>
2026-04-05 21:50:41 +0200

    Fix typo

--- bio/transdecoder/longorfs/wrapper.py
+++ bio/transdecoder/longorfs/wrapper.py
@@ -11,14 +11,6 @@ from snakemake.shell import shell
 extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-gtm = snakemake.input.get("gene_trans_map", "")
-if gtm:
-    gtm = f"--gene_trans_map {gtm}"
-
-# transdecoder fails if output already exists. No force option available
-output_dir = Path(snakemake.output).parent
-shell("rm -rf {output_dir}")
-
 input_fasta = str(snakemake.input.fasta)
 if input_fasta.endswith(".gz"):
     input_fa = Path(input_fasta).with_suffix("")
@@ -26,4 +18,12 @@ if input_fasta.endswith(".gz"):
 else:
     input_fa = input_fasta
 
-shell("TransDecoder.LongOrfs -t {input_fa} {gtm} --output_dir {output_dir} {log}")
+gtm = snakemake.input.get("gene_trans_map", "")
+if gtm:
+    gtm = f"--gene_trans_map {gtm}"
+
+output_dir = Path(snakemake.output[0]).parent
+# transdecoder fails if output already exists. No force option available
+shell(
+    "rm -rf {output_dir} && TransDecoder.LongOrfs -t {input_fa} {gtm} --output_dir {output_dir} {log}"
+)
