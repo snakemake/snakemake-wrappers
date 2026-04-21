60b79617e124cb1cafe240a804c57cae8281b4a0
Patrik Smeds <patrik.smeds@gmail.com>
2026-04-21 10:40:07 +0200

    fix linting

--- bio/pbmarkdup/wrapper.py
+++ bio/pbmarkdup/wrapper.py
@@ -15,6 +15,10 @@ dup_file = f"--dup-file {dup_file}" if dup_file else ""
 inputs = " ".join(snakemake.input)
 
 if "--rmdup" in extra and dup_file:
-    raise ValueError("Cannot specify --rmdup and output duplicates to file using dup_file output option.")
+    raise ValueError(
+        "Cannot specify --rmdup and output duplicates to file using dup_file output option."
+    )
 
-shell("pbmarkdup --num-threads {snakemake.threads} {extra} {inputs} {dedup_file} {dup_file} {log}")
+shell(
+    "pbmarkdup --num-threads {snakemake.threads} {extra} {inputs} {dedup_file} {dup_file} {log}"
+)
