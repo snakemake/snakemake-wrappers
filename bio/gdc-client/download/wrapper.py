bf7f3a2ebaf78024664a2a72ddac7d0abd83b3d5
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-05-21 15:05:21 +0200

    Fix typo

--- bio/gdc-client/download/wrapper.py
+++ bio/gdc-client/download/wrapper.py
@@ -32,22 +32,11 @@ with TemporaryDirectory() as tempdir:
         tmp_path = path.join(tempdir, uuid, path.basename(out_path))
         if not path.exists(tmp_path):
             root, ext1 = path.splitext(out_path)
-            paths = glob.glob(path.join(tempdir, uuid, "*" + ext1))
+            paths = glob.glob(path.join(tempdir, uuid, f"*{ext1}"))
             if len(paths) > 1:
                 root, ext2 = path.splitext(root)
-                paths = glob.glob(path.join(tempdir, uuid, "*" + ext2 + ext1))
-            if len(paths) == 0:
-                raise ValueError(
-                    f"{out_path} file extension {ext1} does not match any downloaded file. Are you sure that UUID {uuid} provides a file of such format?"
-                    )
-                )
-            if len(paths) > 1:
-                raise ValueError(
-                    "Found more than one downloaded file with extension '{}':\n"
-                    "{}\n"
-                    "Cannot match requested output file {} unambiguously.\n".format(
-                        ext2 + ext1, paths, out_path
-                    )
-                )
+                paths = glob.glob(path.join(tempdir, uuid, f"*{ext2}{ext1}"))
+            assert len(paths) > 0, f"File '{out_path}' extension ({ext1}) does not match any downloaded file. Are you sure that UUID {uuid} provides a file of such format?"
+            assert len(paths) == 1, f"Cannot match requested output file {out_path} unambiguously. Found more than one downloaded file with extension '{ext1}{ext2}': {paths}."
             tmp_path = paths[0]
         shell("mv {tmp_path} {out_path}")
