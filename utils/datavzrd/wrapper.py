75effe0d3f425474fdcb09a60e448389461587e6
Felix Wiegand <fxwiegand@wgdnet.de>
2026-05-27 11:55:29 +0200

    Enhance logging during YAML processing
    Redirect stdout and stderr to log file while processing YAML.

--- utils/datavzrd/wrapper.py
+++ utils/datavzrd/wrapper.py
@@ -7,30 +7,31 @@ import tempfile
 from yte import process_yaml
 from snakemake.shell import shell
 import shutil
+import contextlib
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-sys.stderr = open(snakemake.log[0], "w", buffering=1)
-sys.stdout = open(snakemake.log[0], "w", buffering=1)
 
 extra = snakemake.params.get("extra", "")
 
 with tempfile.NamedTemporaryFile(mode="w") as processed, open(
     snakemake.input.config
 ) as f:
-    # support templating in the config file
-    process_yaml(
-        f,
-        outfile=processed,
-        variables={
-            "snakemake": snakemake,
-            # keep the ones below for backwards compatibility
-            "params": snakemake.params,
-            "wildcards": snakemake.wildcards,
-            "input": snakemake.input,
-        },
-        require_use_yte=True,
-    )
-    processed.flush()
+    with open(snakemake.log[0], "w") as log_yte:
+        with contextlib.redirect_stdout(log_yte), contextlib.redirect_stderr(log_yte):
+            # support templating in the config file
+            process_yaml(
+                f,
+                outfile=processed,
+                variables={
+                    "snakemake": snakemake,
+                    # keep the ones below for backwards compatibility
+                    "params": snakemake.params,
+                    "wildcards": snakemake.wildcards,
+                    "input": snakemake.input,
+                },
+                require_use_yte=True,
+            )
+            processed.flush()
 
     if config_out := snakemake.output.get("config"):
         shutil.copy(processed.name, config_out)
