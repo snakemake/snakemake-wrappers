8ea8d22f1507f551e643127143ef946a7afd2470
Felix Wiegand <fxwiegand@wgdnet.de>
2026-05-27 14:25:33 +0200

    Simplify
    Refactor logging to remove temporary stdout/stderr redirection.

--- utils/datavzrd/wrapper.py
+++ utils/datavzrd/wrapper.py
@@ -17,24 +17,20 @@ with tempfile.NamedTemporaryFile(mode="w") as processed, open(
     snakemake.input.config
 ) as f:
     with open(snakemake.log[0], "a") as log_file:
-        old_stdout, old_stderr = sys.stdout, sys.stderr
         sys.stdout = sys.stderr = log_file
-        try:
-            # support templating in the config file
-            process_yaml(
-                f,
-                outfile=processed,
-                variables={
-                    "snakemake": snakemake,
-                    # keep the ones below for backwards compatibility
-                    "params": snakemake.params,
-                    "wildcards": snakemake.wildcards,
-                    "input": snakemake.input,
-                },
-                require_use_yte=True,
-            )
-        finally:
-            sys.stdout, sys.stderr = old_stdout, old_stderr
+        # support templating in the config file
+        process_yaml(
+            f,
+            outfile=processed,
+            variables={
+                "snakemake": snakemake,
+                # keep the ones below for backwards compatibility
+                "params": snakemake.params,
+                "wildcards": snakemake.wildcards,
+                "input": snakemake.input,
+            },
+            require_use_yte=True,
+        )
     processed.flush()
 
     if config_out := snakemake.output.get("config"):
