c325a341051c335f3ef1d4021f0b6bfb3c7cb57e
Donncha O'Toole <donncha@focalise.ie>
2026-08-26 11:33:50 +0100

    refactor: pass DotMatch options via extra

--- bio/dotmatch/crispr-count/wrapper.py
+++ bio/dotmatch/crispr-count/wrapper.py
@@ -8,19 +8,8 @@ from shlex import quote
 from snakemake.shell import shell
 
 extra = snakemake.params.get("extra", "")
-guide_start = snakemake.params.guide_start
-guide_length = snakemake.params.guide_length
-k = snakemake.params.get("k", 1)
-metric = snakemake.params.get("metric", "hamming")
-ambiguity_policy = snakemake.params.get("ambiguity_policy", "radius")
-ambiguous = snakemake.params.get("ambiguous", "discard")
 threads = snakemake.threads
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
-
-indel_window = snakemake.params.get("indel_window")
-indel_window_arg = ""
-if indel_window is not None:
-    indel_window_arg = f"--indel-window {indel_window}"
 
 summary_arg = ""
 summary = snakemake.output.get("summary")
@@ -36,16 +25,9 @@ shell(
     "(dotmatch crispr-count "
     "--library {snakemake.input.library:q} "
     "--samples {snakemake.input.samples:q} "
-    "--guide-start {guide_start} "
-    "--guide-length {guide_length} "
-    "--k {k} "
-    "--metric {metric} "
-    "--ambiguity-policy {ambiguity_policy} "
-    "--ambiguous {ambiguous} "
     "--threads {threads} "
     "--out {snakemake.output.counts:q} "
     "{summary_arg} "
     "{sample_qc_arg} "
-    "{indel_window_arg} "
     "{extra}) {log}"
 )
