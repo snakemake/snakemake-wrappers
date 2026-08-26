3bdf2f424cf1d731e5d27bf3f3fe5676dc8cd867
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-08-26 12:43:41 +0200

    Apply suggestion from @fgvieira

--- bio/dotmatch/crispr-count/wrapper.py
+++ bio/dotmatch/crispr-count/wrapper.py
@@ -18,13 +18,12 @@ sample_qc = snakemake.output.get("sample_qc")
 if sample_qc:
     sample_qc = f"--sample-qc {quote(sample_qc)}"
 
-shell(
-    "(dotmatch crispr-count "
+shell("dotmatch crispr-count "
     "--library {snakemake.input.library:q} "
     "--samples {snakemake.input.samples:q} "
     "--threads {snakemake.threads} "
     "--out {snakemake.output.counts:q} "
     "{summary} "
     "{sample_qc} "
-    "{extra}) {log}"
+    "{extra} {log}"
 )
